1e84db31f0370f0c6de96a96cec6b9034116207f
vilhelmp <magnusp@vilhelm.nu>
2012-12-15 13:41:26 +0100

    added folder and initial files for the splatalogue search API

---
+++ astroquery/splatalogue/splatalogue.py
@@ -0,0 +1,64 @@
+"""
+Splatalogue Catalog Query Tool
+-----------------------------------
+
+:Author: Magnus Vilehlm Persson (magnusp@vilhelm.nu)
+"""
+#~ import cookielib
+#~ import urllib2
+#~ import urllib
+#~ import htmllib
+#~ import formatter
+#~ import gzip
+#~ import os
+#~ import astropy.io.fits as pyfits
+#~ from math import cos, radians
+#~ import multiprocessing as mp
+#~ import time
+#~ import StringIO
+#~ from astroquery.utils import progressbar
+
+
+__all__ = ['splatsearch']
+
+def splatsearch():
+    return None
+
+
+
+# Parse input
+
+# Get/Parse form
+# Fill in form
+# Submit form
+# Get/Parse results
+# Return results in a given structure (astrpy.table.Table?)
+
+
+
+"""
+from astro import table as _table
+
+_table.Table()
+
+Parameters
+----------
+data : numpy ndarray, dict, list, or Table, optional
+    Data to initialize table.
+names : list, optional
+    Specify column names
+dtypes : list, optional
+    Specify column data types
+meta : dict, optional
+    Metadata associated with the table
+copy : boolean, optional
+    Copy the input data (default=True).
+Constructor information:
+ Definition:table.Table(self, data=None, names=None, dtypes=None, meta=None, copy=True)
+
+for example:
+
+t = table.Table(data=array([['line1',10],['line2',40]]), names=['name', 'value'], dtypes=['str', 'float'])
+
+
+"""
