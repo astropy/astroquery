25c1484d5cb73e7f6a7dd3d62321455f38d87aee
Adam Ginsburg <keflavich@gmail.com>
2012-12-27 22:57:49 -0700

    import besancon reader correctly

--- astroquery/tests/test_besancon.py
+++ astroquery/tests/test_besancon.py
@@ -1,5 +1,4 @@
 from astroquery import besancon
-from astropy.io.ascii import besancon as besancon_reader
 import asciitable
 import pytest
 
@@ -8,12 +7,12 @@ from astropy.io.ascii.tests.common import (raises,
                      setup_function, teardown_function, has_isnan)
 
 def test_besancon_reader():
-    B = asciitable.read('besancon_test.txt',Reader=asciitable.besancon.BesanconFixed,guess=False)
+    B = asciitable.read('besancon_test.txt',Reader=besancon.BesanconFixed,guess=False)
     assert_equal(len(B),12)
 
 def test_basic():
     besancon_model = besancon.request_besancon('your@email.net',10.5,0.0)
-    B = asciitable.read(besancon_model,Reader=besancon_reader.BesanconFixed,guess=False)
+    B = asciitable.read(besancon_model,Reader=besancon.BesanconFixed,guess=False)
     B.pprint()
 
 
