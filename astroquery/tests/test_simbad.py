8e29c5561fe0bf00ff953ed4340fa40e0bdb6a34
Adam Ginsburg <keflavich@gmail.com>
2013-02-24 14:45:14 -0700

    added a simbad multiquery test

--- astroquery/tests/test_simbad.py
+++ astroquery/tests/test_simbad.py
@@ -4,3 +4,12 @@ def simbad_test():
     r = simbad.QueryAroundId('m31', radius='0.5s').execute()
     print r.table
 
+
+def multitest():
+    result = simbad.QueryMulti(
+            [simbad.QueryId('m31'),
+             simbad.QueryId('m51')])
+    table = result.execute().table
+    assert "M  31" in table
+    assert "M  51" in table
+
