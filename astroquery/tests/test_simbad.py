564169e3c5daea418832363fa7cb02dbb648d91b
Adam Ginsburg <keflavich@gmail.com>
2013-02-24 14:47:09 -0700

    fixed simbad tests, added some semi-useful assertions

--- astroquery/tests/test_simbad.py
+++ astroquery/tests/test_simbad.py
@@ -3,6 +3,7 @@ from astroquery import simbad
 def simbad_test():
     r = simbad.QueryAroundId('m31', radius='0.5s').execute()
     print r.table
+    assert "M  31" in r.table["MAIN_ID"]
 
 
 def multitest():
@@ -10,6 +11,6 @@ def multitest():
             [simbad.QueryId('m31'),
              simbad.QueryId('m51')])
     table = result.execute().table
-    assert "M  31" in table
-    assert "M  51" in table
+    assert "M  31" in table["MAIN_ID"]
+    assert "M  51" in table["MAIN_ID"]
 
