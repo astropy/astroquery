eab4d5292821782d740a5047e3c69fb8f1d44a2e
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-04-13 14:31:55 +0200

    _PACKAGE_SETUP_ should be _ASTROPY_SETUP_ (see astropy/astropy#950)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.version_info[0] >= 3:
     import builtins
 else:
     import __builtin__ as builtins
-builtins._PACKAGE_SETUP_ = True
+builtins._ASTROPY_SETUP_ = True
 
 import astropy
 from astropy.setup_helpers import (register_commands, adjust_compiler,
