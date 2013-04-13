4396df14836c8839fe04c31d8ea596f66b4734ac
Adam Ginsburg <keflavich@gmail.com>
2013-04-13 16:39:48 -0600

    completely download_file_list code filename namer; it's a bit complex but I think rather useful

--- astroquery/utils/download_file_list.py
+++ astroquery/utils/download_file_list.py
@@ -1,17 +1,39 @@
 import StringIO
+import astropy.io.fits as fits
+import re
+import string
+import astropy.coordinates as coord
+
+whitespace_re = re.compile("\s")
+valid_chars = "-_.()%s%s" % (string.ascii_letters, string.digits)
+
+def validify_filename(filestr):
+    """ Remove invalid characters from a file string """
+    filestr = filestr.strip()
+    filestr = whitespace_re.sub("_",filestr)
+
+    # strip out invalid characters
+    filestr = "".join([c for c in filestr if c in valid_chars])
+    return filestr
 
 def download_list_of_fitsfiles(linklist, output_directory=None,
-        output_prefix=None,):
+        output_prefix=None, save=False, overwrite=False, verbose=False,
+        output_coord_format=None, filename_header_keywords=None,
+        include_input_filename=True):
     """
     Given a list of file URLs, download them and (optionally) rename them
 
     """
     # Loop through links and retrieve FITS images
-    images = []
+    images = {}
     for link in linklist:
 
-        if not os.path.exists(directory):
-            os.mkdir(directory)
+        if output_directory is None:
+            output_directory = ""
+        elif output_directory[-1] != "/":
+            output_directory += "/"
+            if not os.path.exists(output_directory):
+                os.mkdir(output_directory)
 
         with aud.get_readable_fileobj(link, cache=True) as f:
             results = f.read()
@@ -28,24 +50,53 @@ def download_list_of_fitsfiles(linklist, output_directory=None,
             fitsfile = fits.open(G,ignore_missing_end=True)
 
         # Get Multiframe ID from the header
-        images.append(fitsfile)
+        images[link] = fitsfile
 
         if save:
             h0 = fitsfile[0].header
-            filt = str(h0['FILTER']).strip()
-            obj = filt + "_" + str(h0['OBJECT']).strip().replace(":", ".")
 
-            if savename is None:
-                filename = "UKIDSS_%s_G%07.3f%+08.3f_%s.fits" % (filt,glon,glat,obj)
+            if filename_header_keywords: # is not None or empty
+                nametxt = "_".join([validify_filename(str(h0[key])) for key in filename_header_keywords])
             else:
-                filename = savename
+                nametxt = ""
+
+            if output_coord_format:
+                lon = h0['CRVAL1']
+                lat = h0['CRVAL2']
+
+                # this part will eventually be handled by astropy.coordinates directly
+                # ctype = h0['CTYPE1']
+                # if 'RA' in ctype:
+                #     coordinate = coord.ICRSCoordinates(lon,lat,unit=('deg','deg'))
+                # elif 'GLON' in ctype:
+                #     coordinate = coord.GalacticCoordinates(lon,lat,unit=('deg','deg'))
+                # else:
+                #     raise TypeError("Don't recognize ctype %s" % ctype)
+                # coordstr = coordinate.format(output_coord_format)
+                try:
+                    coordstr = output_coord_format.format(lon,lat)
+                except TypeError:
+                    coordstr = output_coord_format % (lon,lat)
+                nametxt += "_" + coordstr
+
+            if include_original_filename:
+                filename_root = os.path.split(link)[1]
+            else:
+                filename_root = ""
+        
+            savename = output_prefix if output_prefix else ""
+            savename += nametxt
+            savename += "_" + filename_root
 
             # Set final directory and file names
-            final_file = directory + '/' + filename
+            final_file = output_directory + filename
 
             if verbose:
                 print "Saving file %s" % final_file
 
-            fitsfile.writeto(final_file, clobber=overwrite)
+            try:
+                fitsfile.writeto(final_file, clobber=overwrite)
+            except IOError:
+                print "Skipped writing file %s because it exists and overwrite=False" % final_file
 
     return images
