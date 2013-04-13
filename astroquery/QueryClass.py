3fc43f17f209947e2b713028bf73de9f21fe58d3
Adam Ginsburg <keflavich@gmail.com>
2013-04-13 15:53:19 -0600

    added Delayed class

--- astroquery/QueryClass.py
+++ astroquery/QueryClass.py
@@ -12,3 +12,11 @@ class QueryClass(object):
 
     def __call__(self):
         raise Exception("All classes must override this!")
+
+class DelayedQueryClass(QueryClass):
+
+    def check_for_update(self, time_min=5):
+        """
+        Check the "result" URL for existence or the presence of useful data
+        """
+        pass
