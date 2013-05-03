7d7c631c6e2d1947b7ac64f2c30ad6680bb56060
Jordan Mirocha <mirochaj@gmail.com>
2013-05-03 13:33:37 -0600

    Now using urllib for SDSS data retrieval, added routines to fetch ALFALFA catalog and spectra.

---
+++ astroquery/alfa/__init__.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+ALFALFA Spectra Archive Query Tool
+-----------------------------------
+
+:Author: Jordan Mirocha (mirochaj@gmail.com)
+"""
+from .core import *
