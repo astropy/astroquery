0188e7787249888543caafeddf58db0360776905
Brian Svoboda <bsvo@lavabit.com>
2013-05-29 02:31:58 -0700

    Added comments and doc-strings

--- astroquery/lamda/core.py
+++ astroquery/lamda/core.py
@@ -2,10 +2,9 @@
 import urllib
 import numpy as np
 from astropy.table import Table
-import ipdb as pdb
 
 # TODO rovib in H2O has wrong format for header
-# TODO convert string elements in table to floats
+
 __all__ = ['LAMDAQuery']
 
 class LAMDAQuery(object):
@@ -73,10 +72,17 @@ class LAMDAQuery(object):
         Parameters
         ----------
         mol : string
-            Molecule designation
+            Molecule or atom designation. For a list of valid designations see
+            the ``print_mols'' method.
         query_type : string
-            energy levels, transitions, rates
-        coll_partner_index : string
+            Query type to execute. Valid options:
+                'erg_levels' -> Energy Levels
+                'rad_trans'  -> Radiative transitions
+                'coll_rates' -> Collisional rates
+        coll_partner_index : number, default 0
+            If collisional rates are queried, set the index of the collisional
+            partner to the order found in the LAMDA datafile. If no index is
+            specified, the first collsional partner is taken.
 
         Returns
         -------
@@ -99,33 +105,69 @@ class LAMDAQuery(object):
         datafile = np.array([s.strip() for s in
             urllib.urlopen(self.url.format(mol)).readlines()])
         # Parse datafile string list and return a table
-        table = self.parse_datafile(datafile, query_type=query_type,
+        table = self._parse_datafile(datafile, query_type=query_type,
             coll_partner_index=coll_partner_index)
         return table
 
-    def parse_datafile(self, datafile, query_type, coll_partner_index=0):
+    def _parse_datafile(self, datafile, query_type, coll_partner_index=0):
         """
+        Parse datafile according to query type and return a Table instance of
+        the data.
+
+        Parameters
+        ----------
+        datafile : np.ndarray
+            Raw data array pulled from LAMDA
+        query_type : string
+            Valid query_type in ['coll_rates', 'rad_trans', 'erg_levels']
+        coll_partner_index : number, default 0
+            Collision partner index, order of partner in file
+
+        Returns
+        -------
+        table : Table
         """
-        # TODO add docstring
         query_identifier = self.query_types[query_type]
         if query_type == 'coll_rates':
             i = coll_partner_index
         else:
             i = 0
+        # Select lines that contain the query identifier
         sections = np.argwhere(np.in1d(datafile, query_identifier))
         if len(sections) == 0:
             raise Exception('Query data not found in file.')
         start_index = sections[i][0]
-        data, col_names = self.select_data(datafile, start_index,
+        # Select rows and columns from the raw datafile list
+        data, col_names = self._select_data(datafile, start_index,
             query_type=query_type)
-        col_dtypes = self.check_dtypes(data)
+        # Convert columns with string data types to floats if possible
+        col_dtypes = self._check_dtypes(data)
         table = Table(data, names=col_names, dtypes=col_dtypes)
         return table
 
-    def select_data(self, data, i, query_type):
+    def _select_data(self, data, i, query_type):
         """
+        Select only data relevant to a specific query type in the raw data
+        array and pick out column names from context.
+
+        Parameters
+        ----------
+        data : np.ndarray
+            Raw data array pulled from LAMDA
+        i : number
+            Row index to start reading from data in
+        query_type : string
+            Valid query_type in ['coll_rates', 'rad_trans', 'erg_levels']
+
+        Returns
+        -------
+        data : np.array
+            Data for query_type
+        col_names : list
+            Column name list
         """
-        # TODO add docstring
+        # LAMDA datafiles have regular formatting, so the index and an "index
+        # offset" are used to retrieve the row data.
         if query_type == 'erg_levels':
             num_erg_levels = int(data[i + 1])
             col_names = [s.strip() for s in data[i + 2][1:].split('+')]
@@ -150,10 +192,20 @@ class LAMDAQuery(object):
         else:
             raise ValueError('Unknown query type.')
 
-    def check_dtypes(self, data):
+    def _check_dtypes(self, data):
         """
+        Check the data types of each column. If a column cannot be converted to
+        a float, a string is used as a fall-back.
+
+        Parameters
+        ----------
+        data : np.ndarray
+
+        Returns
+        -------
+        dtypes : list
+            List of a dtypes for each column in data
         """
-        # TODO add doc-string
         dtypes = []
         for i in xrange(data.shape[1]):
             try:
@@ -162,209 +214,6 @@ class LAMDAQuery(object):
             except ValueError:
                 dtypes.append('|S14')
         return dtypes
-
-    def print_mol_notes():
-        # TODO
-        pass
-
-
-# OLD NIST stuff for template
-def strip_blanks(table):
-    """
-    Remove blank lines from table (included for "human readability" but useless to us...
-
-    returns a single string joined by \n newlines
-    """
-    if isinstance(table,str):
-        table = table.split('\n')
-    table = [line for line in table if numbersletters.search(line)]
-    return "\n".join(table)
-
-class NISTAtomicLinesQuery(object):
-    def __init__(self):
-        self.opener = urllib2.build_opener()
-
-    def query_line_ascii(self, linename, minwav, maxwav, wavelength_unit='A',
-            energy_level_unit='eV', page_size=15, output_order='wavelength',
-            wavelength_type='vacuum'):
-        """
-        Query the NIST Atomic Lines database
-
-        Example
-        =======
-
-        >>> Q = NISTAtomicLinesQuery()
-        >>> Q.query_line_ascii('H I',4000,7000,wavelength_unit='A',energy_level_unit='eV')
-        """
-
-
-
-        unit_code = {'A':0,'angstrom':0,
-                'nm':1,'nanometer':1,
-                'um':2,'micron':2,'micrometer':2}
-        energy_level_code = {'cm-1':0, 'invcm':0,'cm':0,
-                'ev':1,'eV':1,'EV':1,'electronvolt':1,
-                'R':2,'Rydberg':2,'rydberg':2}
-        order_out_code = {'wavelength':0,
-                'multiplet':1}
-        wavelength_unit_code = {'vacuum': 3,
-                'vac+air':4}
-
-        self.request = {
-            "spectra":"H I",
-            "low_wl":minwav,
-            "upp_wl":maxwav,
-            "unit":unit_code[wavelength_unit],
-            "submit":"Retrieve Data",
-            "format":"1", # 0 = html, 1=ascii
-            "line_out":"0",
-            "en_unit":energy_level_code[energy_level_unit],
-            "output":"0",
-            "bibrefs":"1",
-            "page_size":page_size,
-            "show_obs_wl":"1",
-            "show_calc_wl":"1",
-            "order_out":order_out_code[output_order],
-            "max_low_enrg":"",
-            "show_av":wavelength_unit_code[wavelength_type],
-            "max_upp_enrg":"",
-            "tsb_value":"0",
-            "min_str":"",
-            "A_out":"0",
-            "intens_out":"on",
-            "max_str":"",
-            "allowed_out":"1",
-            "forbid_out":"1",
-            "min_accur":"",
-            "min_intens":"",
-            "conf_out":"on",
-            "term_out":"on",
-            "enrg_out":"on",
-            "J_out":"on"}
-            #wavenumber 
-            #"upp_wn":""
-            #"low_wn":""
-
-        page = self.opener.open(url_lines, urllib.urlencode(self.request))
-        pre = pre_re.findall(page.read())[0]
-        table = strip_blanks(pre)
-        Table = asciitable.read(table, Reader=asciitable.FixedWidth,
-                data_start=3)
-
-        return Table
-
-    def query_line_html(self, linename, minwav, maxwav, wavelength_unit='A',
-            energy_level_unit='eV', page_size=15, output_order='wavelength',
-            wavelength_type='vacuum'):
-        """
-        Query the NIST Atomic Lines database
-
-        Example
-        =======
-
-        >>> Q = NISTAtomicLinesQuery()
-        >>> Q.query_line_html('H I',4000,7000,wavelength_unit='A',energy_level_unit='eV')
-        """
-
-        import bs4
-
-
-        unit_code = {'A':0,'angstrom':0,
-                'nm':1,'nanometer':1,
-                'um':2,'micron':2,'micrometer':2}
-        energy_level_code = {'cm-1':0, 'invcm':0,'cm':0,
-                'ev':1,'eV':1,'EV':1,'electronvolt':1,
-                'R':2,'Rydberg':2,'rydberg':2}
-        order_out_code = {'wavelength':0,
-                'multiplet':1}
-        wavelength_unit_code = {'vacuum': 3,
-                'vac+air':4}
-
-        self.request = {
-            "spectra":"H I",
-            "low_wl":minwav,
-            "upp_wl":maxwav,
-            "unit":unit_code[wavelength_unit],
-            "submit":"Retrieve Data",
-            "format":"0", # 0 = html, 1=ascii
-            "line_out":"0",
-            "en_unit":energy_level_code[energy_level_unit],
-            "output":"0",
-            "bibrefs":"1",
-            "page_size":page_size,
-            "show_obs_wl":"1",
-            "show_calc_wl":"1",
-            "order_out":order_out_code[output_order],
-            "max_low_enrg":"",
-            "show_av":wavelength_unit_code[wavelength_type],
-            "max_upp_enrg":"",
-            "tsb_value":"0",
-            "min_str":"",
-            "A_out":"0",
-            "intens_out":"on",
-            "max_str":"",
-            "allowed_out":"1",
-            "forbid_out":"1",
-            "min_accur":"",
-            "min_intens":"",
-            "conf_out":"on",
-            "term_out":"on",
-            "enrg_out":"on",
-            "J_out":"on"}
-            #wavenumber 
-            #"upp_wn":""
-            #"low_wn":""
-
-        page = self.opener.open(url_lines, urllib.urlencode(self.request))
-        tables = bs4.BeautifulSoup(page.read()).findAll('table')[4]
-
-        return parse_nist_table(tables)
-
-def parse_nist_table(table):
-    """
-    Slightly hacky parsing of NIST table into np.recarray
-    """
-    result = []
-    allrows = table.findAll('tr')
-    for row in allrows:
-        result.append([])
-        allcols = row.findAll('td') + row.findAll('th')
-        for col in allcols:
-            thestrings = [s.encode('ascii','ignore').strip(" []()")
-                    for s in col.findAll(text=True)]
-            thetext = ''.join(thestrings)
-            result[-1].append(thetext)
-    header = result.pop(0)
-    header = [x for y in header for x in y.split(',')]
-    def func(lis):
-        pref=""
-        for x in lis:
-            if "lower" in x.lower():
-                pref="Lower"
-            elif "upper" in x.lower():    
-                pref="Upper"
-            if header.count(x)>1:    
-                yield pref+x
-            elif x == "":
-                yield "blank"
-            else:      
-                yield x
-    header = list(func(header))        
-    columns = [np.array(z) for z in zip(*result)]
-    for ii,col in enumerate(columns):
-        try:
-            col[col==''] = np.nan
-            col = col.astype('float')
-            columns[ii] = col
-        except ValueError:
-            pass
-
-    dtypes = zip(header,[z.dtype for z in columns])
-    blankarr = np.recarray(columns[0].size, dtype=dtypes)
-    for H,col in zip(header,columns):
-        blankarr[H] = col
-
-    return blankarr
 
 
 if __name__ == "__main__":
