b498b5d1b317c10692712a530c8ccaa280c537f0
Brian Svoboda <bsvo@lavabit.com>
2013-05-29 01:51:03 -0700

    Now passes all tests for CO

--- astroquery/lamda/core.py
+++ astroquery/lamda/core.py
@@ -129,13 +129,13 @@ class LAMDAQuery(object):
         if query_type == 'erg_levels':
             num_erg_levels = int(data[i + 1])
             col_names = [s.strip() for s in data[i + 2][1:].split('+')]
-            erg_levels = [data[i + j].split() for j in xrange(i,
+            erg_levels = [data[i + 3 + j].split() for j in xrange(0,
                 num_erg_levels)]
             return np.array(erg_levels), col_names
         elif query_type == 'rad_trans':
             num_trans = int(data[i + 1])
             col_names = [s.strip() for s in data[i + 2][1:].split('+')]
-            rad_trans = [data[i + j].split() for j in xrange(i,
+            rad_trans = [data[i + 3 + j].split() for j in xrange(0,
                 num_trans)]
             return np.array(rad_trans), col_names
         elif query_type == 'coll_rates':
@@ -143,7 +143,7 @@ class LAMDAQuery(object):
             num_coll_trans = int(data[i + 3])
             num_coll_temps = int(data[i + 5])
             coll_temps = data[i + 7].split()
-            coll_trans = [data[i + j].split() for j in xrange(i,
+            coll_trans = [data[i + 9 + j].split() for j in xrange(0,
                 num_coll_trans)]
             col_names = ['TRANS', 'UP', 'LOW'] + coll_temps
             return np.array(coll_trans), col_names
