51bde94ffb6bc8a8d810116c171578a5e93b980e
Brian Svoboda <bsvo@lavabit.com>
2013-06-03 02:12:37 -0700

    Added tests from API help page

--- astroquery/sha/tests/test_sha.py
+++ astroquery/sha/tests/test_sha.py
@@ -2,6 +2,10 @@
 from ... import sha
 
 def test_query():
-    sha.query()
+    # Example queries for SHA API help page
+    sha.query(ra=163.6136, dec=-11.784, size=0.5)
+    sha.query(naifid=2003226)
+    sha.query(pid=30080)
+    sha.query(reqkey=21641216)
 
 
