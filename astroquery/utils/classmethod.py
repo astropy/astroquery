6f133ea07dfedb2c60d42cf6302d17213ffc5a0c
Madhura Parikh <madhuraparikh@gmail.com>
2013-06-17 18:48:05 +0530

    Added a brief class description

--- astroquery/utils/classmethod.py
+++ astroquery/utils/classmethod.py
@@ -1,4 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This sub-module has a helper class that can be used to decorate instance
+methods of a class so that they can be called either as a class method
+or as instance methods.
+"""
 import functools
 
 __all__ = ["class_or_instance"]
