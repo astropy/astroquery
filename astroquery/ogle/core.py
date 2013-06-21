f58b041cbd0021bc55559ffd50401472f6aa4317
Brian Svoboda <bsvo@lavabit.com>
2013-06-21 12:55:59 -0700

    First working copy of ogle module

--- astroquery/ogle/core.py
+++ astroquery/ogle/core.py
@@ -11,6 +11,8 @@ __all__ = ['query']
 URL = "http://ogle.astrouw.edu.pl/cgi-ogle/getext.py?"
 ALGORITHMS = ['NG', 'NN']
 QUALITIES = ['GOOD', 'ALL']
+RESULT_DTYPE = ['f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'i8', 'a2',
+                'f8']
 CoordParseError = ValueError('Could not parse `coord` argument.')
 
 def query(coord, algorithm='NG', quality='GOOD', coord_sys='RD'):
@@ -30,43 +32,89 @@ def query(coord, algorithm='NG', quality='GOOD', coord_sys='RD'):
 
     Examples
     --------
-    >>> 
+    Using astropy coordinates:
+    >>> from astropy import coordinates as coord
+    >>> from astropy import units as u
+    >>> co = coord.Galactic(0.0, 3.0, unit=(u.degree, u.degree))
+    >>> t = ogle.query(co)
+    >>> t.pprint()
     """
+    # Inspect inputs
     if algorithm not in ALGORITHMS:
         raise ValueError('Algorithm {0} must be NG or NN'.format(algorithm))
     if quality not in QUALITIES:
         raise ValueError('Quality {0} must be GOOD or ALL'.format(quality))
     # Determine the coord object type and generate list of coordinates
-    if not isinstance(coord, list):
-        try:
-            ra = [coord.fk5.ra.degrees]
-            dec = [coord.fk5.dec.degrees]
-        except:
-            raise CoordParseError
-    elif isinstance(coord, list):
-        try:
-            ra = [co.fk5.ra.degrees for co in np.array(coord)[:,0]]
-            dec = [co.fk5.ra.degrees for co in np.array(coord)[:,1]]
-        except:
-            raise CoordParseError
-    else:
-        raise CoordParseError
+    lon, lat = _parse_coords(coord)
     # Generate payload
     query_header = '# {0} {1} {2}\n'.format(coord_sys, algorithm, quality)
-    sources = '\n'.join(['{0} {1}'.format(ra, dec) for ra, dec in zip(ra, dec)])
+    sources = '\n'.join(['{0} {1}'.format(lon, lat) for lon, lat in zip(lon,
+        lat)])
     file_data = query_header + sources
     files = {'file1': file_data}
     params = {'dnfile':'submit'}
-    pdb.set_trace()
     # Make request
     response = requests.post(URL, params=params, files=files)
     response.raise_for_status()
     # Parse table
     raw_data = response.text.split('\n')[:-2]
-    header = raw_data[0][1:].split()
-    data = np.array([np.fromstring(line, sep=' ') for line in raw_data[1:]])
-    t = Table(data, names=header)
+    header = raw_data[0][1:].encode('ascii').split()
+    data = _parse_raw(raw_data)
+    t = Table(data, names=header, dtypes=RESULT_DTYPE)
     return t
+
+def _parse_coords(coord):
+    """
+    Parse single astropy.coordinates instance, list of astropy.coordinate
+    instances, or 2xN list of coordinate values.
+
+    Parameters
+    ----------
+    coord : list-like
+
+    Returns
+    -------
+    lon : list
+        Longitude coordinate values
+    lat : list
+        Latitude coordinate values
+    """
+    if not isinstance(coord, list):
+        try:
+            ra = [coord.fk5.ra.hours]
+            dec = [coord.fk5.dec.degrees]
+        except:
+            raise CoordParseError
+    elif isinstance(coord, list):
+        try:
+            ra = [co.fk5.ra.hours for co in np.array(coord)[:,0]]
+            dec = [co.fk5.ra.degrees for co in np.array(coord)[:,1]]
+        except:
+            raise CoordParseError
+    else:
+        raise CoordParseError
+    return ra, dec
+
+def _parse_raw(raw_data):
+    """
+    Parse the raw strings returned from the query request and return a list of
+    lists for each column in string format.
+
+    Parameters
+    ----------
+    raw_data : list
+        Raw data from the request formatted to as list of strings for each line
+
+    Returns
+    -------
+    data : list
+        List of lists for each column as strings
+    """
+    # Requests returns unicode encoding, return to ascii
+    data = [line.encode('ascii').split() for line in raw_data[1:]]
+    # Transpose while keeping as list of lists
+    data = map(list, zip(*data))
+    return data
 
 if __name__ == "__main__":
     pass
