27d513aa5e7340239dd32d65917d89237c338145
Brian Svoboda <bsvo@lavabit.com>
2013-07-01 17:31:26 -0700

    Preliminary working version of OGLE query

--- astroquery/ogle/core.py
+++ astroquery/ogle/core.py
@@ -11,23 +11,44 @@ __all__ = ['query']
 URL = "http://ogle.astrouw.edu.pl/cgi-ogle/getext.py?"
 ALGORITHMS = ['NG', 'NN']
 QUALITIES = ['GOOD', 'ALL']
+COORD_SYS = ['RD', 'LB']
 RESULT_DTYPE = ['f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'f8', 'i8', 'a2',
                 'f8']
 
 def query(coord, algorithm='NG', quality='GOOD', coord_sys='RD'):
     """
-    Query the OGLE-III extinction calculator.
+    Query the OGLE-III interstellar extinction calculator.
 
     Parameters
     ----------
-    coord : array-like
-    algorith : string
+    coord : list-like
+        Pointings to evaluate interstellar extinction. Three forms of
+        coordinates may be passed:
+            * single astropy coordinate instance
+            * list-like object (1 x N) of astropy coordinate instances
+            * list-like object (2 x N) of RA/Decs or Glon/Glat as strings or
+              floats.
+    algorithm : string
+        Algorithm to interpolate data for desired coordinate. Valid options:
+            * 'NG': nearest grid point
+            * 'NN': natural neighbor interpolation
     quality : string
+        Quality factor for data. Valid options:
+            * 'All': all points
+            * 'GOOD': QF=0 as described in Nataf et al. (2012).
     coord_sys : string
+        Coordinate system. Valid options:
+            * 'RD': equatorial coordinates
+            * 'LB': Galactic coordinates.
 
     Returns
     -------
     astropy.table.Table
+
+    Raises
+    ------
+    CoordParseError
+        Exception raised for malformed coordinate input
 
     Examples
     --------
@@ -37,12 +58,18 @@ def query(coord, algorithm='NG', quality='GOOD', coord_sys='RD'):
     >>> co = coord.Galactic(0.0, 3.0, unit=(u.degree, u.degree))
     >>> t = ogle.query(co)
     >>> t.pprint()
+      RA/LON   Dec/Lat    A_I  E(V-I) S_E(V-I) R_JKVI   mu    S_mu
+    --------- ---------- ----- ------ -------- ------ ------ ----- ...
+    17.568157 -27.342475 3.126  2.597    0.126 0.3337 14.581 0.212
     """
     # Inspect inputs
     if algorithm not in ALGORITHMS:
         raise ValueError('Algorithm {0} must be NG or NN'.format(algorithm))
     if quality not in QUALITIES:
         raise ValueError('Quality {0} must be GOOD or ALL'.format(quality))
+    if coord_sys not in COORD_SYS:
+        raise ValueError('Coordinate system {0} must be RD or \
+                         LB.'.format(coord_sys)
     # Determine the coord object type and generate list of coordinates
     lon, lat = _parse_coords(coord, coord_sys)
     # Generate payload
@@ -81,20 +108,30 @@ def _parse_coords(coord, coord_sys):
     """
     CoordParseError = ValueError('Could not parse `coord` argument.')
     if not isinstance(coord, list):
+        # single astropy coordinate
         try:
             lon = [coord.fk5.ra.hours]
             lat = [coord.fk5.dec.degrees]
+            return lon, lat
         except:
             raise CoordParseError
     elif isinstance(coord, list):
-        try:
-            lon = [co.fk5.ra.hours for co in np.array(coord)[:,0]]
-            lat = [co.fk5.ra.degrees for co in np.array(coord)[:,1]]
-        except:
+        shape = np.shape(coord)
+        # list-like of values
+        if (len(shape) == 2) & (shape[1] == 2):
+            return coord
+        # list of astropy coordinates
+        elif len(shape) == 1:
+            try:
+                lon = [co.fk5.ra.hours for co in coord]
+                lat = [co.fk5.dec.degrees for co in coord]
+                return lon, lat
+            except:
+                raise CoordParseError
+        else:
             raise CoordParseError
     else:
         raise CoordParseError
-    return lon, lat
 
 def _parse_raw(raw_data):
     """
