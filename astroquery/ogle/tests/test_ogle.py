b38b3b02d5111cc4e94b5d0192c3ceb74edc1150
Brian Svoboda <bsvo@lavabit.com>
2013-07-01 17:39:57 -0700

    Add tests

--- astroquery/ogle/tests/test_ogle.py
+++ astroquery/ogle/tests/test_ogle.py
@@ -1,22 +1,36 @@
 from ... import ogle
 
+from astropy import coordinates as coord
+from astropy import units as u
 
 def test_ogle_single():
     """
-    Test single pointing
+    Test a single pointing using an astropy coordinate instance
     """
-    # Point to the Galactic Center
-    glon = '0.0'
-    glat = '0.0'
-    pass
+    co = coord.GalacticCoordinates(0, 3, unit=(u.degree, u.degree))
+    ogle.query(coord=co)
+    return
 
 def test_ogle_list():
     """
-    Test query on list of sources
+    Test multiple pointings using a list of astropy coordinate instances
     """
-    pass
+    co = coord.GalacticCoordinates(0, 3, unit=(u.degree, u.degree))
+    co_list = [co, co, co]
+    ogle.query(coord=co_list)
+    return
+
+def test_ogle_list():
+    """
+    Test multiple pointings using a list of astropy coordinate instances
+    """
+    co_list = [[0, 0, 0], [3, 3, 3]]
+    ogle.query(coord=co_list)
+    return
 
 if __name__ == '__main__':
     test_ogle_single()
     test_ogle_list()
+    test_ogle_list_values()
+
 
