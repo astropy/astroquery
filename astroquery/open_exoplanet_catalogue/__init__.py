ab2bcc34ba497b369e22d5b064a3876b031adaaf
Hanno Rein <hanno@hanno-rein.de>
2013-07-02 21:06:49 -0400

    Added query_planet routine, renamed query_system routine.

--- astroquery/open_exoplanet_catalogue/__init__.py
+++ astroquery/open_exoplanet_catalogue/__init__.py
@@ -10,5 +10,6 @@ http://openexoplanetcatalogue.com
 from astropy.config import ConfigurationItem
 
 OEC_SERVER = ConfigurationItem('open_exoplanet_catalogue_server', ['https://raw.github.com/hannorein/open_exoplanet_catalogue/master/','http://www.openexoplanetcatalogue.com/open_exoplanet_catalogue/'],'URL of Open Exoplanet Catalogue repository.')
+OEC_META_SERVER = ConfigurationItem('open_exoplanet_catalogue_meta_server', ['https://raw.github.com/hannorein/oec_meta/master/','http://www.openexoplanetcatalogue.com/oec_meta/'],'URL of Open Exoplanet Catalogue repository.')
 
 from .oec_query import *
