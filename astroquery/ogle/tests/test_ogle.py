2d79bd7511a280294936e1ecffd4aaeedc423b4e
Brian Svoboda <bsvo@lavabit.com>
2013-07-07 12:55:42 -0700

    Remove null return statements

--- astroquery/ogle/tests/test_ogle.py
+++ astroquery/ogle/tests/test_ogle.py
@@ -3,13 +3,13 @@ from ... import ogle
 from astropy import coordinates as coord
 from astropy import units as u
 
+
 def test_ogle_single():
     """
     Test a single pointing using an astropy coordinate instance
     """
     co = coord.GalacticCoordinates(0, 3, unit=(u.degree, u.degree))
     ogle.query(coord=co)
-    return
 
 def test_ogle_list():
     """
@@ -18,7 +18,6 @@ def test_ogle_list():
     co = coord.GalacticCoordinates(0, 3, unit=(u.degree, u.degree))
     co_list = [co, co, co]
     ogle.query(coord=co_list)
-    return
 
 def test_ogle_list_values():
     """
@@ -26,7 +25,6 @@ def test_ogle_list_values():
     """
     co_list = [[0, 0, 0], [3, 3, 3]]
     ogle.query(coord=co_list)
-    return
 
 if __name__ == '__main__':
     test_ogle_single()
