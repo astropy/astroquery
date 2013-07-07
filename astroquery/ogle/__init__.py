9622a7dbac8fdaa97121b638aa72d43c446cb71c
Brian Svoboda <bsvo@lavabit.com>
2013-07-07 12:14:42 -0700

    Add reST link for sphinx

--- astroquery/ogle/__init__.py
+++ astroquery/ogle/__init__.py
@@ -6,7 +6,8 @@ OGLE Query Tool
 :Author: Brian Svoboda (svobodb@email.arizona.edu)
 
 This package is for querying interstellar extinction toward the Galactic bulge
-from OGLE-III data hosted at: http://ogle.astrouw.edu.pl/cgi-ogle/getext.py.
+from OGLE-III data
+`hosted at. <http://ogle.astrouw.edu.pl/cgi-ogle/getext.py>`_
 
 Note:
   If you use the data from OGLE please refer to the publication by Nataf et al.
