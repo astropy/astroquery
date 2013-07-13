123ccf3da36378ff1cef0d255d2e876f5dde54f6
Adam Ginsburg <keflavich@gmail.com>
2013-07-13 13:06:44 +0200

    added setup_package (thanks for figuring all this out @jdnc)

---
+++ astroquery/besancon/tests/setup_package.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import os
+
+def get_package_data():
+    paths = [os.path.join('data', '*.txt')]
+    return {'astroquery.besancon.tests': paths}
