0099e4c49deac069eba4e05de52c706bc6d9f504
Madhura Parikh <madhuraparikh@gmail.com>
2013-07-15 10:57:18 +0530

    Added vizier remote tests

---
+++ astroquery/vizier/tests/test_vizier_remote.py
@@ -0,0 +1,42 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from astropy.tests.helper import remote_data
+import astropy.units as u
+import astropy.coordinates as coord
+from ... import vizier
+
+@remote_data
+class TestVizierRemote:
+
+    def test_query_object(self):
+         result = vizier.core.Vizier.query_object("HD 226868", catalog=["NOMAD", "UCAC"])
+         assert isinstance(result, vizier.core.TableList)
+
+
+    def test_query_object_async(self):
+        response = vizier.core.Vizier.query_object_async("HD 226868", catalog=["NOMAD", "UCAC"])
+        assert response is not None
+
+    def test_query_region(self):
+        result = vizier.core.Vizier.query_region(coord.ICRSCoordinates(ra=299.590, dec=35.201, unit=(u.deg, u.deg)),
+                                                     radius=5 * u.deg,
+                                                     catalog=["HIP", "NOMAD", "UCAC"])
+
+        assert isinstance(result, vizier.core.TableList)
+
+    def test_query_region_async(patch_post):
+        response = vizier.core.Vizier.query_region_async(coord.ICRSCoordinates(ra=299.590, dec=35.201, unit=(u.deg, u.deg)),
+                                                     radius=5 * u.deg,
+                                                     catalog=["HIP", "NOMAD", "UCAC"])
+        assert response is not None
+
+
+    def test_query_Vizier_instance(self):
+         v = vizier.core.Vizier(columns=['_RAJ2000', 'DEJ2000','B-V', 'Vmag', 'Plx'],
+                                column_filters={"Vmag":">10"}, keywords=["optical", "xry"])
+         result = v.query_object("HD 226868", catalog=["NOMAD", "UCAC"])
+         assert isinstance(result, vizier.core.TableList)
+         result = v.query_region(coord.ICRSCoordinates(ra=299.590, dec=35.201, unit=(u.deg, u.deg)),
+                                 width="5d0m0s", height="3d0m0s",
+                                 catalog=["NOMAD", "UCAC"])
+         assert isinstance(result, vizier.core.TableList)
