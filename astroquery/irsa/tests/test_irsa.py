3ca2a3a1243403d8257645a8d0cde924e1551518
Leo Singer <leo.singer@ligo.org>
2013-07-21 14:01:53 -0700

    Enable trivial IRSA unit test

--- astroquery/irsa/tests/test_irsa.py
+++ astroquery/irsa/tests/test_irsa.py
@@ -7,10 +7,10 @@ import pytest
 
 # this just wrong.  give up.
 # @pytest.mark.skipif(dv.StrictVersion(np.__version__) <= dv.StrictVersion("1.4.1"))
-# def test_trivial():
-#     """ just make sure it doesn't raise anything 
-#     takes about 3-5 seconds"""
-#     tbl = astroquery.irsa.query_gator_box('pt_src_cat','83.808 -5.391',300)
-# 
-#     assert len(tbl) == 100 # at least, that's what I got...
-#     return tbl
+def test_trivial():
+    """ just make sure it doesn't raise anything
+    takes about 3-5 seconds"""
+    tbl = irsa.query_gator_box('pt_src_cat','83.808 -5.391',300)
+
+    assert len(tbl) == 100 # at least, that's what I got...
+    return tbl
