cfad2d91121a2e37edf3aeeda9cf7aa983bee731
Madhura Parikh <madhuraparikh@gmail.com>
2013-07-30 19:33:57 +0530

    tests not monkeypatched yet but use new code

--- astroquery/ukidss/tests/test_ukidss.py
+++ astroquery/ukidss/tests/test_ukidss.py
@@ -1,29 +1,26 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from ... import ukidss
+import astropy.coordinates as coord
+import astropy.units as u
 
 def test_regression_14():
     """
     Regression test for Issue 14:
     https://github.com/astropy/astroquery/issues/14
     """
-    u = ukidss.UKIDSSQuery()
-    u.get_images_radius(281.9027, -1.9425, 1.0, filter='all', directory='raw', save=True, n_concurrent=8)
+
+    ukidss.core.Ukidss.get_images(coord.GalacticCoordinates(l=281.9027, b=-1.9425, unit=(u.deg, u.deg)), radius=1 * u.arcmin, waveband='all')
 
 def test_ukidss_catalog():
     """
     Copy of doctests for catalog query
     """
     # not necessary to specify database, but done so that changes don't break tests
-    R = ukidss.UKIDSSQuery(programmeID='GPS',database='UKIDSSDR7PLUS')
-    data = R.get_catalog_gal(10.625,-0.38,radius=0.1)
-    bintable = data[0][1]
-    assert bintable.size == 5130
+    ukidss.core.Ukidss.query_region(coord.GalacticCoordinates(l=10.625,b=-0.38,unit=(u.deg, u.deg)), radius=0.1*u.arcmin)
 
 def test_ukidss_get_image_gal():
-    R = ukidss.UKIDSSQuery()
-    fitsfile = R.get_image_gal(10.5,0.0)
 
     # get UWISH2 data (as per http://astro.kent.ac.uk/uwish2/main.html)
-    R.database='U09B8v20120403'
-    R.login(username='U09B8',password='uwish2',community='nonSurvey')
-    R.get_image_gal(49.489,-0.27,frametype='leavstack',size=5,filter='H2',save=False)
+
+    c = ukidss.core.Ukidss(username='U09B8',password='uwish2',community='nonSurvey', database='U09B8v20120403')
+    c.get_images(coord.GalacticCoordinates(l=49.489, b=-0.27, unit=(u.deg, u.deg)), frame_type='leavstack', image_width=5*u.arcmin, waveband='H2')
