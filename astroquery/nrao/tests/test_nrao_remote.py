aae92268223544c421d7bcd3096a5c5d8ca249e8
Madhura Parikh <madhuraparikh@gmail.com>
2013-08-02 23:46:48 +0530

    Added remote tests to nrao

---
+++ astroquery/nrao/tests/test_nrao_remote.py
@@ -0,0 +1,26 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function
+
+import astropy.coordinates as coord
+import astropy.units as u
+from astropy.tests.helper import pytest, remote_data
+
+from ...import nrao
+
+@remote_data
+class TestNrao:
+
+    def test_get_images_async(self):
+        image_list = nrao.core.Nrao.get_images_async(coord.GalacticCoordinates
+                                                     (l=49.489, b=-0.37, unit=(u.deg, u.deg)),
+                                                     band="K")
+        assert len(image_list) > 0
+
+    def test_get_images(self):
+        images = nrao.core.Nrao.get_images("3c 273", radius=2*u.arcsec, band="K", max_rms=500)
+        assert images is not None
+
+    def test_get_image_list(self):
+         image_urls = nrao.core.Nrao.get_image_list("05h34m31.94s 22d00m52.2s",
+                                                    radius='0d0m0.6s', max_rms=500)
+         assert len(image_urls) > 0
