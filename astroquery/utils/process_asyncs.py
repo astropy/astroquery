738560deab7145eb008ce73ebee1110e0d2aea20
Adam Ginsburg <keflavich@gmail.com>
2013-08-04 17:12:56 -0600

    created process_asyncs tool: this should allow us to *just* write async methods and auto-generate the non-async versions

--- astroquery/utils/process_asyncs.py
+++ astroquery/utils/process_asyncs.py
@@ -3,26 +3,30 @@
 Process all "async" methods into direct methods.
 """
 
-import inspect
-from types import MethodType
 from class_or_instance import class_or_instance
 
 def process_asyncs(cls):
     """
     Convert all query_x_async methods to query_x methods
     """
-    methods = inspect.getmembers(cls)
+    methods = cls.__dict__.keys()
     for k in methods:
-        methodname = k.replace("_async","")
-        if 'async' in k and methodname not in methods:
+        newmethodname = k.replace("_async","")
+        if 'async' in k and newmethodname not in methods:
+
+            async_method = getattr(cls,k)
 
             @class_or_instance
-            def method(self, verbose=False, *args, **kwargs):
-                response = self.__dict__[k](*args,**kwargs)
+            def newmethod(self, *args, **kwargs):
+                if 'verbose' in kwargs:
+                    verbose = kwargs.pop('verbose')
+                else:
+                    verbose = False
+                response = async_method(*args,**kwargs)
                 result = self._parse_result(response, verbose=verbose)
                 return result
 
-            method.__docstr__ = ("Returns a table object.\n" +
-                    cls.__dict__[k].__docstr__)
+            newmethod.fn.__doc__ = ("Returns a table object.\n" +
+                                    async_method.__doc__)
 
-            cls.__dict__[methodname] = MethodType(method, None, cls)
+            setattr(cls,newmethodname,newmethod)
