2c2a79c799e46c13968832fb6f62a9b677ea2868
Madhura Parikh <madhuraparikh@gmail.com>
2013-08-04 19:51:54 +0530

    minor fix in init

--- astroquery/ukidss/__init__.py
+++ astroquery/ukidss/__init__.py
@@ -18,6 +18,5 @@ UKIDSS_SERVER = ConfigurationItem('ukidss_server', ["http://surveys.roe.ac.uk:80
                                'Name of the UKIDSS mirror to use.')
 UKIDSS_TIMEOUT = ConfigurationItem('timeout', 60, 'time limit for connecting to UKIDSS server')
 
-UKIDSS_SERVER
 
 from .core import *
