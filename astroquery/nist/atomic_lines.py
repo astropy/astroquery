e537aa65e77d62a501d5282827ed47d020911d5d
Madhura Parikh <madhuraparikh@gmail.com>
2013-08-05 15:20:34 +0530

    Added basic code

--- astroquery/nist/atomic_lines.py
+++ astroquery/nist/atomic_lines.py
@@ -1,18 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import urllib
-import urllib2
+from __future__ import print_function
+
 import re
+
+import astropy.units as u
 try:
     import astropy.io.ascii as asciitable
 except ImportError:
     import asciitable
-import numpy as np
 
-url_lines = "http://physics.nist.gov/cgi-bin/ASD/lines1.pl"
-
-# extract stuff within <pre> tag
-pre_re = re.compile("<pre>(.*)</pre>",flags=re.DOTALL)
-numbersletters = re.compile("[0-9A-Za-z]")
+from ..query import BaseQuery
+from ..utils.class_or_instance import class_or_instance
+from ..utils import commons
 
 def strip_blanks(table):
     """
@@ -20,206 +19,111 @@ def strip_blanks(table):
 
     returns a single string joined by \n newlines
     """
+    numbersletters = re.compile("[0-9A-Za-z]")
     if isinstance(table,str):
         table = table.split('\n')
     table = [line for line in table if numbersletters.search(line)]
     return "\n".join(table)
 
-class NISTAtomicLinesQuery(object):
-    def __init__(self):
-        self.opener = urllib2.build_opener()
-
-    def query_line_ascii(self, linename, minwav, maxwav, wavelength_unit='A',
-            energy_level_unit='eV', page_size=15, output_order='wavelength',
-            wavelength_type='vacuum'):
-        """
-        Query the NIST Atomic Lines database
-
-        Example
-        =======
-
-        >>> Q = NISTAtomicLinesQuery()
-        >>> Q.query_line_ascii('H I',4000,7000,wavelength_unit='A',energy_level_unit='eV')
-        """
-
-
-
-        unit_code = {'A':0,'angstrom':0,
-                'nm':1,'nanometer':1,
-                'um':2,'micron':2,'micrometer':2}
-        energy_level_code = {'cm-1':0, 'invcm':0,'cm':0,
+class Nist(BaseQuery):
+    URL = "http://physics.nist.gov/cgi-bin/ASD/lines1.pl"
+    TIMEOUT = 30
+    unit_code = {'Angstrom':0,
+                 'nm': 1,
+                 'um': 2}
+    energy_level_code = {'cm-1':0, 'invcm':0,'cm':0,
                 'ev':1,'eV':1,'EV':1,'electronvolt':1,
                 'R':2,'Rydberg':2,'rydberg':2}
-        order_out_code = {'wavelength':0,
+    order_out_code = {'wavelength':0,
                 'multiplet':1}
-        wavelength_unit_code = {'vacuum': 3,
+    wavelength_unit_code = {'vacuum': 3,
                 'vac+air':4}
 
-        self.request = {
-            "spectra":"H I",
-            "low_wl":minwav,
-            "upp_wl":maxwav,
-            "unit":unit_code[wavelength_unit],
-            "submit":"Retrieve Data",
-            "format":"1", # 0 = html, 1=ascii
-            "line_out":"0",
-            "en_unit":energy_level_code[energy_level_unit],
-            "output":"0",
-            "bibrefs":"1",
-            "page_size":page_size,
-            "show_obs_wl":"1",
-            "show_calc_wl":"1",
-            "order_out":order_out_code[output_order],
-            "max_low_enrg":"",
-            "show_av":wavelength_unit_code[wavelength_type],
-            "max_upp_enrg":"",
-            "tsb_value":"0",
-            "min_str":"",
-            "A_out":"0",
-            "intens_out":"on",
-            "max_str":"",
-            "allowed_out":"1",
-            "forbid_out":"1",
-            "min_accur":"",
-            "min_intens":"",
-            "conf_out":"on",
-            "term_out":"on",
-            "enrg_out":"on",
-            "J_out":"on"}
-            #wavenumber 
-            #"upp_wn":""
-            #"low_wn":""
+    @class_or_instance
+    def query(self, linename, minwav, maxwav, energy_level_unit='eV', output_order='wavelength',
+              wavelength_type='vacuum', get_query_payload=False):
 
-        page = self.opener.open(url_lines, urllib.urlencode(self.request))
-        pre = pre_re.findall(page.read())[0]
+        response = self.query_async(linename, minwav, maxwav,
+                                    energy_level_unit=energy_level_unit,
+                                    output_order=output_order,
+                                    wavelength_type=wavelength_type,
+                                    get_query_payload=get_query_payload)
+        if get_query_payload:
+            return response
+
+        result = self._parse_result(response)
+        return result
+
+    @class_or_instance
+    def query_async(self, linename, minwav, maxwav, energy_level_unit='eV', output_order='wavelength',
+                    wavelength_type='vacuum', get_query_payload=False):
+
+        request_payload = self._args_to_payload(linename, minwav, maxwav,
+                                                energy_level_unit=energy_level_unit,
+                                                output_order=output_order,
+                                                wavelength_type=wavelength_type)
+        if get_query_payload:
+            return request_payload
+
+        response = commons.send_request(Nist.URL, request_payload, Nist.TIMEOUT, request_type='GET')
+        return response
+
+    @class_or_instance
+    def _args_to_payload(self, *args, **kwargs):
+        request_payload = {}
+        request_payload["spectra"] = args[0]
+        (min_wav, max_wav, wav_unit) = _parse_wavelength(args[1], args[2])
+        request_payload["low_wl"] = min_wav
+        request_payload["upp_wl"] = max_wav
+        request_payload["unit"] = wav_unit
+        request_payload["submit"] = "Retrieve Data"
+        request_payload["format"] = 1 # ascii
+        request_payload["line_out"] = 0 # All lines
+        request_payload["en_unit"] = Nist.energy_level_code[kwargs["energy_level_unit"]]
+        request_payload["output"] = 0 # entirely rather than pagewise
+        request_payload["bibrefs"] = 1
+        request_payload["show_obs_wl"] = 1
+        request_payload["show_calc_wl"] = 1
+        request_payload["order_out"] = Nist.order_out_code[kwargs['output_order']]
+        request_payload["max_low_enrg"] = ""
+        request_payload["show_av"] = Nist.wavelength_unit_code[kwargs['wavelength_type']]
+        request_payload["max_upp_enrg"] = ""
+        request_payload["tsb_value"] = 0
+        request_payload["min_str"] = ""
+        request_payload["A_out"] = 0
+        request_payload["intens_out"] = "on"
+        request_payload["max_str"] = ""
+        request_payload["allowed_out"] = 1
+        request_payload["forbid_out"] = 1
+        request_payload["min_accur"] = ""
+        request_payload["min_intens"] = ""
+        request_payload["conf_out"] = "on"
+        request_payload["term_out"] = "on"
+        request_payload["enrg_out"] = "on"
+        request_payload["J_out"] = "on"
+        request_payload["page_size"] = 15
+        return request_payload
+
+    @class_or_instance
+    def _parse_result(self, response):
+        pre_re = re.compile("<pre>(.*)</pre>",flags=re.DOTALL)
+        pre = pre_re.findall(response.content)[0]
         table = strip_blanks(pre)
         Table = asciitable.read(table, Reader=asciitable.FixedWidth,
                 data_start=3)
-
         return Table
 
-    def query_line_html(self, linename, minwav, maxwav, wavelength_unit='A',
-            energy_level_unit='eV', page_size=15, output_order='wavelength',
-            wavelength_type='vacuum'):
-        """
-        Query the NIST Atomic Lines database
 
-        Example
-        =======
+def _parse_wavelength(min_wav, max_wav):
 
-        >>> Q = NISTAtomicLinesQuery()
-        >>> Q.query_line_html('H I',4000,7000,wavelength_unit='A',energy_level_unit='eV')
-        """
-
-        import bs4
-
-
-        unit_code = {'A':0,'angstrom':0,
-                'nm':1,'nanometer':1,
-                'um':2,'micron':2,'micrometer':2}
-        energy_level_code = {'cm-1':0, 'invcm':0,'cm':0,
-                'ev':1,'eV':1,'EV':1,'electronvolt':1,
-                'R':2,'Rydberg':2,'rydberg':2}
-        order_out_code = {'wavelength':0,
-                'multiplet':1}
-        wavelength_unit_code = {'vacuum': 3,
-                'vac+air':4}
-
-        self.request = {
-            "spectra":"H I",
-            "low_wl":minwav,
-            "upp_wl":maxwav,
-            "unit":unit_code[wavelength_unit],
-            "submit":"Retrieve Data",
-            "format":"0", # 0 = html, 1=ascii
-            "line_out":"0",
-            "en_unit":energy_level_code[energy_level_unit],
-            "output":"0",
-            "bibrefs":"1",
-            "page_size":page_size,
-            "show_obs_wl":"1",
-            "show_calc_wl":"1",
-            "order_out":order_out_code[output_order],
-            "max_low_enrg":"",
-            "show_av":wavelength_unit_code[wavelength_type],
-            "max_upp_enrg":"",
-            "tsb_value":"0",
-            "min_str":"",
-            "A_out":"0",
-            "intens_out":"on",
-            "max_str":"",
-            "allowed_out":"1",
-            "forbid_out":"1",
-            "min_accur":"",
-            "min_intens":"",
-            "conf_out":"on",
-            "term_out":"on",
-            "enrg_out":"on",
-            "J_out":"on"}
-            #wavenumber 
-            #"upp_wn":""
-            #"low_wn":""
-
-        page = self.opener.open(url_lines, urllib.urlencode(self.request))
-        tables = bs4.BeautifulSoup(page.read()).findAll('table')[4]
-
-        return parse_nist_table(tables)
-
-def parse_nist_table(table):
-    """
-    Slightly hacky parsing of NIST table into np.recarray
-    """
-    result = []
-    allrows = table.findAll('tr')
-    for row in allrows:
-        result.append([])
-        allcols = row.findAll('td') + row.findAll('th')
-        for col in allcols:
-            thestrings = [s.encode('ascii','ignore').strip(" []()")
-                    for s in col.findAll(text=True)]
-            thetext = ''.join(thestrings)
-            result[-1].append(thetext)
-    header = result.pop(0)
-    header = [x for y in header for x in y.split(',')]
-    def func(lis):
-        pref=""
-        for x in lis:
-            if "lower" in x.lower():
-                pref="Lower"
-            elif "upper" in x.lower():    
-                pref="Upper"
-            if header.count(x)>1:    
-                yield pref+x
-            elif x == "":
-                yield "blank"
-            else:      
-                yield x
-    header = list(func(header))        
-    columns = [np.array(z) for z in zip(*result)]
-    for ii,col in enumerate(columns):
-        try:
-            col[col==''] = np.nan
-            col = col.astype('float')
-            columns[ii] = col
-        except ValueError:
-            pass
-
-    dtypes = zip(header,[z.dtype for z in columns])
-    blankarr = np.recarray(columns[0].size, dtype=dtypes)
-    for H,col in zip(header,columns):
-        blankarr[H] = col
-
-    return blankarr
-    #try:
-    #    tblarr = np.recarray(columns, names=header, dtype=dtypes)
-    #    return tblarr
-    #except:
-    #    return header,result
-
-
-if __name__ == "__main__":
-    pass
-    #test query
-    Q = NISTAtomicLinesQuery()
-    NIST_Table = Q.query_line_html('H I',4000,7000,wavelength_unit='A',energy_level_unit='eV')
-    #T = Q.query_line_ascii('H I',4000,7000,wavelength_unit='A',energy_level_unit='eV')
+    max_wav = max_wav.to(min_wav.unit)
+    wav_unit = min_wav.unit.to_string()
+    if wav_unit not in Nist.unit_code:
+        max_wav = max_wav.to(u.angstrom).value
+        min_wav = min_wav.to(u.angstrom).value
+        wav_unit = Nist.unit_code['Angstrom']
+    else:
+        max_wav = max_wav.value
+        min_wav = min_wav.value
+        wav_unit = Nist.unit_code[wav_unit]
+    return (min_wav, max_wav, wav_unit)
