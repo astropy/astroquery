f6428452c88f931cf18732538d59f07fcd9ca35c
Madhura Parikh <madhuraparikh@gmail.com>
2013-08-07 09:43:54 +0530

    further restructuring from nrao to nvas

--- astroquery/nvas/tests/setup_package.py
+++ astroquery/nvas/tests/setup_package.py
@@ -4,4 +4,4 @@ def get_package_data():
     paths_test = [os.path.join('data', '*.imfits'),
                   os.path.join('data', '*.html')]
 
-    return {'astroquery.nrao.tests': paths_test}
+    return {'astroquery.nvas.tests': paths_test}
--- astroquery/nvas/tests/test_nvas_remote.py
+++ astroquery/nvas/tests/test_nvas_remote.py
@@ -3,24 +3,24 @@ from __future__ import print_function
 
 import astropy.coordinates as coord
 import astropy.units as u
-from astropy.tests.helper import pytest, remote_data
+from astropy.tests.helper import remote_data
 
-from ...import nrao
+from ...import nvas
 
 @remote_data
-class TestNrao:
+class TestNvas:
 
     def test_get_images_async(self):
-        image_list = nrao.core.Nrao.get_images_async(coord.GalacticCoordinates
+        image_list = nvas.core.Nvas.get_images_async(coord.GalacticCoordinates
                                                      (l=49.489, b=-0.37, unit=(u.deg, u.deg)),
                                                      band="K")
         assert len(image_list) > 0
 
     def test_get_images(self):
-        images = nrao.core.Nrao.get_images("3c 273", radius=2*u.arcsec, band="K", max_rms=500)
+        images = nvas.core.Nvas.get_images("3c 273", radius=2*u.arcsec, band="K", max_rms=500)
         assert images is not None
 
     def test_get_image_list(self):
-         image_urls = nrao.core.Nrao.get_image_list("05h34m31.94s 22d00m52.2s",
+         image_urls = nvas.core.Nvas.get_image_list("05h34m31.94s 22d00m52.2s",
                                                     radius='0d0m0.6s', max_rms=500)
          assert len(image_urls) > 0
