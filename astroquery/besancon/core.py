8854c8c458232f61ef93b7d9b9050f58daa0b0c4
Adam Ginsburg <keflavich@gmail.com>
2013-08-10 17:39:12 -0500

    Besancon refactor

--- astroquery/besancon/core.py
+++ astroquery/besancon/core.py
@@ -8,10 +8,15 @@ import re
 import astropy.utils.data as aud
 import astropy.io.ascii as asciireader
 from . import BESANCON_DOWNLOAD_URL, BESANCON_MODEL_FORM, BESANCON_PING_DELAY
-import requests
 import urllib2 # only needed for urllib2.URLError
 
-__all__ = ['get_besancon_model_file','request_besancon']
+from ..query import BaseQuery
+from ..utils.class_or_instance import class_or_instance
+from ..utils import commons
+from ..utils import prepend_docstr_noreturns
+from ..utils import async_to_sync
+
+__all__ = ['Besancon']
 
 keyword_defaults = {
     'rinf':0.000000,
@@ -56,21 +61,252 @@ keyword_defaults = {
     'outmod':"",
 }
 
-# Since these are configuration options, they should probably be used directly
-# rather than re-stored as local variables.  Then again, we need to refactor
-# this whole project to be class-based, so they should be set for class
-# instances.
-url_download = BESANCON_DOWNLOAD_URL()
-url_request = BESANCON_MODEL_FORM()
-ping_delay = BESANCON_PING_DELAY()
-# sample file:  1340900648.230224.resu
-result_re = re.compile("[0-9]{10}\.[0-9]{6}\.resu")
+colors_limits = {"J-H":(-99,99),"H-K":(-99,99),"J-K":(-99,99),"V-K":(-99,99)}
+mag_limits = {'U':(-99,99), 'B':(-99,99), 'V':(-5,20), 'R':(-99,99),
+              'I':(-99,99), 'J':(-99,99), 'H':(-99,99), 'K':(-99,99), 'L':(-99,99)}
+mag_order = "U","B","V","R","I","J","H","K","L"
+
+
+@async_to_sync
+class Besancon(BaseQuery):
+
+    # Since these are configuration options, they should probably be used directly
+    # rather than re-stored as local variables.  Then again, we need to refactor
+    # this whole project to be class-based, so they should be set for class
+    # instances.
+    url_download = BESANCON_DOWNLOAD_URL()
+    url_request = BESANCON_MODEL_FORM()
+    ping_delay = BESANCON_PING_DELAY()
+    # sample file name:  1340900648.230224.resu
+    result_re = re.compile("[0-9]{10}\.[0-9]{6}\.resu")
+
+    def __init__(self, email=None):
+        self.email = email
+
+
+    @class_or_instance
+    def get_besancon_model_file(self, filename, verbose=True, save=True,
+                                savename=None, overwrite=True, timeout=5.0):
+        """
+        Download a Besancon model from the website
+
+        Parameters
+        ----------
+        filename : string
+            The besancon filename, with format ##########.######.resu
+        verbose : bool
+            Print details about the download process
+        save : bool
+            Save the table after acquiring it?
+        savename : None or string
+            If not specified, defaults to the .resu table name
+        overwrite : bool
+            Overwrite the file if it exists?  Defaults to True because the .resu
+            tables should have unique names by default, so there's little risk of
+            accidentally overwriting important information
+        timeout : float
+            Amount of time to wait after pinging the server to see if a file is
+            present.  Default 5s, which is probably reasonable.
+        """
+
+        url = self.url_download+"/"+filename
+
+        elapsed_time = 0
+        t0 = time.time()
+
+        if verbose:
+            sys.stdout.write("Awaiting Besancon file...\n")
+        while 1:
+            if verbose:
+                sys.stdout.write(u"\r")
+                sys.stdout.flush()
+            try:
+                #U = requests.get(url,timeout=timeout,stream=True)
+                # TODO: add timeout= keyword to get_readable_fileobj (when PR https://github.com/astropy/astropy/pull/1258 is merged)
+                with aud.get_readable_fileobj(url, cache=True) as f:
+                    results = f.read()
+                break
+            except urllib2.URLError:
+                if verbose:
+                    sys.stdout.write(u"Waiting %0.1fs for model to finish (elapsed wait time %0.1fs, total wait time %0.1f)\r" % (self.ping_delay,elapsed_time,time.time()-t0))
+                    sys.stdout.flush()
+                time.sleep(self.ping_delay)
+                elapsed_time += self.ping_delay
+                continue
+            except socket.timeout:
+                if verbose:
+                    sys.stdout.write(u"Waiting %0.1fs for model to finish (elapsed wait time %0.1fs, total wait time %0.1f)\r" % (self.ping_delay,elapsed_time,time.time()-t0))
+                    sys.stdout.flush()
+                time.sleep(self.ping_delay)
+                elapsed_time += self.ping_delay
+                continue
+
+
+        if save:
+            if savename is None:
+                savename = filename
+            if not overwrite and os.path.exists(savename):
+                raise IOError("File %s already exists." % savename)
+            outf = open(savename,'w')
+            print >>outf,results
+            outf.close()
+
+        return parse_besancon_model_string(results)
+
+    @class_or_instance
+    def _parse_result(self, response, verbose=False, retrieve_file=False):
+
+        if verbose:
+            print("Loading request from Besancon server ...")
+
+        # keep the text stored for possible later use
+        with aud.get_readable_fileobj(response.raw) as f:
+            text = f.read()
+        try:
+            filename = self.result_re.search(text).group()
+        except AttributeError: # if there are no matches
+            errors = parse_errors(text)
+            raise ValueError("Errors: "+"\n".join(errors))
+
+        if verbose:
+            print("File is %s and can be aquired from %s" % (filename, self.url_download+'/'+filename))
+
+        if retrieve_file:
+            return self.get_besancon_model_file(filename)
+        else:
+            return filename
+            
+    @class_or_instance
+    def _parse_args(self, glon, glat, email=None, smallfield=True, extinction=0.7,
+                    area=0.0001, verbose=True, clouds=None,
+                    absmag_limits=(-7,15), mag_limits=copy.copy(mag_limits),
+                    colors_limits=copy.copy(colors_limits), retrieve_file=True,
+                    **kwargs):
+        """
+        Perform a query on the Besancon model of the galaxy
+        http://model.obs-besancon.fr/
+
+        Parameters
+        ----------
+        email : string
+            A valid e-mail address to send the report of completion to
+        glon : float
+        glat : float
+            Galactic latitude and longitude at the center
+        smallfield : bool
+            Small field (True) or Large Field (False)
+            LARGE FIELD NOT SUPPORTED YET
+        extinction : float
+            Extinction per kpc in A_V
+        area : float
+            Area in square degrees
+        absmag_limits : (float,float)
+            Absolute magnitude lower,upper limits
+        colors_limits : dict of (float,float)
+            Should contain 4 elements listing color differences in the valid bands, e.g.:
+                {"J-H":(99,-99),"H-K":(99,-99),"J-K":(99,-99),"V-K":(99,-99)}
+        mag_limits = dict of (float,float)
+            Lower and Upper magnitude difference limits for each magnitude band
+            U B V R I J H K L
+        clouds : list of 2-tuples
+            Up to 25 line-of-sight clouds can be specified in pairs of (A_V,
+            distance in pc)
+        verbose : bool
+            Print out extra error messages?
+        retrieve_file : bool
+            If True, will try to retrieve the file every 30s until it shows up.
+            Otherwise, just returns the filename (the job is still executed on
+            the remote server, though)
+        kwargs : dict
+            Can override any argument in the request if you know the name of the
+            POST keyword.
+
+        Returns
+        -------
+        Either the filename or the table depending on whether 'retrieve file' is
+        specified
+        """
+        if email is None and hasattr(self,'email'):
+            email = self.email
+        if email is None or not isinstance(email,str) or not commons.validate_email(email):
+            raise ValueError("Must specify a valid e-mail address.")
+
+        # create a new keyword dict based on inputs + defaults
+        kwd = copy.copy(keyword_defaults)
+        for key,val in kwargs.iteritems():
+            if key in keyword_defaults:
+                kwd[key] = val
+            elif verbose:
+                print "Skipped invalid key %s" % key
+
+        kwd['kleg'] = 1 if smallfield else 2
+        if not smallfield:
+            raise NotImplementedError
+
+        kwd['adif'] = extinction
+        kwd['soli'] = area
+        kwd['oo'][0] = absmag_limits[0]
+        kwd['ff'][0] = absmag_limits[1]
+
+        for ii,(key,val) in enumerate(colors_limits.items()):
+            if key[0] in mag_order and key[1] == '-' and key[2] in mag_order:
+                kwd['colind'][ii] = key
+                kwd['oo'][ii+9] = val[0]
+                kwd['ff'][ii+9] = val[1]
+            else:
+                raise ValueError('Invalid color %s' % key)
+
+        for (key,val) in mag_limits.iteritems():
+            if key in mag_order:
+                kwd['band0'][mag_order.index(key)] = val[0]
+                kwd['bandf'][mag_order.index(key)] = val[1]
+            else:
+                raise ValueError('Invalid band %s' % key)
+
+        if clouds is not None:
+            for ii,(AV,di) in enumerate(clouds):
+                kwd[AV][ii] = AV
+                kwd[di][ii] = di
+
+        # parse the default dictionary
+        request_data = parse_besancon_dict(keyword_defaults)
+
+        # an e-mail address is required
+        request_data.append(('email',email))
+        request_data = dict(request_data)
+
+        return request_data
+
+    @class_or_instance
+    @prepend_docstr_noreturns("\n"+_parse_args.__doc__)
+    def query_async(self, *args, **kwargs):
+        """
+        Returns
+        -------
+        response : `requests.Response` object
+            The response of the HTTP request.
+        """
+        data_payload = self._parse_args(*args, **kwargs)
+        if kwargs.get('get_query_payload'):
+            return data_payload
+
+        response = commons.send_request(
+            self.QUERY_URL,
+            data_payload,
+            self.TIMEOUT,
+            stream=True)
+        return response
+
 
 def parse_besancon_dict(bd):
     """
     Turn a dict like default_keys into a list of tuples (must be a list of
     tuples because there are some repeated entries, which dictionaries do not
     support)
+
+    .. todo::
+        In the future, a better way to do this is to make each dict entry a list;
+        requests knows how to deal with this properly
     """
 
     http_dict = []
@@ -110,196 +346,6 @@ def parse_errors(text):
     return error_list
 
     
-colors_limits = {"J-H":(-99,99),"H-K":(-99,99),"J-K":(-99,99),"V-K":(-99,99)}
-mag_limits = {'U':(-99,99), 'B':(-99,99), 'V':(-5,20), 'R':(-99,99),
-              'I':(-99,99), 'J':(-99,99), 'H':(-99,99), 'K':(-99,99), 'L':(-99,99)}
-mag_order = "U","B","V","R","I","J","H","K","L"
-
-def request_besancon(email, glon, glat, smallfield=True, extinction=0.7,
-                     area=0.0001, verbose=True, clouds=None,
-                     absmag_limits=(-7,15), mag_limits=copy.copy(mag_limits),
-                     colors_limits=copy.copy(colors_limits),
-                     retrieve_file=True, **kwargs):
-    """
-    Perform a query on the Besancon model of the galaxy
-    http://model.obs-besancon.fr/
-
-    Parameters
-    ----------
-    email : string
-        A valid e-mail address to send the report of completion to
-    glon : float
-    glat : float
-        Galactic latitude and longitude at the center
-    smallfield : bool
-        Small field (True) or Large Field (False)
-        LARGE FIELD NOT SUPPORTED YET
-    extinction : float
-        Extinction per kpc in A_V
-    area : float
-        Area in square degrees
-    absmag_limits : (float,float)
-        Absolute magnitude lower,upper limits
-    colors_limits : dict of (float,float)
-        Should contain 4 elements listing color differences in the valid bands, e.g.:
-            {"J-H":(99,-99),"H-K":(99,-99),"J-K":(99,-99),"V-K":(99,-99)}
-    mag_limits = dict of (float,float)
-        Lower and Upper magnitude difference limits for each magnitude band
-        U B V R I J H K L
-    clouds : list of 2-tuples
-        Up to 25 line-of-sight clouds can be specified in pairs of (A_V,
-        distance in pc)
-    verbose : bool
-        Print out extra error messages?
-    retrieve_file : bool
-        If True, will try to retrieve the file every 30s until it shows up.
-        Otherwise, just returns the filename (the job is still executed on
-        the remote server, though)
-    kwargs : dict
-        Can override any argument in the request if you know the name of the
-        POST keyword.
-
-    Returns
-    -------
-    Either the filename or the table depending on whether 'retrieve file' is
-    specified
-
-    """
-
-    # create a new keyword dict based on inputs + defaults
-    kwd = copy.copy(keyword_defaults)
-    for key,val in kwargs.iteritems():
-        if key in keyword_defaults:
-            kwd[key] = val
-        elif verbose:
-            print "Skipped invalid key %s" % key
-
-    kwd['kleg'] = 1 if smallfield else 2
-    if not smallfield:
-        raise NotImplementedError
-
-    kwd['adif'] = extinction
-    kwd['soli'] = area
-    kwd['oo'][0] = absmag_limits[0]
-    kwd['ff'][0] = absmag_limits[1]
-
-    for ii,(key,val) in enumerate(colors_limits.items()):
-        if key[0] in mag_order and key[1] == '-' and key[2] in mag_order:
-            kwd['colind'][ii] = key
-            kwd['oo'][ii+9] = val[0]
-            kwd['ff'][ii+9] = val[1]
-        else:
-            raise ValueError('Invalid color %s' % key)
-
-    for (key,val) in mag_limits.iteritems():
-        if key in mag_order:
-            kwd['band0'][mag_order.index(key)] = val[0]
-            kwd['bandf'][mag_order.index(key)] = val[1]
-        else:
-            raise ValueError('Invalid band %s' % key)
-
-    if clouds is not None:
-        for ii,(AV,di) in enumerate(clouds):
-            kwd[AV][ii] = AV
-            kwd[di][ii] = di
-
-    # parse the default dictionary
-    request_data = parse_besancon_dict(keyword_defaults)
-
-    # an e-mail address is required
-    request_data.append(('email',email))
-    request_data = dict(request_data)
-
-    # load the URL as text
-    response = requests.post(url_request, data=request_data, stream=True)
-
-    if verbose:
-        print "Loading request from Besancon server ..."
-
-    # keep the text stored for possible later use
-    with aud.get_readable_fileobj(response.raw) as f:
-        text = f.read()
-    try:
-        filename = result_re.search(text).group()
-    except AttributeError: # if there are no matches
-        errors = parse_errors(text)
-        raise ValueError("Errors: "+"\n".join(errors))
-
-    if verbose:
-        print "File is %s and can be aquired from %s" % (filename, url_download+filename)
-
-    if retrieve_file:
-        return get_besancon_model_file(filename)
-    else:
-        return filename
-
-def get_besancon_model_file(filename, verbose=True, save=True, savename=None,
-                            overwrite=True, timeout=5.0):
-    """
-    Download a Besancon model from the website
-
-    Parameters
-    ----------
-    filename : string
-        The besancon filename, with format ##########.######.resu
-    verbose : bool
-        Print details about the download process
-    save : bool
-        Save the table after acquiring it?
-    savename : None or string
-        If not specified, defaults to the .resu table name
-    overwrite : bool
-        Overwrite the file if it exists?  Defaults to True because the .resu
-        tables should have unique names by default, so there's little risk of
-        accidentally overwriting important information
-    timeout : float
-        Amount of time to wait after pinging the server to see if a file is
-        present.  Default 5s, which is probably reasonable.
-    """
-
-    url = url_download+filename
-
-    elapsed_time = 0
-    t0 = time.time()
-
-    if verbose:
-        sys.stdout.write("Awaiting Besancon file...\n")
-    while 1:
-        if verbose:
-            sys.stdout.write(u"\r")
-            sys.stdout.flush()
-        try:
-            #U = requests.get(url,timeout=timeout,stream=True)
-            # TODO: add timeout= keyword to get_readable_fileobj (when PR https://github.com/astropy/astropy/pull/1258 is merged)
-            with aud.get_readable_fileobj(url, cache=True) as f:
-                results = f.read()
-            break
-        except urllib2.URLError:
-            if verbose:
-                sys.stdout.write(u"Waiting %0.1fs for model to finish (elapsed wait time %0.1fs, total wait time %0.1f)\r" % (ping_delay,elapsed_time,time.time()-t0))
-                sys.stdout.flush()
-            time.sleep(ping_delay)
-            elapsed_time += ping_delay
-            continue
-        except socket.timeout:
-            if verbose:
-                sys.stdout.write(u"Waiting %0.1fs for model to finish (elapsed wait time %0.1fs, total wait time %0.1f)\r" % (ping_delay,elapsed_time,time.time()-t0))
-                sys.stdout.flush()
-            time.sleep(ping_delay)
-            elapsed_time += ping_delay
-            continue
-
-
-    if save:
-        if savename is None:
-            savename = filename
-        if not overwrite and os.path.exists(savename):
-            raise IOError("File %s already exists." % savename)
-        outf = open(savename,'w')
-        print >>outf,results
-        outf.close()
-
-    return parse_besancon_model_string(results)
 
 def parse_besancon_model_string(bms,):
 
