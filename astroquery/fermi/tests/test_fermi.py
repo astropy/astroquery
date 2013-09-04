89e370e2c66963d0a5360e548090a0a1aa5c9c74
Adam Ginsburg <keflavich@gmail.com>
2013-09-03 19:38:05 -0600

    remove socket guard; it is excessive at the moment

--- astroquery/fermi/tests/test_fermi.py
+++ astroquery/fermi/tests/test_fermi.py
@@ -1,11 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function
-
-import socket
-def guard(*args, **kwargs):
-    raise Exception("I told you not to use the Internet!")
-socket.socket = guard
-
 from ... import fermi
 from astropy.tests.helper import pytest
 import requests
