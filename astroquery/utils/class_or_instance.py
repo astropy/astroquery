0d619dfbf4ec6b0de91edadbcce8dce1e4ff7ae7
Adam Ginsburg <keflavich@gmail.com>
2013-09-05 13:31:38 -0600

    added magic that preserves argspec.  Hooray! However, because some default args have repr's that are NOT evaluable, this fails!

--- astroquery/utils/class_or_instance.py
+++ astroquery/utils/class_or_instance.py
@@ -5,6 +5,8 @@ methods of a class so that they can be called either as a class method
 or as instance methods.
 """
 import functools
+import inspect
+from collections import namedtuple
 
 __all__ = ["class_or_instance"]
 
@@ -12,6 +14,28 @@ class class_or_instance(object):
 
     def __init__(self, fn):
         self.fn = fn
+        argspec = inspect.getargspec(fn)
+        if argspec.args[0] != 'self':
+            raise ValueError('Method created with "self" not the first argument')
+        else:
+            # in the "fake" definition, we leave out "self"
+            args = argspec.args[1:]
+
+        # Some "default args" have repr's that are *not* evaluatable
+        # These must be replaced with strings
+        if argspec.defaults is not None:
+            defaults = []
+            for ii,default in enumerate(argspec.defaults):
+                try:
+                    eval("%s" % repr(default))
+                    defaults.append(default)
+                except SyntaxError:
+                    defaults.append('%s' % repr(default))
+        else:
+            defaults = None
+
+        self.argspec_d = dict(args=args, varargs=argspec.varargs,
+                              varkw=argspec.keywords, defaults=defaults)
 
         if hasattr(fn,'__doc__'):
             self.__doc__ = fn.__doc__
@@ -25,6 +49,17 @@ class class_or_instance(object):
             f = lambda *args, **kwds: self.fn(cls, *args, **kwds)
         functools.update_wrapper(f, self.fn)
         return f
+
+        # this was a failed attempt, preserved in history
+        formatted_args = inspect.formatargspec(**self.argspec_d)
+        fndef = 'lambda %s: f%s' % (formatted_args.lstrip('(').rstrip(')'),
+                                    formatted_args)
+        try:
+            fake_fn = eval(fndef, {'f': f})
+        except SyntaxError:
+            # raise it at this level so it can be debugged
+            raise Exception('syntax error')
+        return functools.wraps(self.fn)(fake_fn)
 
 class property_class_or_instance(property):
 
