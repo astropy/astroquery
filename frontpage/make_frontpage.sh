885c9ffad1cf18c67495e66d7fdfcd8ce3074539
Adam Ginsburg <keflavich@gmail.com>
2013-09-15 09:48:38 -0600

    get rid of "makefile", use bash script instead

---
+++ frontpage/make_frontpage.sh
@@ -0,0 +1,15 @@
+CWD=$PWD
+
+echo ${CWD}
+
+# ALL of these must pass!
+cd .. && \
+git checkout gh-pages && \
+git checkout master -- README.rst && \
+rst2html.py --stylesheet=stylesheets/styles.css,stylesheets/pygment_trac.css,stylesheets/normalize.css,stylesheets/ie.css --link-stylesheet README.rst index.html && \
+rm README.rst && \
+git reset && \
+git add index.html && \
+git commit -m "Index.html generated." && \
+git checkout master && \
+cd ${CWD}
