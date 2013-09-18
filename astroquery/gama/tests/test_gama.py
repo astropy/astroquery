f9a1d3f1775f5b35a2f7a830217b3fd405ea2a2b
James T. Allen <j.allen@physics.usyd.edu.au>
2013-09-18 12:06:06 +1000

    Flagged GAMA tests as remote.

--- astroquery/gama/tests/test_gama.py
+++ astroquery/gama/tests/test_gama.py
@@ -1,14 +1,17 @@
 from astropy.io import fits
 from ... import gama
+from astropy.tests.helper import remote_data
 
 SQL_QUERY = "SELECT * FROM SpecAll LIMIT 5"
 GAMA_URL = "http://www.gama-survey.org/"
 
+@remote_data
 def test_GAMA_query_sql_async():
     """Tests that a URL to the GAMA website is returned."""
     result = gama.core.GAMA.query_sql_async(SQL_QUERY)
     assert result.startswith(GAMA_URL)
 
+@remote_data
 def test_GAMA_query_sql():
     """Tests that a valid HDUList object is returned."""
     result = gama.core.GAMA.query_sql(SQL_QUERY)
