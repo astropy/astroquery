8d5f50b972530bab15a391bffe14fb3bc4dc08d2
James T. Allen <j.allen@physics.usyd.edu.au>
2013-09-18 12:10:11 +1000

    Fleshed out docstring.

--- astroquery/gama/__init__.py
+++ astroquery/gama/__init__.py
@@ -1,5 +1,10 @@
 """
-Access to GAMA data.
+gama
+----
+Access to GAMA (Galaxy And Mass Assembly) data, via the DR2 SQL query form.
+http://www.gama-survey.org/dr2/query/
+
+:author: James T. Allen <james.thomas.allen@gmail.com>
 """
 
 from .core import GAMA
