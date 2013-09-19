fd2b63c5bbfc80119373f65984844326b0fdf537
Adam Ginsburg <keflavich@gmail.com>
2013-09-19 19:58:36 -0400

    added internet connection testing

---
+++ astroquery/tests/test_internet.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from astropy.tests.helper import remote_data
+import requests
+
+@remote_data
+def test_net_connection():
+    R = requests.post('http://httpbin.org/post', 
+                      headers={'User-Agent':'astropy:astroquery.0.1.dev'})
+    R.raise_for_status()
