68eedf19bdb34ba4782b1e74ad5b8c79cd9eab5e
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-09-30 00:11:47 +0200

    parallel option can only be used with Astropy 0.3 or later

--- packagename/__init__.py
+++ packagename/__init__.py
@@ -29,8 +29,7 @@ def _get_test_runner():
 
 def test(package=None, test_path=None, args=None, plugins=None,
          verbose=False, pastebin=None, remote_data=False, pep8=False,
-         pdb=False, coverage=False, open_files=False, parallel=0,
-         **kwargs):
+         pdb=False, coverage=False, open_files=False, **kwargs):
     """
     Run the tests using py.test. A proper set of arguments is constructed and
     passed to `pytest.main`.
@@ -89,7 +88,7 @@ def test(package=None, test_path=None, args=None, plugins=None,
         When provided, run the tests in parallel on the specified
         number of CPUs.  If parallel is negative, it will use the all
         the cores on the machine.  Requires the `pytest-xdist` plugin
-        is installed.
+        is installed. Only available when using Astropy 0.3 or later.
 
     kwargs
         Any additional keywords passed into this function will be passed
@@ -107,8 +106,7 @@ def test(package=None, test_path=None, args=None, plugins=None,
         package=package, test_path=test_path, args=args,
         plugins=plugins, verbose=verbose, pastebin=pastebin,
         remote_data=remote_data, pep8=pep8, pdb=pdb,
-        coverage=coverage, open_files=open_files,
-        parallel=parallel, **kwargs)
+        coverage=coverage, open_files=open_files, **kwargs)
 
 if not _ASTROPY_SETUP_:
 
