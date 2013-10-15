9f3dd75f54d9d7362fc953da731d7212419833e4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-10-15 11:44:04 +0200

    another attempt to get the warnings to test right

--- astroquery/tests/test_import.py
+++ astroquery/tests/test_import.py
@@ -6,8 +6,10 @@ from distutils.version import LooseVersion
 # first test occurs with astropy import locally
 def test_monkeypatch_warning(recwarn):
     import astroquery
-    # need to clear the registry to get the warning to appear this time
-    astroquery.__warningregistry__.clear()
+    # if there has been a warning, there will be a warning registry
+    if hasattr(astroquery,'__warningregistry__'):
+        # need to clear the registry to get the warning to appear this time
+        astroquery.__warningregistry__.clear()
     reload(astroquery)
     if LooseVersion(astropy.version.version) < LooseVersion('0.3.dev4957'):
         warnstr = "You are using an 'old' version of astropy prior to the change that made all units singular.  astropy is being monkeypatched such that degrees and degree are both allow and hours and hour are both allowed.  This is NOT normal astropy behavior."
