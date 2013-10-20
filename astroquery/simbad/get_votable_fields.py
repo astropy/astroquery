61ae790da5bd9a1128d47866a97010e6474e9b27
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-10-20 15:52:46 +0200

    added the get_votable_fields script originally created by Madhura this is not intended for user use, but can be used to regenerate the .json file if it's updated the JSON file is now sorted

---
+++ astroquery/simbad/get_votable_fields.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import astropy.utils.data as aud
+content = aud.get_file_contents("http://simbad.u-strasbg.fr/simbad/sim-help?Page=sim-fscript#VotableFields")
+
+import re
+import bs4
+htmldoc = bs4.BeautifulSoup(content)
+search_text = re.compile(r'Field names for VOTable output', re.IGNORECASE)
+foundtext = htmldoc.find('h2', text=search_text)
+
+
+table = foundtext.findNext('table')# Find the first <table> tag that follows it
+outd = {}
+for row in table.findAll('tr'):
+    cols = row.findChildren('td')
+    if len(cols) > 1:
+        smallest_child = cols[0].find_all()[-1]
+        if cols[0].findChild("ul"):
+            text1 = cols[0].findChild('ul').getText()
+        elif cols[0].find_all():
+            text1 = smallest_child.getText()
+        else:
+            text1 = cols[0].getText()
+        if cols[1].findChild("ul"):
+            text2 = cols[1].findChild('ul').getText()
+        else:
+            text2 = cols[1].getText()
+        # ignore blank entries & headers
+        if (text2.strip() != '' and not
+               (smallest_child.name == 'font' and
+                    'size' in smallest_child.attrs and
+                    smallest_child.attrs['size'] == '+2')):
+            outd[text1.strip()] = text2.strip()
+
+import json
+with open('data/votable_fields_dict.json','w') as f:
+    json.dump(outd,f,indent=2,sort_keys=True)
