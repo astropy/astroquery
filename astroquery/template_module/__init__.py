bd1847f676ab10a445f8e6bdc4dad2db94fcb184
Madhura Parikh <madhuraparikh@gmail.com>
2013-12-25 19:04:15 +0530

    added ref to config docs

--- astroquery/template_module/__init__.py
+++ astroquery/template_module/__init__.py
@@ -1,14 +1,15 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-<Put Your Tool Name Here> 
+<Put Your Tool Name Here>
 ------------------------------------------------------
 
-:author: <your name> (<your email>) 
+:author: <your name> (<your email>)
 """
 
-# Make the URL of the server, timeout and other items configurable 
-# See   for docs and examples on how to do this
+# Make the URL of the server, timeout and other items configurable
+# See <http://docs.astropy.org/en/latest/config/index.html#developer-usage>
+# for docs and examples on how to do this
 # Below is a common use case
 
 from astropy.config import ConfigurationItem
@@ -19,10 +20,9 @@ DUMMY_SERVER = ConfigurationItem('dummy_server',['http://dummy_server_mirror_1',
 # Set the timeout for connecting to the server
 TIMEOUT = ConfigurationItem('timeout', <time in seconds>, 'default timeout for connecting to server')
 
-# Now import your public class 
+# Now import your public class
 # Should probably have the same name as your module
 
 from .core import DummyTool
 
 __all__ = ['DummyTool']
-
