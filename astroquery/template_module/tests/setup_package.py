f1708c9d9afbb90192fbae2c3c48d9be99a335cd
Madhura Parikh <madhuraparikh@gmail.com>
2013-12-28 20:54:21 +0530

    added templates for tests

--- astroquery/template_module/tests/setup_package.py
+++ astroquery/template_module/tests/setup_package.py
@@ -1,1 +1,10 @@
-# setup paths to test data
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import os
+# setup paths to the test data
+# can specify a single file or a list of files
+
+paths = [os.path.join('data', '*.dat'),
+         os.path.join('data', '*.xml')] # etc, add other extensions
+# you can also enlist files individually by names
+# finally construct and return a dict for the sub module
+return {'astroquery.template_module.tests': paths}
