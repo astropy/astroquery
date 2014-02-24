34813e149f2077392a022300372ac5e00b280196
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-24 10:13:33 +0100

    added new splatalogue utilities

---
+++ astroquery/splatalogue/templates.py
@@ -0,0 +1,10 @@
+"""
+Template queries modifying the defaults of Splatalogue
+"""
+
+from .core import SplatalogueClass
+
+SplatalogueKelvins = SplatalogueClass(energy_max=500, energy_type='eu_k',
+                                      energy_levels=['el4'],
+                                      line_strengths=['ls4'],
+                                      only_NRAO_recommended=True, noHFS=True)
