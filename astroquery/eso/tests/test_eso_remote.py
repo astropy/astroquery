4333c213bbbe76ed502ce384947328e7a8d3b1e1
Brigitta Sipocz <bsipocz@gmail.com>
2014-04-26 15:49:01 +0100

    Adding remote_data to test

--- astroquery/eso/tests/test_eso_remote.py
+++ astroquery/eso/tests/test_eso_remote.py
@@ -6,20 +6,22 @@ from ...eso import Eso
 CACHE_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
 
-def test_SgrAstar():
+@remote_data
+class TestEso:
+    def test_SgrAstar():
+        eso = Eso()
+        eso.cache_location = CACHE_PATH
 
-    eso = Eso()
-    eso.cache_location = CACHE_PATH
-    instruments = eso.list_instruments()
-    result_i = eso.query_instrument(instruments[0], target='Sgr A*')
-    surveys = eso.list_surveys()
-    result_s = eso.query_survey(surveys[0], target='Sgr A*')
+        instruments = eso.list_instruments()
+        result_i = eso.query_instrument(instruments[0], target='Sgr A*')
 
+        surveys = eso.list_surveys()
+        result_s = eso.query_survey(surveys[0], target='Sgr A*')
 
-def test_empty_return():
-    # test for empty return with an object from the North
-    eso = Eso()
-    surveys = eso.list_surveys()
-    result_s = eso.query_survey(surveys[0], target='M51')
+    def test_empty_return():
+        # test for empty return with an object from the North
+        eso = Eso()
+        surveys = eso.list_surveys()
+        result_s = eso.query_survey(surveys[0], target='M51')
 
-    assert result_s is None
+        assert result_s is None
