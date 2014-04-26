fced0245fee4b0fabdb9506edeef4a5a47dad7b0
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-26 12:49:12 +0200

    ESO tests: adding more

---
+++ astroquery/eso/tests/test_eso.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import os
+from astropy.tests.helper import remote_data
+from ...eso import Eso
+
+CACHE_PATH = os.path.join(os.path.dirname(__file__), 'data')
+
+def test_SgrAstar():
+    # Local caching prevents a remote query here
+
+    eso = Eso()
+    eso.cache_location = CACHE_PATH
+    instruments = eso.list_instruments()
+    result = eso.query_instrument(instruments[0], target='Sgr A*')
+
