9bc409f53f53bc6a48e2e088a766a91cb22a76b7
Michael Droettboom <mdboom@gmail.com>
2014-05-28 11:04:08 -0400

    Fix #69: Affiliated packages can opt-in to deprecations-as-exceptions

--- packagename/tests/test_example.py
+++ packagename/tests/test_example.py
@@ -5,3 +5,10 @@ def test_primes_c():
 def test_primes():
     from ..example_mod import primes
     assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+def test_deprecation():
+    import warnings
+    warnings.warn(
+        "This is deprecated, but shouldn't raise an exception, unless "
+        "enable_deprecations_as_exceptions() called from conftest.py",
+        DeprecationWarning)
