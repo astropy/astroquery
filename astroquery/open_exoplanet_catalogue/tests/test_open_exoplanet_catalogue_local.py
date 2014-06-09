61454db02eb3aacce5002199500cabe39d31f9d2
CadenArmstrong <cadenarmstrong@gmail.com>
2014-06-09 12:05:31 -0400

    Using localized version of database

--- astroquery/open_exoplanet_catalogue/tests/test_open_exoplanet_catalogue_local.py
+++ astroquery/open_exoplanet_catalogue/tests/test_open_exoplanet_catalogue_local.py
@@ -5,31 +5,13 @@ from ...utils.testing_tools import MockResponse
 from ... import open_exoplanet_catalogue as oec
 
 
-@pytest.fixture(autouse=True)
-def patch_urlopen(request):
-    mp = request.getfuncargvalue("monkeypatch")
-    try:
-        import urllib.request
-        mp.setattr(urllib.request, 'urlopen', get_mock_return)
-    except ImportError:
-        import urllib2
-        mp.setattr(urllib2, 'urlopen', get_mock_return)
-    return mp
-
-def get_mock_return(url, params=None, timeout=10,**kwargs):
-    # dummy function to replace urllib get functionality
-    # function returns what the http request would but with local data
-    filename = data_path('systems.xml.gz')
-    content = open(filename, "r")
-    return content
-
 # get file path of a static data file for testing
 def data_path(filename):
     data_dir = os.path.join(os.path.dirname(__file__), 'data')
     return os.path.join(data_dir, filename)
 
 def test_function(patch_urlopen):
-    cata = oec.get_catalogue()
+    cata = oec.get_catalogue(data_path('systems.xml.gz')) # use local version of database
     assert len(cata.findall('.//planet')) > 0
 
     for planet in cata.findall(".//planet[name]"):
