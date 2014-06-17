068a80b03a3c1af06d50c003508615d64f032356
derdon <liedtke.simon@googlemail.com>
2014-06-17 16:49:42 +0200

    Added trivial non-remote tests for XMatch (error checking).

--- astroquery/xmatch/tests/test_xmatch.py
+++ astroquery/xmatch/tests/test_xmatch.py
@@ -1,1 +1,22 @@
+import pytest
 
+from ...xmatch import XMatch
+
+
+def test_xmatch_query_invalid_max_distance():
+    with pytest.raises(ValueError):
+        XMatch().query('', '', 181, '')
+
+
+def test_xmatch_query_invalid_format():
+    with pytest.raises(ValueError):
+        XMatch().query('', '', 100, 'html')
+
+
+def test_get_avail_tables_invalid_format():
+    with pytest.raises(ValueError):
+        XMatch().get_available_tables('votable')
+    with pytest.raises(ValueError):
+        XMatch().get_available_tables('csv')
+    with pytest.raises(ValueError):
+        XMatch().get_available_tables('tsv')
