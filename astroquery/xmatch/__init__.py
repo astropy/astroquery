d3cfa28a851837dff1ec89604af41b28396ddec8
derdon <liedtke.simon@googlemail.com>
2014-06-17 17:08:59 +0200

    xmatch: minor change removed unnecessary kwarg `module` to ConfigurationItem

--- astroquery/xmatch/__init__.py
+++ astroquery/xmatch/__init__.py
@@ -3,7 +3,7 @@ from astropy.config import ConfigurationItem
 # TODO: use ConfigItem instead of deprecated ConfigurationItem
 XMATCH_URL = ConfigurationItem(
     'xmatch_url', 'http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
-    'xMatch URL', module='astroquery.xmatch')
+    'xMatch URL')
 
 XMATCH_TIMEOUT = ConfigurationItem(
     'timeout', 60, 'time limit for connecting to xMatch server')
