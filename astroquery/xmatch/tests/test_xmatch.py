b906ed15b52fbf8b7365074b8b23b22caf4b2a6b
derdon <liedtke.simon@googlemail.com>
2014-06-21 09:57:44 +0200

    test for XMatch.query: check the error message as well

--- astroquery/xmatch/tests/test_xmatch.py
+++ astroquery/xmatch/tests/test_xmatch.py
@@ -5,5 +5,7 @@ from ...xmatch import XMatch
 
 
 def test_xmatch_query_invalid_max_distance():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as ex:
         XMatch().query('', '', 181 * arcsec)
+        assert str(ex.value) == (
+            'max_distance argument must not be greater than 180')
