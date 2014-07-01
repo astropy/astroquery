769cc6b686f43b3823f9a8636bc1972d9a093ab7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-01 09:02:32 +0200

    package-template: another failed merge: __init__

--- astroquery/__init__.py
+++ astroquery/__init__.py
@@ -11,5 +11,5 @@ from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from example_mod import *
+#if not _ASTROPY_SETUP_:
+#    from astroquery import *
