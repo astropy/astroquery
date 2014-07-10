ea880be2bf7658d2d098d78b8287ca9b40bfd926
derdon <liedtke.simon@googlemail.com>
2014-07-10 18:54:49 +0200

    added utility function urljoin_keep_path

---
+++ astroquery/utils/url_helpers.py
@@ -0,0 +1,27 @@
+import urlparse
+import os.path
+
+
+def urljoin_keep_path(url, path):
+    """Join a base URL and a relative or absolute path. The important difference to :func:`urlparse.urljoin`
+    is that ``urljoin_keep_path`` does not remove the last directory of the path found in the parameter `url`
+    if it is in relative form. Compare the examples below to verify.
+
+    Examples
+    --------
+    >>> urljoin_keep_path('http://example.com/foo', 'bar')
+    'http://example.com/foo/bar'
+    >>> import urlparse
+    >>> urlparse.urljoin('http://example.com/foo', 'bar')
+    'http://example.com/bar'
+
+    """
+    # urlparse.SplitResult doesn't allow overriding attribute values,
+    # so ``splitted_url.path = ...`` is not possible here, unfortunately.
+    splitted_url = urlparse.urlsplit(url)
+    return urlparse.SplitResult(
+        splitted_url.scheme,
+        splitted_url.netloc,
+        os.path.join(splitted_url.path, path),
+        splitted_url.query,
+        splitted_url.fragment).geturl()
