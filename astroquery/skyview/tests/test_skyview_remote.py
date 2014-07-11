df90b71e31e7e6b48b3ffd9a076a243171011b20
derdon <liedtke.simon@googlemail.com>
2014-07-11 07:46:55 +0200

    fixed syntax error in SkyView test file

--- astroquery/skyview/tests/test_skyview_remote.py
+++ astroquery/skyview/tests/test_skyview_remote.py
@@ -2,7 +2,7 @@ from ...skyview import SkyView
 
 
 def test_get_image_list():
-    urls = SkyView().get_image_list(position='Eta Carinae', survey=['Fermi 5', 'HRI', 'DSS')
+    urls = SkyView().get_image_list(position='Eta Carinae', survey=['Fermi 5', 'HRI', 'DSS'])
     assert urls == [
         u'http://skyview.gsfc.nasa.gov/tempspace/fits/skv6183161285798_1.fits',
         u'http://skyview.gsfc.nasa.gov/tempspace/fits/skv6183161285798_2.fits',
