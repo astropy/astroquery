2790e45d6e940c99fdd591034d97d959a52fbef4
derdon <liedtke.simon@googlemail.com>
2014-07-24 13:18:24 +0200

    added or fixed non-remote tests for the xmatch package

--- astroquery/xmatch/tests/test_xmatch.py
+++ astroquery/xmatch/tests/test_xmatch.py
@@ -2,13 +2,16 @@ import os.path
 
 import pytest
 import requests
+from astropy.io import ascii
+from astropy.table import Table
 from astropy.units import arcsec
 
+from ...utils import commons
 from ...utils.testing_tools import MockResponse
 from ...xmatch import XMatch
 
 DATA_FILES = {'get':'tables.csv', # .action.getVizieRTableNames
-              'post':'pos_list.csv',} # .request.xmatch
+              'post':'query_res.csv',} # .request.xmatch
 
 class MockResponseXmatch(MockResponse):
     def __init__(self, method, url, data, **kwargs):
@@ -21,6 +24,7 @@ class MockResponseXmatch(MockResponse):
 
     def get_content(self):
         return self.content
+
 
 @pytest.fixture
 def patch_request(request):
@@ -62,11 +66,42 @@ def test_xmatch_is_avail_table(monkeypatch):
     assert xm.is_table_available('II/246/out')
     assert not xm.is_table_available('vizier:II/311/wise')
 
-# This should pass if we give it reasonable inputs (i.e. not 'something')
-@pytest.mark.xfail
-def test_xmatch_query(monkeypatch):
-    xm = XMatch()
-    monkeypatch.setattr(xm, 'request', request_mockreturn)
 
-    blah = xm.query('something')
-    blah = xm().query('something')
+def test_xmatch_query_local(monkeypatch):
+    monkeypatch.setattr(
+        commons,
+        'send_request',
+        lambda url, data, timeout, request_type='POST', headers={}, **kwargs:
+                request_mockreturn(request_type, url, data, **kwargs))
+    xm = XMatch()
+    with open(data_path('posList.csv')) as pos_list:
+        response = xm.query_async(
+            cat1=pos_list, cat2='vizier:II/246/out', max_distance=5 * arcsec,
+            colRA1='ra', colDec1='dec')
+    table = ascii.read(response.get_content())
+    assert isinstance(table, Table)
+    assert table.colnames == [
+        'angDist', 'ra', 'dec', '2MASS', 'RAJ2000', 'DEJ2000',
+        'errHalfMaj', 'errHalfMin', 'errPosAng', 'Jmag', 'Hmag', 'Kmag',
+        'e_Jmag', 'e_Hmag', 'e_Kmag', 'Qfl', 'Rfl', 'X', 'MeasureJD']
+    assert len(table) == 11
+
+
+def test_xmatch_query_cat1_table_local(monkeypatch):
+    monkeypatch.setattr(
+        commons,
+        'send_request',
+        lambda url, data, timeout, request_type='POST', headers={}, **kwargs:
+                request_mockreturn(request_type, url, data, **kwargs))
+    with open(data_path('posList.csv')) as pos_list:
+        input_table = Table.read(pos_list, names=['ra', 'dec'], format='ascii')
+    xm = XMatch()
+    response = xm.query_async(
+        cat1=input_table, cat2='vizier:II/246/out', max_distance=5 * arcsec)
+    table = ascii.read(response.get_content())
+    assert isinstance(table, Table)
+    assert table.colnames == [
+        'angDist', 'ra', 'dec', '2MASS', 'RAJ2000', 'DEJ2000',
+        'errHalfMaj', 'errHalfMin', 'errPosAng', 'Jmag', 'Hmag', 'Kmag',
+        'e_Jmag', 'e_Hmag', 'e_Kmag', 'Qfl', 'Rfl', 'X', 'MeasureJD']
+    assert len(table) == 11
