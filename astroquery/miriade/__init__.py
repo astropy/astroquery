dbecb6f15aee021cf2e3b39a05e8eb3a253b14df
Julien Woillez <jwoillez@gmail.com>
2014-08-01 09:41:24 +0200

    Added license information

--- astroquery/miriade/__init__.py
+++ astroquery/miriade/__init__.py
@@ -1,3 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Miriade Query Tool
+------------------
+A tool to query Miriade, The Virtual Observatory Solar System Object
+Ephemeris Generator, a web service of the IMCCE
+http://vo.imcce.fr/webservices/miriade/
+
+:Author: Julien Woillez (jwoillez@gmail.com) et al.
+"""
 
 from .core import Miriade, MiriadeClass
 
