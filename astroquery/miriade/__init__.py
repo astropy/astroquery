87e176b478a380316ad3df73071e9d293ad4ce9a
Julien Woillez <jwoillez@gmail.com>
2014-08-01 09:42:23 +0200

    Implements ConfigItem for observatory codes and rts URL

--- astroquery/miriade/__init__.py
+++ astroquery/miriade/__init__.py
@@ -9,6 +9,25 @@ http://vo.imcce.fr/webservices/miriade/
 :Author: Julien Woillez (jwoillez@gmail.com) et al.
 """
 
-from .core import Miriade, MiriadeClass
+from astropy import config as _config
 
-__all__ = ['Miriade','MiriadeClass']
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for `astroquery.miriade`.
+    """
+
+    obs_codes_url = _config.ConfigItem(
+        'http://www.minorplanetcenter.net/iau/lists/ObsCodes.html',
+        'Observatory codes URL from the Minor Planet Center.'
+        )
+    rts_url = _config.ConfigItem(
+        'http://vo.imcce.fr/webservices/miriade/rts_query.php',
+        'URL of the Rise Transit Time web service.'
+        )
+
+conf = Conf()
+
+from .core import Miriade, MiriadeClass
+ 
+__all__ = ['Miriade', 'MiriadeClass']
+
