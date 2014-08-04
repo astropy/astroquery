3316f50473e6ee4afbf9e860d2f2db8de07cee94
derdon <liedtke.simon@googlemail.com>
2014-08-04 14:42:21 +0200

    fixed path in skyview/setup_package.py

--- astroquery/skyview/setup_package.py
+++ astroquery/skyview/setup_package.py
@@ -2,4 +2,4 @@ import os
 
 
 def get_package_data():
-    return {'astroquery.xmatch.tests': [os.path.join('data', '*.html')]}
+    return {'astroquery.skyview.tests': [os.path.join('data', '*.html')]}
