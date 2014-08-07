1680f65bada8d818226888a51176b3cb861c335f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-08-07 10:08:00 +0200

    remove print statement

--- docs/make_doctests.py
+++ docs/make_doctests.py
@@ -19,7 +19,6 @@ with open('doctests.py','w') as doctests:
     for root,dirs,files in os.walk('.'):
         for fn in files:
             if os.path.splitext(fn)[1] == '.rst':
-                print os.path.join(root, fn)
                 with open(os.path.join(root, fn),'r') as f:
                     lines = f.readlines()
 
