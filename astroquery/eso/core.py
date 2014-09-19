dd9997d57256cc58debbd26524313396031c9250
Julien Woillez <jwoillez@gmail.com>
2014-09-19 20:53:58 +0200

    Clear session redirect_cache
    New requests will break otherwise

--- astroquery/eso/core.py
+++ astroquery/eso/core.py
@@ -549,6 +549,7 @@ class EsoClass(QueryWithLogin):
                 fileLink = "http://dataportal.eso.org/dataPortal"+fileId.attrs['value'].split()[1]
                 filename = self._request("GET", fileLink, save=True)
                 files.append(system_tools.gunzip(filename))
+        self._session.redirect_cache.clear() # EMpty the redirect cache of this request session
         log.info("Done!")
         if (not return_list) and (len(files)==1):
             files = files[0]
