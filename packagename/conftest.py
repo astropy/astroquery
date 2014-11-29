64f3e09eb74158233af37513d0cedf75b8d62aae
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-11-29 17:01:51 +0100

    Add note about adding/removing packages to/from pytest header

--- packagename/conftest.py
+++ packagename/conftest.py
@@ -7,3 +7,9 @@ from astropy.tests.pytest_plugins import *
 ## Uncomment the following line to treat all DeprecationWarnings as
 ## exceptions
 # enable_deprecations_as_exceptions()
+
+## Uncomment and customize the following lines to add/remove entries
+## from the list of packages for which version numbers are displayed
+## when running the tests
+# PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
+# del PYTEST_HEADER_MODULES['h5py']
