82cd81d652da3186dd19cf24b618cc7b61af6d83
Magnus Persson <vilhelmp@gmail.com>
2015-01-12 16:09:32 +0100

    NASA ADS simple text search working. Results NOT parsed.

--- astroquery/nasa_ads/__init__.py
+++ astroquery/nasa_ads/__init__.py
@@ -6,50 +6,98 @@ SAO/NASA ADS Query Tool
 :Author: Magnus Vilhelm Persson (magnusp@vilhelm.nu)
 
 """
-from astropy import config as _config
 
+#~ from astropy.config import ConfigurationItem
+
+from astropy import config as _config
 
 class Conf(_config.ConfigNamespace):
     """
     Configuration parameters for `astroquery.nasa_ads`.
     """
-    servers = _config.ConfigItem(
-        ['http://adswww.harvard.edu/',
-        'http://cdsads.u-strasbg.fr/',
-        'http://ukads.nottingham.ac.uk/',
-        'http://esoads.eso.org/',
-        'http://ads.ari.uni-heidelberg.de/',
-        'http://ads.inasan.ru/',
-        'http://ads.mao.kiev.ua/',
-        'http://ads.astro.puc.cl/',
-        'http://ads.nao.ac.jp/',
-        'http://ads.bao.ac.cn/',
-        'http://ads.iucaa.ernet.in/',
-        'http://ads.arsip.lipi.go.id/',
-        'http://saaoads.chpc.ac.za/',
-        'http://ads.on.br/'],
-        'SAO/NASA ADS mirrors around the world'
-        )
+    server = _config.ConfigItem(
+            'http://adswww.harvard.edu',
+            'SAO/NASA ADS main server.'
+            )
+    mirrors = _config.ConfigItem(
+            ['http://cdsads.u-strasbg.fr',
+            'http://ukads.nottingham.ac.uk',
+            'http://esoads.eso.org',
+            'http://ads.ari.uni-heidelberg.de',
+            'http://ads.inasan.ru',
+            'http://ads.mao.kiev.ua',
+            'http://ads.astro.puc.cl',
+            'http://ads.nao.ac.jp',
+            'http://ads.bao.ac.cn',
+            'http://ads.iucaa.ernet.in',
+            'http://ads.arsip.lipi.go.id',
+            'http://saaoads.chpc.ac.za',
+            'http://ads.on.br'],
+            'SAO/NASA ADS mirrors around the world'
+            )
+    advanced_path = _config.ConfigItem(
+            '/cgi-bin/nph-abs_connect',
+            'Path for advanced query'    
+            )
     
-    advanced_url = _config.ConfigItem(
-        'abstract_service.html',
-        'Path for advanced query'
-        )
-        
-    simple_url = _config.ConfigItem(
-        'index.html',
-        'Path for advanced query'
-        )
-
+    simple_path = _config.ConfigItem(
+            '/cgi-bin/nph-basic_connect',
+            'Path for simple query'
+            )
+    
     timeout = _config.ConfigItem(
-        60,
-        'Time limit for connecting to ADS server'
-        )
-
+            60,
+            'Time limit for connecting to ADS server'
+    )
+        
 conf = Conf()
 
-from .core import ADS, ADSClass
 
-__all__ = ['ADS', 'ADSClass',
-           'Conf', 'conf',
-           ]
+from .core import ADSClass, ADS
+
+__all__ = ['ADSClass', 'ADS',
+        'Conf', 'conf']
+
+#~ class Conf(_config.ConfigNamespace):
+    #~ """
+    #~ Configuration parameters for `astroquery.nasa_ads`.
+    #~ """
+    #~ servers = _config.ConfigItem(
+        #~ ['http://adswww.harvard.edu',
+        #~ 'http://cdsads.u-strasbg.fr',
+        #~ 'http://ukads.nottingham.ac.uk',
+        #~ 'http://esoads.eso.org',
+        #~ 'http://ads.ari.uni-heidelberg.de',
+        #~ 'http://ads.inasan.ru',
+        #~ 'http://ads.mao.kiev.ua',
+        #~ 'http://ads.astro.puc.cl',
+        #~ 'http://ads.nao.ac.jp',
+        #~ 'http://ads.bao.ac.cn',
+        #~ 'http://ads.iucaa.ernet.in',
+        #~ 'http://ads.arsip.lipi.go.id',
+        #~ 'http://saaoads.chpc.ac.za',
+        #~ 'http://ads.on.br'],
+        #~ 'SAO/NASA ADS mirrors around the world'
+        #~ )
+    #~ 
+    #~ advanced_url = _config.ConfigurationItem(
+        #~ '/cgi-bin/nph-abs_connect',
+        #~ 'Path for advanced query'
+        #~ )
+        #~ 
+    #~ simple_url = _config.ConfigurationItem(
+        #~ '/cgi-bin/nph-basic_connect',
+        #~ 'Path for simple query'
+        #~ )
+#~ 
+    #~ timeout = _config.ConfigurationItem(
+        #~ 60,
+        #~ 'Time limit for connecting to ADS server'
+        #~ )
+#~ 
+#~ conf = Conf()
+#~ 
+#~ from .core import ADS, ADSClass
+#~ 
+#~ __all__ = ['ADS', 'ADSClass',
+           #~ 'Conf', 'conf']
