561392c4aa788d2d5d7fc27d2f4da0b434ba7dd1
Adam Ginsburg <keflavich@yahoo.com>
2015-02-05 14:46:02 +0100

    remove redundant section in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -200,18 +200,3 @@ else:
 
 edit_on_github_source_root = ""
 edit_on_github_doc_root = "docs"
-
-## -- Options for the edit_on_github extension ----------------------------------------
-
-if eval(setup_cfg.get('edit_on_github')):
-    extensions += ['astropy.sphinx.ext.edit_on_github']
-
-    versionmod = __import__(setup_cfg['package_name'] + '.version')
-    edit_on_github_project = setup_cfg['github_project']
-    if versionmod.version.release:
-        edit_on_github_branch = "v" + versionmod.version.version
-    else:
-        edit_on_github_branch = "master"
-
-    edit_on_github_source_root = ""
-    edit_on_github_doc_root = "docs"
