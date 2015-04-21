47cffb062cd82a4624bdb22400a57290a8f559ad
Magnus Persson <vilhelmp@gmail.com>
2015-04-21 15:43:13 +0200

    changed the test, removed patch_post

--- astroquery/nasa_ads/tests/test_nasaads.py
+++ astroquery/nasa_ads/tests/test_nasaads.py
@@ -1,5 +1,6 @@
 from ... import nasa_ads
+from astropy.tests.helper import pytest
 
-def test_simple(patch_post):
+def test_simple():
     x = nasa_ads.ADS.query_simple('^Persson Origin of water around deeply embedded low-mass protostars')
     assert x[-1]['authors'][0] == 'Persson, M. V.'
