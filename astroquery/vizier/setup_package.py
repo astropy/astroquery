1bbc86fe6a2076d119ca59d17d72c05be9997a53
Erik Tollerud <erik.tollerud@gmail.com>
2015-10-27 18:21:56 -0400

    add test for find_catalogs + get_catalogs

--- astroquery/vizier/setup_package.py
+++ astroquery/vizier/setup_package.py
@@ -8,6 +8,7 @@ def get_package_data():
     paths_test = [os.path.join('data', 'viz.xml'),
                   os.path.join('data', 'kang2010.xml'),
                   os.path.join('data', 'afgl2591_iram.xml'),
+                  os.path.join('data', 'find_kangapj70683.xml'),
                   ]
 
     paths_core = [os.path.join('data', 'inverse_dict.json'),
