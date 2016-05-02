1f7862cb0e33c4f5e0ad253da98038f0eb23fafa
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-05-02 11:05:11 -0400

    remove meteo & ambient from tests

--- astroquery/eso/tests/test_eso_remote.py
+++ astroquery/eso/tests/test_eso_remote.py
@@ -12,7 +12,7 @@ instrument_list = [u'fors1', u'fors2', u'sphere', u'vimos', u'omegacam',
                    u'hawki', u'isaac', u'naco', u'visir', u'vircam', u'apex',
                    u'giraffe', u'uves', u'xshooter', u'muse', u'crires',
                    u'kmos', u'sinfoni', u'amber', u'midi', u'pionier',
-                   u'harps', u'ambient_paranal', u'meteo_paranal']
+                   u'harps',]
 
 
 @remote_data
