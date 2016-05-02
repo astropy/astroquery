3ccf01b57edea060544c92bc94d655370b35d739
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-05-02 11:06:25 -0400

    fix local test urls

--- astroquery/eso/tests/test_eso.py
+++ astroquery/eso/tests/test_eso.py
@@ -10,12 +10,12 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 def data_path(filename):
     return os.path.join(DATA_DIR, filename)
 
-DATA_FILES = {'GET': {'http://archive.eso.org/wdb/wdb/asm/amber/form':
+DATA_FILES = {'GET': {'http://archive.eso.org/wdb/wdb/eso/amber/form':
                       'amber_form.html',
                       'http://archive.eso.org/wdb/wdb/adp/phase3_main/form':
                       'vvv_sgra_form.html',
                       },
-              'POST': {'http://archive.eso.org/wdb/wdb/asm/amber/query':
+              'POST': {'http://archive.eso.org/wdb/wdb/eso/amber/query':
                        'amber_sgra_query.tbl',
                        'http://archive.eso.org/wdb/wdb/adp/phase3_main/query':
                        'vvv_sgra_survey_response.tbl',
