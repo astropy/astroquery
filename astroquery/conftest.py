78c14351706531f3ee898d1a8108a759715e4695
Brigitta Sipocz <bsipocz@gmail.com>
2016-06-03 12:16:21 +0100

    Updating to package template 1.1.1

--- astroquery/conftest.py
+++ astroquery/conftest.py
@@ -1,11 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import print_function
 import os
-
-
-# This is to figure out the astroquery version, rather than using Astropy's
-from . import version
-
 
 # this contains imports plugins that configure py.test for astropy tests.
 # by importing them here in conftest.py they are discoverable by py.test
@@ -28,5 +22,20 @@ try:
     PYTEST_HEADER_MODULES['pyregion'] = 'pyregion'
     del PYTEST_HEADER_MODULES['h5py']
     del PYTEST_HEADER_MODULES['Scipy']
-except NameError:
+except (NameError, KeyError):
     pass
+
+## Uncomment the following line to treat all DeprecationWarnings as
+## exceptions
+# enable_deprecations_as_exceptions()
+
+
+# This is to figure out the affiliated package version, rather than
+# using Astropy's
+try:
+    from .version import version
+except ImportError:
+    version = 'dev'
+
+packagename = os.path.basename(os.path.dirname(__file__))
+TESTED_VERSIONS[packagename] = version
