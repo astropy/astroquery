8abb1113dd1790702e1cbcc07536e65b781fa8c3
Henrik Norman <Honke.norman@gmail.com>
2016-10-10 17:57:08 +0200

    Corrected remote tests

--- astroquery/esasky/tests/test_esasky_remote.py
+++ astroquery/esasky/tests/test_esasky_remote.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function
 
-from astropy.table import Table
+from astroquery.utils.commons import TableList
 from astropy.tests.helper import remote_data
 
 from ... import esasky
@@ -11,17 +11,16 @@ class TestESASky:
         
     def test_esasky_query_region_maps(self):
         result = esasky.core.ESASkyClass().query_region_maps("M51", "5 arcmin")
-        assert isinstance(result, Table)
+        assert isinstance(result, TableList)
 
     def test_esasky_query_object_maps(self):
         result = esasky.core.ESASkyClass().query_object_maps("M51")
-        assert isinstance(result, Table)
+        assert isinstance(result, TableList)
         
     def test_esasky_query_region_catalogs(self):
         result = esasky.core.ESASkyClass().query_region_catalogs("M51", "5 arcmin")
-        assert isinstance(result, Table)
+        assert isinstance(result, TableList)
         
     def test_esasky_query_object_catalogs(self):
         result = esasky.core.ESASkyClass().query_object_maps("M51")
-        assert isinstance(result, Table)
-        
+        assert isinstance(result, TableList)
