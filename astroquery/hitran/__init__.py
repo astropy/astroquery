9bd22c176fc347282814aa59f724b851230f3555
Miguel de Val-Borro <miguel@archlinux.net>
2016-11-01 18:43:04 -0400

    Add __init__.py file for hitran subpackage

---
+++ astroquery/hitran/__init__.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+HITRAN Catalog Query Tool
+-------------------------
+
+:Author: Adam Ginsburg (adam.g.ginsburg@gmail.com)
+"""
+from .reader import read_hitran_file
