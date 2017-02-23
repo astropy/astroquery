7605e8d52d0b36a433e82034e8cdb9f33f11f81a
Juan-Carlos Segovia <juancarlos.segovia@gmail.com>
2017-02-23 16:04:08 +0100

    Added setup_package

---
+++ astroquery/gaia/tests/setup_package.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import
+
+import os
+
+
+# setup paths to the test data
+# can specify a single file or a list of files
+def get_package_data():
+    paths = [os.path.join('data', '*.vot'),
+             os.path.join('data', '*.xml'),
+             ]  # etc, add other extensions
+    # you can also enlist files individually by names
+    # finally construct and return a dict for the sub module
+    return {'astroquery.gaia.tests': paths}
