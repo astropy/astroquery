c7313c7c4704bad7b9ef9a639d633d19bfb1235c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-03-28 13:54:32 -0400

    add a test for resuming started queries
    (this has been sitting uncommitted on my machine for months.... sigh)

---
+++ astroquery/tests/test_resume.py
@@ -0,0 +1,40 @@
+import string
+import requests
+
+from astropy.tests.helper import remote_data
+from .. import query
+
+def test_resume():
+    # Test that a resumed query will finish
+
+    length = 2048
+
+    # 'range' will return abcd...xyz repeating
+    target_url = 'http://127.0.0.1:5000/range/{0}'.format(length)
+
+    # simple check: make sure the server does what's expected
+    assert len(requests.get(target_url).content) == length
+
+    qu = query.BaseQuery()
+
+    result_1 = qu._request('GET', target_url, save=True)
+    # now the full file is written, so we have to delete parts of it
+
+    with open(result_1,'rb') as fh:
+        data = fh.read()
+    with open(result_1,'wb') as fh:
+        # overwrite with a partial file
+        fh.write(data[:1024])
+    with open(result_1,'rb') as fh:
+        data = fh.read()
+        assert len(data) == 1024
+
+    result_2 = qu._request('GET', target_url, save=True, continuation=True)
+
+    assert 'range' in qu._session.headers
+    assert qu._session.headers['range'] == 'bytes={0}-{1}'.format(1024, length-1)
+
+    with open(result_2,'rb') as fh:
+        data = fh.read()
+    assert len(data) == length
+    assert data == (string.ascii_lowercase*80)[:length].encode('ascii')
