e7ee4f6d7d046bf7955761277c8b2dbc6408d12e
Brigitta Sipocz <bsipocz@gmail.com>
2017-05-05 01:28:47 +0100

    xfailing the lamda writer for windows and py3 until #894 is solved

--- astroquery/lamda/tests/test_lamda.py
+++ astroquery/lamda/tests/test_lamda.py
@@ -2,6 +2,10 @@
 import os
 import tempfile
 import numpy as np
+import pytest
+import sys
+import platform
+
 from ...lamda import core
 
 DATA_FILES = {'co': 'co.txt'}
@@ -21,6 +25,9 @@ def test_parser():
     assert len(radtransitions) == 40
 
 
+@pytest.mark.xfail(platform.system() == 'Windows' and sys.version_info[0] >= 3,
+                   reason=("https://github.com/astropy/astroquery/pull/894 and "
+                           "https://github.com/astropy/astropy/issues/5126"))
 def test_writer():
     tables = core.parse_lamda_datafile(data_path('co.txt'))
     coll, radtrans, enlevels = tables
