4a9d19254f95a14795b7581487622fb0660a9cb4
Clara Brasseur <cbrasseur@stsci.edu>
2017-07-05 16:29:18 -0400

    one more pep8 issue

--- astroquery/mast/tests/setup_package.py
+++ astroquery/mast/tests/setup_package.py
@@ -2,6 +2,6 @@ import os
 
 
 def get_package_data():
-    paths_test = [os.path.join('data', '*.json')] +[os.path.join('data', '*.extjs')]
+    paths_test = [os.path.join('data', '*.json')] + [os.path.join('data', '*.extjs')]
 
     return {'astroquery.mast.tests': paths_test}
