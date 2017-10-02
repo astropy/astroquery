fa91939317933b1cbcc19b230f495cf83b90667a
Julien Woillez <jwoillez@eso.org>
2017-10-03 00:18:30 +0200

    Fixed remote test test_SgrAstar_remotevslocal

--- astroquery/eso/tests/test_eso_remote.py
+++ astroquery/eso/tests/test_eso_remote.py
@@ -98,18 +98,12 @@ class TestEso:
     def test_SgrAstar_remotevslocal(self, temp_dir):
         eso = Eso()
         # Remote version
-        instruments = eso.list_instruments(cache=False)
-        # result1 = eso.query_instrument(instruments[0], target='Sgr A*')
-        result1 = eso.query_instrument(instruments[0], coord1=266.41681662,
+        result1 = eso.query_instrument('gravity', coord1=266.41681662,
                                        coord2=-29.00782497, cache=False)
-
         # Local version
         eso.cache_location = temp_dir
-        instruments = eso.list_instruments(cache=False)
-        # result2 = eso.query_instrument(instruments[0], target='Sgr A*')
-        result2 = eso.query_instrument(instruments[0], coord1=266.41681662,
-                                       coord2=-29.00782497, cache=False)
-
+        result2 = eso.query_instrument('gravity', coord1=266.41681662,
+                                       coord2=-29.00782497, cache=True)
         assert all(result1 == result2)
 
     def test_list_instruments(self):
