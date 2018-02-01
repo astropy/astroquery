be513a74a123139160f0a115546a7268e3e1dd24
Leo Singer <leo.singer@ligo.org>
2018-02-01 17:01:01 -0500

    Empty out and deprecate astroquery.ibe module
    IRSA no longer supports the IBE interface. To access IRSA datasets
    programatically, users encouraged to use the TAP protocol
    (http://irsa.ipac.caltech.edu/docs/program_interface/TAP.html) via
    PyVO (https://pyvo.readthedocs.io/en/latest/dal/index.html).

--- astroquery/ibe/__init__.py
+++ astroquery/ibe/__init__.py
@@ -3,43 +3,14 @@
 IRSA Image Server program interface (IBE) Query Tool
 ====================================================
 
-This module contains various methods for querying the
-IRSA Image Server program interface (IBE).
+The ``astroquery.ibe`` module is deprecated and will be removed in a future
+release of Astroquery. IRSA no longer supports the IBE interface. To access
+IRSA datasets programatically, users encouraged to use the TAP protocol
+(http://irsa.ipac.caltech.edu/docs/program_interface/TAP.html) via PyVO
+(https://pyvo.readthedocs.io/en/latest/dal/index.html#table-access-protocol).
 """
-from astropy import config as _config
+import warnings
+from astropy.utils.exceptions import AstropyDeprecationWarning
 
-
-class Conf(_config.ConfigNamespace):
-    """
-    Configuration parameters for `astroquery.ibe`.
-    """
-
-    server = _config.ConfigItem(
-        'http://irsa.ipac.caltech.edu/ibe/',
-        'Name of the IBE server to use.')
-    mission = _config.ConfigItem(
-        'ptf',
-        ('Default mission. See, for example, '
-         'http://irsa.ipac.caltech.edu/ibe/search/ for options.'))
-
-    dataset = _config.ConfigItem(
-        'images',
-        ('Default data set. See, for example, '
-         'http://irsa.ipac.caltech.edu/ibe/search/ptf for options.'))
-    table = _config.ConfigItem(
-        'level1',
-        ('Default table. See, for example, '
-         'http://irsa.ipac.caltech.edu/ibe/search/ptf/images for options.'))
-    timeout = _config.ConfigItem(
-        60,
-        'Time limit for connecting to the IRSA server.')
-
-
-conf = Conf()
-
-
-from .core import Ibe, IbeClass
-
-__all__ = ['Ibe', 'IbeClass',
-           'Conf', 'conf',
-           ]
+warnings.warn(__doc__.partition('\n\n')[2].strip().replace('\n', ' '),
+              AstropyDeprecationWarning)
