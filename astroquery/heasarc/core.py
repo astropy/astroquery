eae8d296363acbb4aa3bc2e4fe0fd5c1552e3b5c
Josh Cardenzana <jcardenzana@irap.omp.eu>
2018-02-16 16:22:34 +0100

    Modify heasarc module to be more generic and improve functionality. Add ability to set radius (arcmin) for query Add ability to return all columns from a given mission table Add ability to return user specified subset of columns from a mission table Generalize layout to make adding additional query methods simpler

--- astroquery/heasarc/core.py
+++ astroquery/heasarc/core.py
@@ -15,31 +15,52 @@ __all__ = ['Heasarc', 'HeasarcClass']
 class HeasarcClass(BaseQuery):
 
     """HEASARC query class.
+
+    For a list of available HEASARC mission tables, visit:
+        https://heasarc.gsfc.nasa.gov/cgi-bin/W3Browse/w3catindex.pl
     """
 
     URL = conf.server
     TIMEOUT = conf.timeout
 
-    def query_object_async(self, object_name, mission, cache=True,
-                           get_query_payload=False,
-                           display_mode='FitsDisplay'):
-        """TODO: document this!
-
-        (maybe start by copying over from some other service.)
+    def query_async(self, request_payload, cache=True):
         """
-        request_payload = dict()
-        request_payload['Entry'] = object_name
-        request_payload['tablehead'] = ('BATCHRETRIEVALCATALOG_2.0 {}'
-                                        .format(mission))
-        request_payload['Action'] = 'Query'
-        request_payload['displaymode'] = display_mode
-
-        if get_query_payload:
-            return request_payload
-
+        Submit a query based on a given request_payload. This allows detailed
+        control of the query to be submitted.
+        """
         response = self._request('GET', self.URL, params=request_payload,
                                  timeout=self.TIMEOUT, cache=cache)
         return response
+
+
+    def query_object_async(self, object_name, mission,
+                           cache=True, get_query_payload=False,
+                           display_mode='FitsDisplay', **kwargs):
+        """ Query around a specific object within a given mission catalog
+
+        Parameters
+        ----------
+        object_name : str
+            Object to query around. To set search radius use the hidden 'radius'
+            parameter.
+        mission : str
+            Mission table to search from
+        **kwargs : 
+            see :func:`_args_to_payload` for list of additional parameters that
+            can be used to refine search query
+        """
+        request_payload = self._args_to_payload(
+            mission = mission,
+            entry   = object_name,
+            **kwargs
+        )
+
+        # Return payload if requested
+        if get_query_payload:
+            return request_payload
+
+        return self.query_async(request_payload, cache)
+
 
     def _fallback(self, content):
         """
@@ -80,6 +101,79 @@ class HeasarcClass(BaseQuery):
             return table
         except ValueError:
             return self._fallback(response.content)
+
+
+    def _args_to_payload(self, **kwargs):
+        """
+        Generates the payload based on user supplied arguments
+
+        Parameters
+        ----------
+        mission : str
+            Mission table to query
+        entry : str, optional
+            Object or position for center of query. A blank value will return
+            all entries in the mission table. Acceptable formats:
+            * Object name : Name of object, e.g. 'Crab'
+            * Coordinates : CSV list of coordinates, either as 'degrees,degrees' or 'hh mm ss,hh mm ss'
+        fields : str, optional
+            Return format for columns from the server available options:
+            * Standard (default) : Return default table columns
+            * All                : Return all table columns
+            * <custom>           : User defined csv list of columns to be returned
+        radius : float (arcmin), optional
+            Return all mission entries within a given distance of search query.
+            If not supplied, the server default will be used (~ 60 arcmin)
+        coordsys: str ('equatorial' or 'galactic'), optional
+            If 'entry' is a set of coordinates, this specifies the coordinate 
+            system used to interpret them. By default, equatorial coordinates
+            are assumed.
+        equinox : int, optional
+            Epoch by which to interpret supplied equatorial coordinates (ignored
+            if coordsys is galactic)
+
+        displaymode : str, optional
+            Return format from server. Since the user does not interact with 
+            this directly, it's best to leave this alone
+        action : str, optional
+            Type of action to be taken (defaults to 'Query')
+        """
+        # Define the basic query for this object
+        request_payload = dict(
+            tablehead   = ('name=BATCHRETRIEVALCATALOG_2.0 {}'
+                          .format(kwargs.get('mission'))),
+            Entry       = kwargs.get('entry', 'none'),
+            Action      = kwargs.get('action', 'Query'),
+            displaymode = kwargs.get('displaymode','FitsDisplay')
+        )
+
+        # Fill in optional information
+
+        fields = kwargs.get('fields', None)
+        if fields is not None:
+            if fields.lower() == 'standard':
+                request_payload['Fields'] = 'Standard'
+            elif fields.lower() == 'all':
+                request_payload['Fields'] = 'All'
+            else:
+                request_payload['varon'] = fields.lower().split(',')
+
+        radius = kwargs.get('radius', None)
+        if radius is not None:
+            request_payload['Radius'] = radius
+            
+        coordinates = kwargs.get('coordsys', 'equatorial')
+        if coordinates.lower() == 'equatorial':
+            request_payload['Coordinates'] = 'Equatorial: R.A. Dec'
+
+            equinox = kwargs.get('equinox', None)
+            if equinox is not None:
+                request_payload['Equinox'] = str(equinox) 
+
+        elif coordinates.lower() == 'galactic':
+            request_payload['Coordinates'] = 'Galactic: LII BII'
+              
+        return request_payload
 
 
 Heasarc = HeasarcClass()
