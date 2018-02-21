f9a32e6359814d0d0bcf064e8124f75294098fb1
Philip Cowperthwaite <pcowpert@cfa.harvard.edu>
2018-02-21 13:24:33 -0700

    Fix docstring errors.

--- astroquery/oac/core.py
+++ astroquery/oac/core.py
@@ -94,44 +94,44 @@ class OACClass(BaseQuery):
         >>> from astroquery.oac import OAC
         >>> metadata = OAC.query_object("GW170817")
         >>> print(metadata.keys())
-        >>> ['event', 'hostoffsetdist', 'masses', 'ra', 'instruments',
-             'lumdist', 'hostdec', 'host', 'velocity', 'ebv', 'hostra',
-             'claimedtype', 'redshift', 'maxabsmag', 'alias', 'hostoffsetang',
-             'download', 'maxdate', 'discoverdate', 'xraylink', 'dec',
-             'maxappmag', 'radiolink', 'spectralink', 'references', 'name',
-             'photolink']
+        ['event', 'hostoffsetdist', 'masses', 'ra', 'instruments',
+         'lumdist', 'hostdec', 'host', 'velocity', 'ebv', 'hostra',
+         'claimedtype', 'redshift', 'maxabsmag', 'alias', 'hostoffsetang',
+         'download', 'maxdate', 'discoverdate', 'xraylink', 'dec',
+         'maxappmag', 'radiolink', 'spectralink', 'references', 'name',
+         'photolink']
 
         Specific data can be requested using quantity and attribute
         entries. For example, to request a light curve for an event:
 
         >>> photometry = OAC.query_object("GW170817", quantity="photometry",
-                                          attribute=["time", "magnitude",
-                                                     "e_magnitude", "band",
-                                                     "instrument"])
+        ...                               attribute=["time", "magnitude",
+        ...                                          "e_magnitude", "band",
+        ...                                          "instrument"])
         >>> print(photometry[:5])
-        >>> event      time    magnitude e_magnitude band instrument
-            -------- --------- --------- ----------- ---- ----------
-            GW170817 57743.334     20.44                r
-            GW170817 57790.358     21.39                r
-            GW170817 57791.323     21.34                r
-            GW170817 57792.326     21.26                r
-            GW170817 57793.335     21.10                r
+        event      time    magnitude e_magnitude band instrument
+        -------- --------- --------- ----------- ---- ----------
+        GW170817 57743.334     20.44                r
+        GW170817 57790.358     21.39                r
+        GW170817 57791.323     21.34                r
+        GW170817 57792.326     21.26                r
+        GW170817 57793.335     21.10                r
 
         The results can be further refined using the argument entry:
 
         >>> photometry = OAC.query_object("GW170817", quantity="photometry",
-                                          attribute=["time", "magnitude",
-                                                     "e_magnitude", "band",
-                                                     "instrument"],
-                                          argument=["band=i"])
+        ...                               attribute=["time", "magnitude",
+        ...                                          "e_magnitude", "band",
+        ...                                          "instrument"],
+        ...                               argument=["band=i"])
         >>> print(photometry[:5])
-        >>>  event          time magnitude e_magnitude band instrument
-            -------- ----------- --------- ----------- ---- ----------
-            GW170817    57982.98      17.3                i
-            GW170817  57982.9814     17.48        0.02    i      Swope
-            GW170817 57983.00305     17.48        0.03    i      DECam
-            GW170817    57983.05    16.984       0.050    i       ROS2
-            GW170817 57983.23125     17.24        0.06    i        PS1
+        event          time magnitude e_magnitude band instrument
+        -------- ----------- --------- ----------- ---- ----------
+        GW170817    57982.98      17.3                i
+        GW170817  57982.9814     17.48        0.02    i      Swope
+        GW170817 57983.00305     17.48        0.03    i      DECam
+        GW170817    57983.05    16.984       0.050    i       ROS2
+        GW170817 57983.23125     17.24        0.06    i        PS1
 
         """
         request_payload = self._args_to_payload(event,
@@ -225,47 +225,45 @@ class OACClass(BaseQuery):
         >>> import astropy.coordinates as coord
         >>> import astropy.units as u
         >>> from astroquery.oac import OAC
-
         >>> #Sample coordinates. We are using GW170817.
         >>> ra = 197.45037
         >>> dec = -23.38148
         >>> test_coords = coord.SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
-
         >>> test_radius = 10*u.arcsec
         >>> test_height = 10*u.arcsec
         >>> test_width = 10*u.arcsec
 
         An example cone search:
         >>> photometry = OAC.query_region(coordinates=test_coords,
-                                          radius=test_radius,
-                                          quantity="photometry",
-                                          attribute=["time", "magnitude",
-                                                     "e_magnitude", "band",
-                                                     "instrument"])
+        ...                               radius=test_radius,
+        ...                               quantity="photometry",
+        ...                               attribute=["time", "magnitude",
+        ...                                          "e_magnitude", "band",
+        ...                                          "instrument"])
         >>> print(photometry[:5])
-        >>>  event      time   magnitude e_magnitude band instrument
-            -------- --------- --------- ----------- ---- ----------
-            GW170817 57743.334     20.44                r
-            GW170817 57790.358     21.39                r
-            GW170817 57791.323     21.34                r
-            GW170817 57792.326     21.26                r
-            GW170817 57793.335     21.10                r
+        event      time   magnitude e_magnitude band instrument
+        -------- --------- --------- ----------- ---- ----------
+        GW170817 57743.334     20.44                r
+        GW170817 57790.358     21.39                r
+        GW170817 57791.323     21.34                r
+        GW170817 57792.326     21.26                r
+        GW170817 57793.335     21.10                r
 
         An example box search:
         >>> photometry = OAC.query_region(coordinates=test_coords,
-                                          width=test_width, height=test_height,
-                                          quantity="photometry",
-                                          attribute=["time", "magnitude",
-                                                     "e_magnitude", "band",
-                                                     "instrument"])
+        ...                               width=test_width, height=test_height,
+        ...                               quantity="photometry",
+        ...                               attribute=["time", "magnitude",
+        ...                                          "e_magnitude", "band",
+        ...                                          "instrument"])
         >>> print(photometry[:5])
-        >>>  event      time   magnitude e_magnitude band instrument
-            -------- --------- --------- ----------- ---- ----------
-            GW170817 57743.334     20.44                r
-            GW170817 57790.358     21.39                r
-            GW170817 57791.323     21.34                r
-            GW170817 57792.326     21.26                r
-            GW170817 57793.335     21.10                r
+        event      time   magnitude e_magnitude band instrument
+        -------- --------- --------- ----------- ---- ----------
+        GW170817 57743.334     20.44                r
+        GW170817 57790.358     21.39                r
+        GW170817 57791.323     21.34                r
+        GW170817 57792.326     21.26                r
+        GW170817 57793.335     21.10                r
 
         These searches can be refined using the quantities, attributes, and
         arguments, as with query_object.
@@ -397,14 +395,13 @@ class OACClass(BaseQuery):
         >>> from astroquery.oac import OAC
         >>> photometry = OAC.get_photometry("SN2014J")
         >>> print(photometry[0:5])
-
-        >>> event       time         magnitude     e_magnitude band instrument
-            ------- -------------- ---------------- ----------- ---- ----------
-            SN2014J 56677.68443724 11.3932586807338                R
-            SN2014J 56678.31205141   11.00561836355                R
-            SN2014J 56678.81414463 11.2078655297216               i'
-            SN2014J 56678.84552409 12.5056184232995               g'
-            SN2014J 56678.84552409 11.4662920294307               r'
+        event       time         magnitude     e_magnitude band instrument
+        ------- -------------- ---------------- ----------- ---- ----------
+        SN2014J 56677.68443724 11.3932586807338                R
+        SN2014J 56678.31205141   11.00561836355                R
+        SN2014J 56678.81414463 11.2078655297216               i'
+        SN2014J 56678.84552409 12.5056184232995               g'
+        SN2014J 56678.84552409 11.4662920294307               r'
 
         The search can be refined using only the argument features of
         query_object. For example:
@@ -412,14 +409,13 @@ class OACClass(BaseQuery):
         >>> from astroquery.oac import OAC
         >>> photometry = OAC.get_photometry("SN2014J", argument="band=R")
         >>> print(photometry[0:5])
-
-        >>>  event       time         magnitude     e_magnitude band instrument
-            ------- -------------- ---------------- ----------- ---- ----------
-            SN2014J 56677.68443724 11.3932586807338                R
-            SN2014J 56678.31205141   11.00561836355                R
-            SN2014J       56678.87           11.105       0.021    R
-            SN2014J 56678.90828613 11.1235949161792                R
-            SN2014J 56679.06518967 11.0561795725355                R
+        event       time         magnitude     e_magnitude band instrument
+        ------- -------------- ---------------- ----------- ---- ----------
+        SN2014J 56677.68443724 11.3932586807338                R
+        SN2014J 56678.31205141   11.00561836355                R
+        SN2014J       56678.87           11.105       0.021    R
+        SN2014J 56678.90828613 11.1235949161792                R
+        SN2014J 56679.06518967 11.0561795725355                R
 
         """
         response = self.query_object_async(event=event,
@@ -469,13 +465,13 @@ class OACClass(BaseQuery):
         >>> test_time = 57740
         >>> spectrum = OAC.get_single_spectrum("GW170817", time=test_time)
         >>> print(spectrum[0:5])
-        >>> wavelength    flux
-            ---------- ----------
-            3501.53298 3.6411e-17
-            3501.73298 4.0294e-17
-            3502.33298 4.0944e-17
-            3502.53298 4.1159e-17
-            3502.73298 4.3485e-17
+        wavelength    flux
+        ---------- ----------
+        3501.53298 3.6411e-17
+        3501.73298 4.0294e-17
+        3502.33298 4.0944e-17
+        3502.53298 4.1159e-17
+        3502.73298 4.3485e-17
 
         This method does not allow further customization of searches.
 
@@ -523,13 +519,13 @@ class OACClass(BaseQuery):
         >>> from astroquery.oac import OAC
         >>> spectra = OAC.get_spectra("SN2014J")
         >>> print (spectra.keys())
-        >>> dict_keys(['SN2014J'])
+        dict_keys(['SN2014J'])
         >>> print (spectra["SN2014J"].keys())
-        >>> dict_keys(['spectra'])
+        dict_keys(['spectra'])
         >>> print (spectra["SN2014J"]["spectra"][0][0])
-        >>> 56680.0
+        56680.0
         >>> print (spectra["SN2014J"]["spectra"][0][1][0])
-        >>> ['5976.1440', '1.17293e-14']
+        ['5976.1440', '1.17293e-14']
 
         Note that the query must return a JSON-compliant dictionary which will
         have nested lists of MJD and [wavelength, flux] pairs.
