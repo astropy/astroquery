18a25ad32702cf934bb7af27e779f013e88430b2
David Collom <dcollom@dcollom-linux.lco.gtn>
2018-03-08 09:43:49 -0800

    added module to query MPC and local and remote unit tests

---
+++ astroquery/mpc/tests/test_mpc.py
@@ -0,0 +1,32 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import os
+import re
+
+import pytest
+
+from ... import mpc
+from ...utils.testing_tools import MockResponse
+from ...utils import commons
+from ...exceptions import TableParseError
+
+class MockResponseMPC(MockResponse):
+
+    def __init__(self, **kwargs):
+        super(MockResponseMPC, self).__init__(**kwargs)
+
+def read_data_file(filename):
+    query_args = dict()
+    with open(os.path.join(os.getcwd(), 'tests/data', filename), 'r') as data:
+        for line in data:
+            curr_line = line.split(',')
+            query_args[curr_line[0]] = curr_line[1]
+    return query_args
+
+def test_args_to_payload():
+    test_args = mpc.MPC._args_to_payload(name="eros", number=433)
+    assert test_args == {"name": "eros", "number": 433}
+
+def test_all_args_to_payload():
+    data_from_file = read_data_file("complete_query_by_name.data")
+    test_args = mpc.MPC._args_to_payload(data_from_file)
+    assert test_args == {"name": "eros", "number": 433}
