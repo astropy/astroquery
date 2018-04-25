72593f1373b93d37b0cf1b74bc49e4d2cd8b649a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-04-25 10:38:05 -0600

    default -> dr2

--- astroquery/gaia/__init__.py
+++ astroquery/gaia/__init__.py
@@ -24,7 +24,7 @@ class Conf(_config.ConfigNamespace):
     Configuration parameters for `astroquery.gaia`.
     """
 
-    MAIN_GAIA_TABLE = _config.ConfigItem("gaiadr1.gaia_source",
+    MAIN_GAIA_TABLE = _config.ConfigItem("gaiadr2.gaia_source",
                                          "GAIA source data table")
     MAIN_GAIA_TABLE_RA = _config.ConfigItem("ra",
                                             "Name of RA parameter in table")
