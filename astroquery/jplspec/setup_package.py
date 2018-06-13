ff88f402941606332c7cef95e1b5e8b5aca9e6c6
Giannina Guzman <gguzman2@villanova.edu>
2018-06-13 12:44:02 -0400

    Fixed paths to test data

--- astroquery/jplspec/setup_package.py
+++ astroquery/jplspec/setup_package.py
@@ -4,12 +4,10 @@ from __future__ import absolute_import
 import os
 
 def get_package_data():
-    paths_test1 = [os.path.join('data', 'CO.data')]
-    paths_test2 = [os.path.join('data', 'CO_6.data')]
-    paths_test3 = [os.path.join('data', 'multi.data')]
+    paths_test = [os.path.join('data', 'CO.data'),
+                  os.path.join('data', 'CO_6.data'),
+                  os.path.join('data', 'multi.data')]
     paths_data = [os.path.join('data', 'catdir.cat')]
 
-    return {'astroquery.jplspec.tests': paths_test1,
-            'astroquery.jplspec.tests': paths_test2,
-            'astroquery.jplspec.tests': paths_test3,
+    return {'astroquery.jplspec.tests': paths_test,
             'astroquery.jplspec': paths_data, }
