9d4c6a635a086d405d4f31d8b129d2d5c835eff4
Jaffe <tjaffe@gs66-sumac.ndc.nasa.gov>
2018-06-25 08:10:08 -0400

    Both tests working. Pickle files named correctkly.

--- astroquery/vo/tests/setup_package.py
+++ astroquery/vo/tests/setup_package.py
@@ -2,6 +2,6 @@ import os
 
 
 def get_package_data():
-    paths_test = [os.path.join('data', '*.json')]
+    paths_test = [os.path.join('data', '*.json')] + [os.path.join('data', '*.pkl')]
 
     return {'astroquery.vo.tests': paths_test}
