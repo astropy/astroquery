6fd913af47681bb7087fbca9231f528c10ce95cf
ruizca <angel.ruizca@gmail.com>
2018-10-15 20:30:48 +0300

    Fix PEP8 issues

--- astroquery/wfau/core.py
+++ astroquery/wfau/core.py
@@ -872,11 +872,11 @@ def clean_catalog(wfau_catalog, clean_band='K_1', badclass=-9999,
     """
 
     band = clean_band
-    mask = ((wfau_catalog[band + 'ERRBITS'] <= maxerrbits)
-            * (wfau_catalog[band + 'ERRBITS'] >= minerrbits)
-            * ((wfau_catalog['PRIORSEC'] == wfau_catalog['FRAMESETID'])
-            + (wfau_catalog['PRIORSEC'] == 0))
-            * (wfau_catalog[band + 'PPERRBITS'] < maxpperrbits)
+    mask = ((wfau_catalog[band + 'ERRBITS'] <= maxerrbits) *
+            (wfau_catalog[band + 'ERRBITS'] >= minerrbits) *
+            ((wfau_catalog['PRIORSEC'] == wfau_catalog['FRAMESETID']) +
+             (wfau_catalog['PRIORSEC'] == 0)) *
+            (wfau_catalog[band + 'PPERRBITS'] < maxpperrbits)
             )
     if band + 'CLASS' in wfau_catalog.colnames:
         mask *= (wfau_catalog[band + 'CLASS'] != badclass)
