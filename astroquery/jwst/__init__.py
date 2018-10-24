045fadb8f7cd935dd18765c13f17d0c35886933b
Raul Gutierrez <Raul.Gutierrez@sciops.esa.int>
2018-10-24 15:55:57 +0200

    First version of JWST prototype

---
+++ astroquery/jwst/__init__.py
@@ -0,0 +1,41 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+=============
+JWST TAP plus
+=============
+
+@author: Raul Gutierrez-Sanchez
+@contact: raul.gutierrez@sciops.esa.int
+
+European Space Astronomy Centre (ESAC)
+European Space Agency (ESA)
+
+Created on 23 oct. 2018
+
+
+"""
+
+from astroquery.utils.tap.core import TapPlus
+from astropy import config as _config
+
+
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for `astroquery.jwst`.
+    """
+
+    JWST_OBSERVATION_TABLE = _config.ConfigItem("jwst.observation",
+                                         "JWST observation table")
+    JWST_OBSERVATION_TABLE_RA = _config.ConfigItem("targetposition_coordinates_cval1",
+                                            "Name of RA parameter in table")
+    JWST_OBSERVATION_TABLE_DEC = _config.ConfigItem("targetposition_coordinates_cval2",
+                                             "Name of Dec parameter in table")
+
+
+conf = Conf()
+
+gaia = TapPlus(url="http://jwstdummytap.com", verbose=False)
+
+from .core import Jwst, JwstClass
+
+__all__ = ['Jwst', 'JwstClass', 'Conf', 'conf']
---
+++ astroquery/jwst/tests/__init__.py
@@ -0,0 +1,16 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+=============
+JWST TAP plus
+=============
+
+@author: Raul Gutierrez-Sanchez
+@contact: raul.gutierrez@sciops.esa.int
+
+European Space Astronomy Centre (ESAC)
+European Space Agency (ESA)
+
+Created on 24 oct. 2018
+
+
+"""
