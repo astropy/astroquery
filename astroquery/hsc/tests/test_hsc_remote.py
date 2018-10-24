84fe8caa250457461c461eaee048be4269b48990
ruizca <angel.ruizca@gmail.com>
2018-10-24 12:40:12 +0300

    Remote tests for Hsc service.

--- astroquery/hsc/tests/test_hsc_remote.py
+++ astroquery/hsc/tests/test_hsc_remote.py
@@ -1,54 +1,91 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function
 
-# performs similar tests as test_module.py, but performs
-# the actual HTTP request rather than monkeypatching them.
-# should be disabled or enabled at will - use the
-# remote_data decorator from astropy:
-
 import pytest
+#from pytest.mark import skip
 from astropy.tests.helper import remote_data
-from astropy.coordinates import SkyCoord
+from astropy.table import Table
+from astropy.io import fits
+import astropy.coordinates as coord
 import astropy.units as u
 
 from ... import hsc
 
 
 @remote_data
+@pytest.mark.skip(reason='Local testing. Works only with an HSC archive account.')
 class TestHsc:
     def test_query_region_async(self):
         hsc.core.Hsc.login()
         response = hsc.core.Hsc.query_region_async(
-            SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
-            radius=5 * u.arcsec)
-        assert response.ok
-        # assert response is not None
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'))
 
-#    def test_query_region(self):
-#        table = hsc.core.Ukidss.query_region(
-#            SkyCoord(l=10.625, b=-0.38, unit=(u.deg, u.deg), frame='icrs'),
-#            radius=6 * u.arcsec, programme_id='GPS')
-#        assert isinstance(table, Table)
-#        assert len(table) > 0
-#
-#    def test_get_images_1(self):
-#        images = hsc.core.Ukidss.get_images("m1")
-#        assert images is not None
-#
-#    def test_get_images_2(self):
-#        images = hsc.core.Ukidss.get_images(
-#            SkyCoord(l=49.489, b=-0.27, unit=(u.deg, u.deg), frame='galactic'),
-#            image_width=5 * u.arcmin)
-#        assert images is not None
-#
-#    def test_get_images_async(self):
-#        images = ukidss.core.Ukidss.get_images_async("m1")
-#        assert images is not None
-#
-#    def test_get_image_list(self):
-#        urls = ukidss.core.Ukidss.get_image_list(
-#            SkyCoord(ra=83.633083, dec=22.0145, unit=(u.deg, u.deg),
-#                     frame='icrs'),
-#            frame_type='all', waveband='all')
-#        assert len(urls) > 0
-#
+        assert response.ok
+
+    def test_query_region(self):
+        hsc.core.Hsc.login()
+        table = hsc.core.Hsc.query_region(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
+            radius=6 * u.arcsec)
+
+        assert isinstance(table, Table)
+        assert len(table) > 0
+
+    def test_get_images_cutout(self):
+        hsc.core.Hsc.login()
+        image = hsc.core.Hsc.get_images(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
+            image_width=10 * u.arcsec)
+
+        assert len(image) == 1
+        assert isinstance(image[0], fits.HDUList)
+
+    def test_get_images_async_cutout(self):
+        hsc.core.Hsc.login()
+        url_list = hsc.core.Hsc.get_images_async(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'))
+
+        assert len(url_list) == 1
+
+    def test_get_image_list_cutout(self):
+        hsc.core.Hsc.login()
+        url_list = hsc.core.Hsc.get_image_list(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'))
+
+        assert len(url_list) == 1
+
+# This test is not included because coadd images are very large (>100Mb)    
+#    def test_get_images_coadd(patch_request, patch_get_fits):
+#        image = hsc.core.Hsc.get_images(
+#            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
+#            radius=6 * u.arcsec)
+#    
+#        assert len(image) == 1
+#        assert isinstance(image[0], fits.HDUList)
+
+    def test_get_images_async_coadd(self):
+        hsc.core.Hsc.login()
+        url_list = hsc.core.Hsc.get_images_async(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
+            radius=6 * u.arcsec)
+
+        assert len(url_list) >= 1
+
+    def test_get_image_list_coadd(self):
+        hsc.core.Hsc.login()
+        url_list = hsc.core.Hsc.get_image_list(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
+            radius=6 * u.arcsec)
+
+        assert len(url_list) >= 1
+
+    def test_get_image_list_coadd_filters(self):
+        hsc.core.Hsc.login()
+        filters = ['g', 'r']
+        url_list = hsc.core.Hsc.get_image_list(
+            coord.SkyCoord(ra=34.0, dec=-5.0, unit='deg', frame='icrs'),
+            radius=6 * u.arcsec, filters=filters)
+
+        assert len(url_list) >= 1
+        assert len(url_list) <= len(filters)
+
