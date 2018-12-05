ed8070c2e0827abf5bb56a8eb2ea6b48655f455c
Christian Mesh <christianmesh1@gmail.com>
2018-12-05 10:52:51 -0500

    Update accept headers for session info

--- astroquery/mast/core.py
+++ astroquery/mast/core.py
@@ -705,7 +705,8 @@ class MastClass(QueryWithLogin):
         """
 
         # get user information
-        response = self._session.request("GET", self._SESSION_INFO_URL, headers={"Accept": "application/json"})
+        self._session.headers["Accept"] = "application/json"
+        response = self._session.request("GET", self._SESSION_INFO_URL)
 
         infoDict = json.loads(response.text)
 
