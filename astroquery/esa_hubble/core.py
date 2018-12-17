7a19157bb86a9b4fc7ffd432047ac57936400808
jduran <jduran@dhcp-10-66-197-14.esac.esa.int>
2018-12-17 09:26:39 +0100

    Refactoring after following change from Semyeong Oh:
    commit 2331108aa60f60fccb8a7c14c4e8e454d8d7011d
    Author: Semyeong Oh <semyeong.oh@gmail.com> 2018-10-11 15:32:29
    Committer: Semyeong Oh <semyeong.oh@gmail.com> 2018-10-11 15:32:29
    Parent: f7927cddd46dcfa7d2bf197508443eaf999a1c4c (PEP8:
    E111,E124,E201,E202,E203,E211,E221,E225,E231,E241,E251,E261,E265,E271,E272,E301,E302,E303,E305,E502,E703,E711,E712,E714,W291,W292,W293,W391)
    Branches: ehst, tap_1_2, astroquery/master, origin/ehst, origin/jwst,
    origin/master, origin/revert-1-tap-overhaul, origin/tap_1_2
    
    Clean get/set for TapTableMeta class

--- astroquery/esa_hubble/core.py
+++ astroquery/esa_hubble/core.py
@@ -290,7 +290,7 @@ class ESAHubbleClass(object):
         if only_names is True:
             table_names = []
             for t in tables:
-                table_names.append(t.get_name())
+                table_names.append(t.name)
             return table_names
         else:
             return tables
@@ -320,8 +320,8 @@ class ESAHubbleClass(object):
                                        verbose=verbose)
         columns = None
         for t in tables:
-            if str(t.get_name()) == str(table_name):
-                columns = t.get_columns()
+            if str(t.name) == str(table_name):
+                columns = t.columns
                 break
 
         if columns is None:
@@ -331,7 +331,7 @@ class ESAHubbleClass(object):
         if only_names is True:
             column_names = []
             for c in columns:
-                column_names.append(c.get_name())
+                column_names.append(c.name)
             return column_names
         else:
             return columns
