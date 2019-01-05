7f6d3761d62a2d9de709ee0e1db766cbd4ed17fb
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-01-06 02:15:41 +0530

    Add the init as specified in astroquery

--- astroquery/neos/__init__.py
+++ astroquery/neos/__init__.py
@@ -1,13 +1,32 @@
-"""Code related to NEOs.
-
-Functions related to NEOs and different NASA APIs.
-All of them are coded as part of SOCIS 2017 proposal.
-
-Notes
------
-
-The orbits returned by the functions in this package are in the
-:py:class:`~poliastro.frames.HeliocentricEclipticJ2000` frame.
-
 """
-from poliastro.neos import neows, dastcom5  # flake8: noqa
+NEOS Query Tool
+===============
+
+:Authors:
+1) Antonio Hidalgo (antoniohidalgo.inves@gmail.com)
+2) Juan Luis Cano Rodríguez (juanlu001@gmail.com)
+
+This module contains various methods for querying the
+NEOWS.
+
+All of the methods are coded as part of SOCIS 2017 for poliastro by Antonio Hidalgo[1].
+"""
+from astropy import config as _config
+
+
+class Conf(_config.ConfigNamespace):
+    NEOWS_URL = "https://api.nasa.gov/neo/rest/v1/neo/"
+    SBDB_URL = "https://ssd.jpl.nasa.gov/sbdb.cgi"
+
+    neows_server = _config.ConfigItem(NEOWS_URL, "Near Earth Object Web Service URL")
+
+    sbdb_server = _config.ConfigItem(SBDB_URL, "JPL Small-Body Database Browser URL")
+
+    timeout = _config.ConfigItem(60, "time limit for connecting to the server")
+
+
+conf = Conf()
+
+from .core import *
+
+__all__ = [Neows, NeowsClass]
