7b3a85a3b88d36a52c7b7482e7f08c193e91514b
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-01-06 12:11:48 +0530

    Add some tests for the module

---
+++ astroquery/neos/tests/test_neows.py
@@ -0,0 +1,19 @@
+import pytest
+
+from ...neos import Neows
+
+from astropy.table import Table
+
+def test_observations_from_spk_id():
+    result = Neows().from_spk_id(spk_id='2000433')
+    assert isinstance(result, Table)
+
+
+def test_name_from_spk_id():
+    result = Neows().from_spk_id(spk_id='2000433')
+    assert result[0][1] == '433 Eros (A898 PA)'
+
+
+def test_name_gives_correct_spk_id():
+    result = Neows()._spk_id_from_name(name='Eros')
+    assert result == '2000433'
