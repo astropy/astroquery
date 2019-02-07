0345ce7788f3e17a8ecef547be56891daddcf4e5
Michael Mommert <mommermiscience@gmail.com>
2019-02-06 21:39:59 -0700

    added path for test file

--- astroquery/mpc/setup_package.py
+++ astroquery/mpc/setup_package.py
@@ -4,6 +4,6 @@ import os
 
 def get_package_data():
     paths_test = [os.path.join('data', 'comet_object_C20112S1.json'),
-                  os.path.join('data', '*.html')]
+                  os.path.join('data', '*.html', 'mpc_obs.dat')]
 
     return {'astroquery.mpc.tests': paths_test}
