e3fe3dfb072b082b156c874082f5628c727d0844
Julien Burnier <julien.burnier@unige.ch>
2019-02-27 14:58:49 +0100

    Improve test DACE remote with 'business' tests. I check if radial_velocities processed with HARPS instrument are present in the Table and that at least 100 rows of a publication are present

--- astroquery/dace/tests/test_dace_remote.py
+++ astroquery/dace/tests/test_dace_remote.py
@@ -2,6 +2,8 @@ import unittest
 from astropy.tests.helper import remote_data
 from astroquery.dace import Dace
 
+HARPS_PUBLICATION = '2009A&A...493..639M'
+
 
 @remote_data
 class TestDaceClass(unittest.TestCase):
@@ -9,6 +11,10 @@ class TestDaceClass(unittest.TestCase):
     def test_should_get_radial_velocities(self):
         radial_velocities_table = Dace.query_radial_velocities('HD40307')
         assert radial_velocities_table is not None and 'rv' in radial_velocities_table.colnames
+        assert 'HARPS' in radial_velocities_table['ins_name']
+        assert HARPS_PUBLICATION in radial_velocities_table['pub_bibcode']
+        public_harps_data = [row for row in radial_velocities_table['pub_bibcode'] if HARPS_PUBLICATION in row]
+        assert len(public_harps_data) > 100
 
 
 if __name__ == "__main__":
