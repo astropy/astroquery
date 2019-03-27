dbec4240a90fdfdf778245f2332f70cc0049506e
Kris Stern <kakirastern@gmail.com>
2019-03-28 00:53:30 +0800

    Set http user-agent for Travis CI and AppVeyor when running test_internet

--- astroquery/tests/test_internet.py
+++ astroquery/tests/test_internet.py
@@ -1,14 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from astropy.tests.helper import remote_data
 import requests
+import os
 
 from .. import version
+
+IS_CI = os.environ.get('TRAVIS', False) or os.environ.get('APPVEYOR', False) == 'True'
 
 
 @remote_data
 def test_net_connection():
     R = requests.post('http://httpbin.org/post',
                       headers={'User-Agent':
-                               'astropy:astroquery.{0}'
-                               .format(version.version)})
+                      'astropy:astroquery-ci-tests.{0}'
+                      .format(version.version)})
     R.raise_for_status()
