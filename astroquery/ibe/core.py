0d38eff76b28439addfd2d32251153304ef4694d
Sean Lake <odysseus9672@gmail.com>
2019-04-24 15:48:56 +0800

    Fix bug in `list_missions`
    Add `.rstrip('/')` to prevent `os.path.basename` from returning empty strings.

--- astroquery/ibe/core.py
+++ astroquery/ibe/core.py
@@ -276,7 +276,7 @@ class IbeClass(BaseQuery):
 
             root = BeautifulSoup(response.text)
             links = root.findAll('a')
-            missions = [os.path.basename(a.attrs['href']) for a in links]
+            missions = [os.path.basename(a.attrs['href'].rstrip('/')) for a in links]
             self._missions = missions
 
         return missions
