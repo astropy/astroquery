ff4988c4221896c357d397308597dd7bb7bec17c
Sean Lake <odysseus9672@gmail.com>
2019-04-25 10:19:08 +0800

    break a long line

--- astroquery/ibe/core.py
+++ astroquery/ibe/core.py
@@ -276,7 +276,8 @@ class IbeClass(BaseQuery):
 
             root = BeautifulSoup(response.text)
             links = root.findAll('a')
-            missions = [os.path.basename(a.attrs['href']).rstrip('/') for a in links]
+            missions = [os.path.basename(a.attrs['href']).rstrip('/') \
+                            for a in links]
             self._missions = missions
 
         return missions
