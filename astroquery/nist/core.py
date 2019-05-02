61de3542f8026fa58a9e0b503f3e3f8c6f4bd275
Daniel Berke <berke.daniel@gmail.com>
2019-05-02 13:32:59 +1000

    Improved Python2/3 checking logic, moved html import to top of file.
    The code now checks for PY2 from six rather than PY3 for better forward
    compatibility.

--- astroquery/nist/core.py
+++ astroquery/nist/core.py
@@ -5,12 +5,15 @@ import re
 
 import astropy.units as u
 import astropy.io.ascii as asciitable
-from six import PY3
+from six import PY2
 
 from ..query import BaseQuery
 from ..utils import async_to_sync, prepend_docstr_nosections
 from . import conf
 from ..exceptions import TableParseError
+
+if not PY2:
+    import html  # html is available in Py3 stdlib, but not in Py2
 
 __all__ = ['Nist', 'NistClass']
 
@@ -170,8 +173,7 @@ class NistClass(BaseQuery):
         try:
             table = _strip_blanks(pre)
             table = links_re.sub(r'\1', table)
-            if PY3:
-                import html
+            if not PY2:
                 table = html.unescape(table)
             table = asciitable.read(table, Reader=asciitable.FixedWidth,
                                     data_start=3, delimiter='|')
