16df976ac638d3a08146582658b066d34f2eba40
jaladh-singhal <jaladhsinghal@gmail.com>
2019-10-08 18:43:32 +0530

    PEP8 fixes
    Fix PEP8 issue

--- astroquery/svo_fps/tests/test_svo_fps.py
+++ astroquery/svo_fps/tests/test_svo_fps.py
@@ -3,6 +3,7 @@ from astropy.tests.helper import remote_data
 
 from ..core import SvoFps
 
+
 @remote_data
 class TestSvoFpsClass:
 
@@ -11,14 +12,14 @@ class TestSvoFpsClass:
         # Check if column for Filter ID (named 'filterID') exists in table
         assert 'filterID' in table.to_table().colnames
 
-    @pytest.mark.parametrize('test_filter_id', 
+    @pytest.mark.parametrize('test_filter_id',
                             ['HST/NICMOS1.F113N', 'HST/WFPC2.f218w'])
     def test_get_transmission_data(self, test_filter_id):
         table = SvoFps.get_transmission_data(test_filter_id)
         # Check if data is downloaded properly, with > 0 rows
         assert len(table.to_table()) > 0
 
-    @pytest.mark.parametrize('test_facility, test_instrument', 
+    @pytest.mark.parametrize('test_facility, test_instrument',
                             [('HST', 'WFPC2'), ('Keck', None)])
     def test_get_filter_list(self, test_facility, test_instrument):
         table = SvoFps.get_filter_list(test_facility, test_instrument)
