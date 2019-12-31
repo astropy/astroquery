217eb743b9848b7b6ff95a11185d8884243e8e49
Jaladh Singhal <jaladhsinghal@gmail.com>
2019-12-31 17:27:32 +0530

    Made functions return astropy table instead of votable object

--- astroquery/svo_fps/tests/test_svo_fps.py
+++ astroquery/svo_fps/tests/test_svo_fps.py
@@ -10,18 +10,18 @@ class TestSvoFpsClass:
     def test_get_filter_index(self):
         table = SvoFps.get_filter_index()
         # Check if column for Filter ID (named 'filterID') exists in table
-        assert 'filterID' in table.to_table().colnames
+        assert 'filterID' in table.colnames
 
     @pytest.mark.parametrize('test_filter_id',
-                            ['HST/NICMOS1.F113N', 'HST/WFPC2.f218w'])
+                            ['HST/NICMOS1.F113N', 'HST/WFPC2-pc.f218w'])
     def test_get_transmission_data(self, test_filter_id):
         table = SvoFps.get_transmission_data(test_filter_id)
         # Check if data is downloaded properly, with > 0 rows
-        assert len(table.to_table()) > 0
+        assert len(table) > 0
 
     @pytest.mark.parametrize('test_facility, test_instrument',
                             [('HST', 'WFPC2'), ('Keck', None)])
     def test_get_filter_list(self, test_facility, test_instrument):
         table = SvoFps.get_filter_list(test_facility, test_instrument)
         # Check if column for Filter ID (named 'filterID') exists in table
-        assert 'filterID' in table.to_table().colnames
+        assert 'filterID' in table.colnames
