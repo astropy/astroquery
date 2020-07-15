4c48a62bc98e5f63c8d019e644a9f6e4455151bf
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-07-15 17:51:31 -0400

    more cleanup

--- astroquery/svo_fps/__init__.py
+++ astroquery/svo_fps/__init__.py
@@ -1,1 +1,10 @@
-from .core import SvoFps
+"""
+SVO FPS query tool
+==================
+
+Access to the Spanish Virtual Observatory (SVO) Filter Profile Service (FPS).
+"""
+
+from .core import SvoFps, SvoFpsClass
+
+__all__ = ["SvoFps", "SvoFpsClass"]
