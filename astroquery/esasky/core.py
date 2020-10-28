8aaf8adaa4980ecbdeda008790029e947512dece
Henrik Norman <honke.norman@gmail.com>
2020-10-28 15:18:46 +0100

    Added feature to query & download spectra

--- astroquery/esasky/core.py
+++ astroquery/esasky/core.py
@@ -5,6 +5,7 @@ import os
 import tempfile
 import tarfile
 import sys
+import re
 
 import six
 from astropy.io import fits
@@ -35,6 +36,7 @@ class ESASkyClass(BaseQuery):
     __ALL_STRING = "all"
     __CATALOGS_STRING = "catalogs"
     __OBSERVATIONS_STRING = "observations"
+    __SPECTRA_STRING = "spectra"
     __MISSION_STRING = "mission"
     __TAP_TABLE_STRING = "tapTable"
     __TAP_NAME_STRING = "tapName"
@@ -48,9 +50,6 @@ class ESASkyClass(BaseQuery):
 
     __HERSCHEL_STRING = 'herschel'
     __HST_STRING = 'hst'
-    __INTEGRAL_STRING = 'integral'
-    __AKARI_STRING = 'akari'
-    __ALMA_STRING = 'alma'
 
     __HERSCHEL_FILTERS = {
         'psw': '250',
@@ -58,7 +57,16 @@ class ESASkyClass(BaseQuery):
         'plw': '500',
         'mapb_blue': '70',
         'mapb_green': '100',
-        'mapr_': '160'}
+        'mapr_': '160',
+        'ssw.*ssc': 'SSW',
+        'slw.*ssc': 'SLW',
+        'sds': 'SSW+SLW',
+        'spss': 'SSW+SLW',
+        'bs_': 'blue',
+        'rs_': 'red',
+        'wbs': 'WBS',
+        'hrs': 'HRS'
+        }
 
     _MAPS_DOWNLOAD_DIR = "Maps"
     _isTest = ""
@@ -76,6 +84,13 @@ class ESASkyClass(BaseQuery):
         """
         return self._json_object_field_to_list(
             self._get_catalogs_json(), self.__MISSION_STRING)
+
+    def list_spectra(self):
+        """
+        Get a list of the mission names of the available spectra in ESASky
+        """
+        return self._json_object_field_to_list(
+            self._get_spectra_json(), self.__MISSION_STRING)
 
     def query_object_maps(self, position, missions=__ALL_STRING,
                           get_query_payload=False, cache=True, row_limit=DEFAULT_ROW_LIMIT):
@@ -316,6 +331,72 @@ class ESASkyClass(BaseQuery):
 
         return commons.TableList(query_result)
 
+    def query_region_spectra(self, position, radius, missions=__ALL_STRING,
+                          row_limit=DEFAULT_ROW_LIMIT, get_query_payload=False, cache=True):
+        """
+        This method queries a chosen region for all available spectra and returns a
+        TableList with all the found spectra metadata for the chosen missions and
+        region.
+
+        Parameters
+        ----------
+        position : str or `astropy.coordinates` object
+            Can either be a string of the location, eg 'M51', or the coordinates
+            of the object.
+        radius : str or `~astropy.units.Quantity`
+            The radius of a region.
+        missions : string or list, optional
+            Can be either a specific mission or a list of missions (all mission
+            names are found in list_spectra()) or 'all' to search in all
+            missions. Defaults to 'all'.
+        row_limit : int, optional
+            Determines how many rows that will be fetched from the database
+            for each mission. Can be -1 to select maximum (currently 100 000).
+            Defaults to 10000.
+        get_query_payload : bool, optional
+            When set to True the method returns the HTTP request parameters.
+            Defaults to False.
+        cache : bool, optional
+            When set to True the method will use a cache located at
+            .astropy/astroquery/cache. Defaults to True.
+
+        Returns
+        -------
+        table_list : `~astroquery.utils.TableList`
+            Each mission returns a `~astropy.table.Table` with the metadata
+            and observations available for the chosen missions and region.
+            It is structured in a TableList like this:
+            TableList with 8 tables:
+            '0:HERSCHEL' with 8 column(s) and 25 row(s)
+            '1:HST' with 8 column(s) and 735 row(s)
+
+        Examples
+        --------
+        query_region_spectra("m101", "14'", "all")
+
+        import astropy.units as u
+        query_region_spectra("265.05, 69.0", 14*u.arcmin, "Herschel")
+        query_region_spectra("265.05, 69.0", ["Herschel", "HST"])
+        """
+        sanitized_position = self._sanitize_input_position(position)
+        sanitized_radius = self._sanitize_input_radius(radius)
+        sanitized_missions = self._sanitize_input_spectra(missions)
+        sanitized_row_limit = self._sanitize_input_row_limit(row_limit)
+
+        query_result = {}
+
+        sesame_database.set('simbad')
+        coordinates = commons.parse_coordinates(sanitized_position)
+
+        self._store_query_result(query_result, sanitized_missions,
+                                    coordinates, sanitized_radius, sanitized_row_limit,
+                                    get_query_payload, cache, self._get_spectra_json())
+
+        if (get_query_payload):
+            return query_result
+
+        return commons.TableList(query_result)
+
     def get_maps(self, query_table_list, missions=__ALL_STRING,
                  download_dir=_MAPS_DOWNLOAD_DIR, cache=True):
         """
@@ -359,30 +440,24 @@ class ESASkyClass(BaseQuery):
 
         Examples
         --------
-        get_maps(query_region_catalogs("m101", "14'", "all"))
+        get_maps(query_region_maps("m101", "14'", "all"))
 
         """
         sanitized_query_table_list = self._sanitize_input_table_list(query_table_list)
         sanitized_missions = [m.lower() for m in self._sanitize_input_mission(missions)]
 
         maps = dict()
+        json = self._get_observation_json()
 
         for query_mission in sanitized_query_table_list.keys():
 
             if (query_mission.lower() in sanitized_missions):
-                # INTEGRAL & AKARI does not have a product url yet.
-                if (query_mission.lower() == self.__INTEGRAL_STRING
-                    or query_mission.lower() == self.__AKARI_STRING
-                    or query_mission.lower() == self.__ALMA_STRING):
-                    log.info(query_mission + " does not yet support downloading of "
-                            "fits files")
-                    continue
                 maps[query_mission] = (
                     self._get_maps_for_mission(
                         sanitized_query_table_list[query_mission],
                         query_mission,
                         download_dir,
-                        cache))
+                        cache, json))
 
         if all([maps[mission].count(None) == len(maps[mission])
                 for mission in maps]):
@@ -453,19 +528,14 @@ class ESASkyClass(BaseQuery):
                                                   get_query_payload=False,
                                                   cache=cache)
 
+        json = self._get_observation_json()
         for query_mission in map_query_result.keys():
-            # INTEGRAL & AKARI does not have a product url yet.
-            if (query_mission.lower() == self.__INTEGRAL_STRING
-                or query_mission.lower() == self.__AKARI_STRING):
-                log.info(query_mission + " does not yet support downloading of "
-                        "fits files")
-                continue
             maps[query_mission] = (
                 self._get_maps_for_mission(
                     map_query_result[query_mission],
                     query_mission,
                     download_dir,
-                    cache))
+                    cache, json))
 
         if all([maps[mission].count(None) == len(maps[mission])
                 for mission in maps]):
@@ -475,6 +545,82 @@ class ESASkyClass(BaseQuery):
         else:
             log.info("No maps found.")
         return maps
+
+    def get_spectra(self, position, radius=__ZERO_ARCMIN_STRING, missions=__ALL_STRING,
+                   download_dir=_MAPS_DOWNLOAD_DIR, cache=True):
+        """
+        This method gets the fits files available for the selected position and
+        mission and downloads all spectra to the the selected folder.
+        The method returns a dictionary which is divided by mission.
+        All mission except Herschel returns a list of HDULists.
+        For Herschel each item in the list is a dictionary where the used
+        filter is the key and the HDUList is the value.
+
+        Parameters
+        ----------
+        position : str or `astropy.coordinates` object
+            Can either be a string of the location, eg 'M51', or the coordinates
+            of the object.
+        radius : str or `~astropy.units.Quantity`, optional
+            The radius of a region. Defaults to 0.
+        missions : string or list, optional
+            Can be either a specific mission or a list of missions (all mission
+            names are found in list_spectra()) or 'all' to search in all
+            missions. Defaults to 'all'.
+        download_dir : string, optional
+            The folder where all downloaded maps should be stored.
+            Defaults to a folder called 'Maps' in the current working directory.
+        cache : bool, optional
+            When set to True the method will use a cache located at
+            .astropy/astroquery/cache. Defaults to True.
+
+        Returns
+        -------
+        maps : `dict`
+            All mission except Herschel returns a list of HDULists.
+            For Herschel each item in the list is a dictionary where the used
+            filter is the key and the HDUList is the value.
+            It is structured in a dictionary like this:
+            dict: {
+            'HERSCHEL': [{'70': [HDUList], '160': [HDUList]}, {'70': [HDUList], '160': [HDUList]}, ...],
+            'HST':[[HDUList], [HDUList], [HDUList], [HDUList], [HDUList], ...],
+            'XMM-EPIC' : [[HDUList], [HDUList], [HDUList], [HDUList], ...]
+            ...
+            }
+
+        Examples
+        --------
+        get_spectra("m101", "14'", "all")
+
+        """
+        sanitized_position = self._sanitize_input_position(position)
+        sanitized_radius = self._sanitize_input_radius(radius)
+        sanitized_missions = self._sanitize_input_spectra(missions)
+
+        spectra = dict()
+
+        spectra_query_result = self.query_region_spectra(sanitized_position,
+                                                  sanitized_radius,
+                                                  sanitized_missions,
+                                                  get_query_payload=False,
+                                                  cache=cache)
+        json = self._get_spectra_json()
+        for query_mission in spectra_query_result.keys():
+            spectra[query_mission] = (
+                self._get_maps_for_mission(
+                    spectra_query_result[query_mission],
+                    query_mission,
+                    download_dir,
+                    cache, json))
+
+        if all([spectra[mission].count(None) == len(spectra[mission])
+                for mission in spectra]):
+            log.info("No spectra got downloaded, check errors above.")
+        elif (len(spectra_query_result) > 0):
+            log.info("Spectra available at {}".format(os.path.abspath(download_dir)))
+        else:
+            log.info("No spectra found.")
+        return spectra
 
     def _sanitize_input_position(self, position):
         if (isinstance(position, str) or isinstance(position,
@@ -503,6 +649,17 @@ class ESASkyClass(BaseQuery):
         raise ValueError("Mission must be either a string or a list of "
                          "missions")
 
+    def _sanitize_input_spectra(self, spectra):
+        if isinstance(spectra, list):
+            return spectra
+        if isinstance(spectra, str):
+            if (spectra.lower() == self.__ALL_STRING):
+                return self.list_spectra()
+            else:
+                return [spectra]
+        raise ValueError("Spectra must be either a string or a list of "
+                         "Spectra")
+
     def _sanitize_input_catalogs(self, catalogs):
         if isinstance(catalogs, list):
             return catalogs
@@ -530,9 +687,14 @@ class ESASkyClass(BaseQuery):
             return row_limit
         raise ValueError("Row_limit must be an integer")
 
-    def _get_maps_for_mission(self, maps_table, mission, download_dir, cache):
+    def _get_maps_for_mission(self, maps_table, mission, download_dir, cache, json):
         maps = []
 
+        if(self.__PRODUCT_URL_STRING not in maps_table.keys()):
+            log.info(mission + " does not yet support downloading of "
+            "fits files")
+            return maps
+    
         if (len(maps_table[self.__PRODUCT_URL_STRING]) > 0):
             mission_directory = self._create_mission_directory(mission,
                                                                download_dir)
@@ -547,7 +709,8 @@ class ESASkyClass(BaseQuery):
                     if commons.ASTROPY_LT_4_1:
                         observation_id = observation_id.decode('utf-8')
                 else:
-                    observation_id = maps_table[self._get_tap_observation_id(mission)][index]
+                    # observation_id = maps_table[self._get_tap_observation_id(mission)][index]
+                    observation_id = maps_table[self._get_json_data_for_mission(json, mission)["uniqueIdentifierField"]][index]
                     if commons.ASTROPY_LT_4_1:
                         observation_id = observation_id.decode('utf-8')
                 log.info("Downloading Observation ID: {} from {}"
@@ -574,13 +737,11 @@ class ESASkyClass(BaseQuery):
                     try:
                         response.raise_for_status()
 
-                        file_name = ""
-                        if (product_url.endswith(self.__FITS_STRING)):
+                        file_name = (directory_path +
+                                        self._extract_file_name_from_response_header(response.headers))
+                        if (file_name == ""):
                             file_name = (directory_path +
                                          self._extract_file_name_from_url(product_url))
-                        else:
-                            file_name = (directory_path +
-                                         self._extract_file_name_from_response_header(response.headers))
 
                         fits_data = response.content
                         with open(file_name, 'wb') as fits_file:
@@ -616,19 +777,31 @@ class ESASkyClass(BaseQuery):
             i = 0
             for member in tar.getmembers():
                 member_name = member.name.lower()
-                if ('hspire' in member_name or 'hpacs' in member_name):
+                if ('hspire' in member_name or 'hpacs' in member_name
+                    or 'hifi' in member_name):
                     herschel_filter = self._get_herschel_filter_name(member_name)
                     tar.extract(member, directory_path)
-                    observation[herschel_filter] = fits.open(
-                        directory_path +
-                        member.name)
+                    if (herschel_filter in observation):
+                        if(isinstance(observation[herschel_filter][0], list)):
+                            observation[herschel_filter].append(
+                                fits.open(directory_path + member.name)
+                            )
+                        else:
+                            observation[herschel_filter] = [
+                                observation[herschel_filter],
+                                fits.open(directory_path + member.name)
+                            ]
+                    else: 
+                        observation[herschel_filter] = fits.open(
+                            directory_path +
+                            member.name)
                     i += 1
         os.remove(tar_file.name)
         return observation
 
     def _get_herschel_filter_name(self, member_name):
         for herschel_filter in self.__HERSCHEL_FILTERS.keys():
-            if herschel_filter in member_name:
+            if (bool(re.search(herschel_filter, member_name))):
                 return self.__HERSCHEL_FILTERS[herschel_filter]
 
     def _remove_extra_herschel_directory(self, file_and_directory_name,
@@ -658,25 +831,23 @@ class ESASkyClass(BaseQuery):
         if (content_disposition[start_index] == '\"'):
             start_index += 1
 
-        if (self.__FITS_STRING in content_disposition[start_index:]):
+        if (self.__FITS_STRING in content_disposition[start_index:].lower()):
             end_index = (
-                content_disposition.index(self.__FITS_STRING, start_index + 1) +
+                content_disposition.lower().index(self.__FITS_STRING, start_index + 1) +
                 len(self.__FITS_STRING))
             return content_disposition[start_index: end_index]
-        elif (self.__FTZ_STRING in content_disposition[start_index:]):
+        elif (self.__FTZ_STRING in content_disposition[start_index:].upper()):
             end_index = (
-                content_disposition.index(self.__FTZ_STRING, start_index + 1) +
+                content_disposition.upper().index(self.__FTZ_STRING, start_index + 1) +
                 len(self.__FTZ_STRING))
             return content_disposition[start_index: end_index]
-        elif (self.__TAR_STRING in content_disposition[start_index:]):
+        elif (self.__TAR_STRING in content_disposition[start_index:].lower()):
             end_index = (
-                content_disposition.index(self.__TAR_STRING, start_index + 1) +
+                content_disposition.lower().index(self.__TAR_STRING, start_index + 1) +
                 len(self.__TAR_STRING))
             return content_disposition[start_index: end_index]
         else:
-            raise ValueError("Could not find file name in header. "
-                             "Content disposition: {}.".format(
-                                 content_disposition))
+            return ""
 
     def _extract_file_name_from_url(self, product_url):
         start_index = product_url.rindex("/") + 1
@@ -766,6 +937,9 @@ class ESASkyClass(BaseQuery):
     def _get_catalogs_json(self):
         return self._fetch_and_parse_json(self.__CATALOGS_STRING)
 
+    def _get_spectra_json(self):
+        return self._fetch_and_parse_json(self.__SPECTRA_STRING)
+
     def _fetch_and_parse_json(self, object_name):
         url = self.URLbase + "/" + object_name
         response = self._request(
@@ -790,9 +964,6 @@ class ESASkyClass(BaseQuery):
         for index in range(len(json)):
             if(json[index][self.__MISSION_STRING].lower() == mission.lower()):
                 return json[index]
-
-    def _get_tap_observation_id(self, mission):
-        return self._get_json_data_for_mission(self._get_observation_json(), mission)["uniqueIdentifierField"]
 
     def _create_request_payload(self, query):
         return {'REQUEST': 'doQuery', 'LANG': 'ADQL', 'FORMAT': 'VOTABLE',
