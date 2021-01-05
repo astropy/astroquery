e0e30ddba47e0809057db809ee7c544a5967b935
Brigitta Sipocz <bsipocz@gmail.com>
2021-01-05 00:33:44 -0800

    Use the helpers setup rather than setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import glob
 import os
 import sys
 
@@ -9,16 +8,11 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 import ah_bootstrap
-from setuptools import setup
 
 import builtins
 
 builtins._ASTROPY_SETUP_ = True
 
-from astropy_helpers.git_helpers import get_git_devstr
-from astropy_helpers.version_helpers import generate_version_py
-from astropy_helpers.distutils_helpers import is_distutils_display_option
-
-from setuptools.config import read_configuration
+from astropy_helpers.setup_helpers import setup
 
 setup()
