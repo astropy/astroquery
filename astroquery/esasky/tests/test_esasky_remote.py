e3e63ad3b518ee2427dbf2207264199b9c516336
Henrik Norman <honke.norman@gmail.com>
2021-01-06 16:18:38 +0100

    Updated esasky remote tests

--- astroquery/esasky/tests/test_esasky_remote.py
+++ astroquery/esasky/tests/test_esasky_remote.py
@@ -13,7 +13,6 @@ ESASkyClass = esasky.core.ESASkyClass()
 
 @pytest.mark.remote_data
 class TestESASky:
-
     ESASkyClass._isTest = "Remote Test"
 
     def test_esasky_query_region_maps(self):
@@ -25,22 +24,22 @@ class TestESASky:
         assert isinstance(result, TableList)
 
     @pytest.mark.bigdata
-    @pytest.mark.xfail(reason='Unknown.  This regularly fails on travis, but not locally.')
     def test_esasky_get_images(self):
         download_directory = "ESASkyRemoteTest"
         if not os.path.exists(download_directory):
             os.makedirs(download_directory)
 
-        missionsExceptHstAndIntegral = ESASkyClass.list_maps()
-        missionsExceptHstAndIntegral.remove("HST")
-        missionsExceptHstAndIntegral.remove("INTEGRAL")
-        # Added May 10, 2018: there are no hits from SUZAKU
-        missionsExceptHstAndIntegral.remove("SUZAKU")
+        missions = ESASkyClass.list_maps()
+        # Remove very large map missions & missions with many results
+        # & missions without proper download url (INTEGRAL, SUZAKU, ALMA, AKARI)
+        missions = [mission for mission in missions if mission not in
+                    ("HST-OPTICAL", "HST-IR", "HST-UV", "XMM-OM-UV", "Chandra", "INTEGRAL", "SUZAKU", "ALMA",
+                     "Spitzer", "AKARI")]
 
-        ESASkyClass.get_images("M51", missions=missionsExceptHstAndIntegral, download_dir=download_directory)
+        ESASkyClass.get_images("M51", missions=missions, download_dir=download_directory)
 
-        for mission in missionsExceptHstAndIntegral:
-            file_path = os.path.join(download_directory, ESASkyClass._MAPS_DOWNLOAD_DIR, mission)
+        for mission in missions:
+            file_path = os.path.join(download_directory, mission)
             assert os.path.exists(file_path)
 
         shutil.rmtree(download_directory)
@@ -50,26 +49,26 @@ class TestESASky:
         if not os.path.exists(download_directory):
             os.makedirs(download_directory)
 
-        # ISO is only ~ 800 kB
-        missions = ['ISO']
+        # ISO is only ~ 163 kB
+        missions = ['ISO-IR']
 
-        ESASkyClass.get_images("M51", missions=missions, download_dir=download_directory)
+        ESASkyClass.get_images("M6", radius="12arcmin", missions=missions, download_dir=download_directory)
 
         for mission in missions:
-            file_path = os.path.join(download_directory, ESASkyClass._MAPS_DOWNLOAD_DIR, mission)
+            file_path = os.path.join(download_directory, mission)
             assert os.path.exists(file_path)
 
         shutil.rmtree(download_directory)
 
-    @pytest.mark.xfail(reason='Internal Error. Please try later')
+    @pytest.mark.bigdata
     def test_esasky_get_images_hst(self):
         download_directory = "ESASkyRemoteTest"
         if not os.path.exists(download_directory):
             os.makedirs(download_directory)
 
-        ESASkyClass.get_images("M7", radius="10 arcmin", missions="HST", download_dir=download_directory)
+        ESASkyClass.get_images("M11", radius="2.1 deg", missions="HST-UV", download_dir=download_directory)
 
-        file_path = os.path.join(download_directory, ESASkyClass._MAPS_DOWNLOAD_DIR, "HST")
+        file_path = os.path.join(download_directory, "HST-UV")
         assert os.path.exists(file_path)
 
         shutil.rmtree(download_directory)
@@ -87,23 +86,17 @@ class TestESASky:
         if not os.path.exists(download_directory):
             os.makedirs(download_directory)
 
-        file_path = os.path.join(download_directory,
-                                 ESASkyClass._MAPS_DOWNLOAD_DIR, 'ISO')
+        file_path = os.path.join(download_directory, 'ISO-IR')
 
         all_maps = ESASkyClass.query_object_maps("M51")
-        isomaps = ESASkyClass.query_object_maps("M51", missions='ISO')
-
+        isomaps = ESASkyClass.query_object_maps("M51", missions='ISO-IR')
         # Remove a few maps, so the other list will have downloadable ones, too
-        isomaps['ISO'].remove_rows([0, 1])
-
-        isomaps2 = dict({'ISO': all_maps['ISO'][:2]})
-
+        isomaps['ISO-IR'].remove_rows([0, 1])
         ESASkyClass.get_maps(isomaps, download_dir=download_directory)
+        assert len(os.listdir(file_path)) == len(all_maps['ISO-IR']) - 2
 
-        assert len(os.listdir(file_path)) == len(all_maps['ISO']) - 2
-
+        isomaps2 = dict({'ISO-IR': all_maps['ISO-IR'][:2]})
         ESASkyClass.get_maps(isomaps2, download_dir=download_directory)
-
-        assert len(os.listdir(file_path)) == len(all_maps['ISO'])
+        assert len(os.listdir(file_path)) == len(all_maps['ISO-IR'])
 
         shutil.rmtree(download_directory)
