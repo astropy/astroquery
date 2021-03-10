be32a9893dcaa32a8ff05f87cf444f28f0e910a5
Michael S. P. Kelley <msk@astro.umd.edu>
2021-03-10 12:47:32 -0500

    More doc fixes

--- astroquery/jplsbdb/tests/test_jplsbdb.py
+++ astroquery/jplsbdb/tests/test_jplsbdb.py
@@ -88,11 +88,11 @@ def test_missing_value(patch_request):
 
 def test_quantities(patch_request):
     """Make sure query returns quantities.
-    
+
     Regression test for astroquery #2011.
-    
+
     """
-    
+
     sbdb = SBDB.query('Ceres', id_type='search', phys=True,
                       alternate_id=True, full_precision=True,
                       covariance='mat', validity=True,
