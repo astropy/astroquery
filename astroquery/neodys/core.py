e1022bd86be607bed32b7e00af29a4c19c8acf98
Kathleen Kiker <kathleen@b612foundation.orgm>
2021-03-16 15:27:32 -0400

    Adding init files

--- astroquery/neodys/core.py
+++ astroquery/neodys/core.py
@@ -1,8 +1,14 @@
 
 import requests
 import pandas as pd
+from . import conf
+
+__all__ = ['NEODyS', 'NEODySClass']  
+
 
 class NEODySClass():
+    NEODYS_URL = conf.server
+    TIMEOUT = conf.timeout
 
     def __init__(self):
         super(NEODySClass, self).__init__()
@@ -15,55 +21,49 @@ class NEODySClass():
 
         if OrbitElementType == "Keplerian":
             if Epoch == "Near_Middle":
-                NEODyS_url = f'https://newton.spacedys.com/~neodys2/epoch//{Object_ID}.ke0'
-                print(NEODyS_url)
+                Object_url = f'{NEODYS_URL}/{Object_ID}.ke0'
             elif Epoch == "Near_Present":
-                NEODyS_url = f'https://newton.spacedys.com/~neodys2/epoch//{Object_ID}.ke1'
-                print(NEODyS_url)
+                Object_url = f'{NEODYS_URL}/{Object_ID}.ke1'
             else:
                 print("Epoch must be Near_Middle or Near_Present")
         elif OrbitElementType == "Equinoctial":
             if Epoch == "Near_Middle":
-                NEODyS_url = f'https://newton.spacedys.com/~neodys2/epoch//{Object_ID}.eq0'
-                print(NEODyS_url)
+                Object_url = f'https://newton.spacedys.com/~neodys2/epoch//{Object_ID}.eq0'
             elif Epoch == "Near_Present":
-                NEODyS_url = f'https://newton.spacedys.com/~neodys2/epoch//{Object_ID}.eq1'
-                print(NEODyS_url)
+                Object_url = f'https://newton.spacedys.com/~neodys2/epoch//{Object_ID}.eq1'
             else:
                 print("Epoch must be Near_Middle or Near_Present")
         else:
             print("OrbitElementType must be Keplerian or Equinoctial")
             
-        html_out = requests.get(NEODyS_url)
+        html_out = requests.get(Object_url)
         html_text = (html_out.text).split('\n')
+
+        Results = {}
 
         for line in html_text:
             if 'KEP' in line:
-                KEP = [float(x) for x in line.split()[1:]]
+                Results["KEP"] = [float(x) for x in line.split()[1:]]
             if 'EQU' in line:
-                EQU = [float(x) for x in line.split()[1:]]
+                Results["EQU"] = [float(x) for x in line.split()[1:]]
             if 'MJD' in line:
-                MJD = line.split()[1:]
+                Results["MJD"] = line.split()[1:]
             if 'MAG' in line:
-                MAG = [float(x) for x in line.split()[1:]]
+                Results["MAG"] = [float(x) for x in line.split()[1:]]
             if 'LSP' in line:
-                LSP = [int(x) for x in line.split()[1:]]
+                Results["LSP"] = [int(x) for x in line.split()[1:]]
             if 'COV' in line:
                 COV.extend([float(x) for x in line.split()[1:]])
             if 'COR' in line:
                 COR.extend([float(x) for x in line.split()[1:]])
             if 'NOR' in line:
                 NOR.extend([float(x) for x in line.split()[1:]])
-        
-        print("Time = " + str(MJD))
-        print("MAG = " + str(MAG))
-        print("LSP = " + str(LSP))
-        print("COV = " + str(COV))
-        if OrbitElementType == "Keplerian":
-            print("KEP = " + str(KEP))
-            print("COR = " + str(COR))
-        if OrbitElementType == "Equinoctial":
-            print("EQU = " + str(EQU))
-            print("NOR = " + str(NOR))
+        Results["COV"] = COV
+        Results["COR"] = COR
+        Results["NOR"] = NOR
 
-        return 
+        return Results
+
+
+
+NEODyS = NEODySClass()
