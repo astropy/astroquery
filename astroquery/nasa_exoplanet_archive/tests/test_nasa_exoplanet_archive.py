454cebc4427b82ffc134038d2c5e3fb5e1380525
Ricky Nilsson <ricky.nilsson@gmail.com>
2021-05-04 10:33:05 -0700

    Tests updated and verfied.

--- astroquery/nasa_exoplanet_archive/tests/test_nasa_exoplanet_archive.py
+++ astroquery/nasa_exoplanet_archive/tests/test_nasa_exoplanet_archive.py
@@ -7,6 +7,7 @@ import sys
 from urllib.parse import urlencode
 
 import astropy.units as u
+from astropy.io.ascii import write as ap_write
 import numpy as np
 import pkg_resources
 import pytest
@@ -17,11 +18,12 @@ from astropy.utils.exceptions import AstropyDeprecationWarning
 
 from ...exceptions import InputWarning, InvalidQueryError, NoResultsWarning
 from ...utils.testing_tools import MockResponse
-from ..core import NasaExoplanetArchive, conf, InvalidTableError
+from ..core import NasaExoplanetArchive, conf, InvalidTableError, request_to_sql
 
 MAIN_DATA = pkg_resources.resource_filename("astroquery.nasa_exoplanet_archive", "data")
 TEST_DATA = pkg_resources.resource_filename(__name__, "data")
 RESPONSE_FILE = os.path.join(TEST_DATA, "responses.json")
+os.environ["NASA_EXOPLANET_ARCHIVE_GENERATE_RESPONSES"] = '1' # Activate for generating responses 
 
 # TAP supported: ps, pscomppars, keplernames, k2names. API support: all others
 # TODO: add tables transitspec and emissionspec
@@ -29,7 +31,7 @@ RESPONSE_FILE = os.path.join(TEST_DATA, "responses.json")
 ALL_TABLES = [
     # ("ps", dict(where="hostname='Kepler-11'")),
     # ("pscomppars", dict(where="hostname='WASP-166'")),
-    ("microlensing", dict(where="plntname='MOA-2010-BLG-353L b'")),
+    # ("ml", dict(where="pl_name='MOA-2010-BLG-353L b'")), # new microlensing table not ready for the wild yet
     ("cumulative", dict(where="kepid=10601284")),
     ("koi", dict(where="kepid=10601284")),
     ("q1_q17_dr25_sup_koi", dict(where="kepid=10601284")),
@@ -84,6 +86,7 @@ def mock_get(self, method, url, *args, **kwargs):  # pragma: nocover
         responses = {}
 
 
+    table = params["table"]
     # Set base URL depending on whether TAP or API for access
     if table in ["ps", "pscomppars", "k2names", "keplernames"]:
         assert url == conf.url_tap
@@ -92,19 +95,18 @@ def mock_get(self, method, url, *args, **kwargs):  # pragma: nocover
         assert url == conf.url_api
         service_type = "api"
 
-
-    # Work out where the expected response is saved
-    table = params["table"]
+    # Work out where the expected response is saved  
     if service_type == "api":
         key = urlencode(sorted(params.items())) # construct URL query string from params
     else:
-        key = 
+        key = request_to_sql(sorted(params.items())) # construct SQL query string from params
     try:
-        # Find index of URL query string in list of URLs for specific table.
+        # Find index of query string in list of queries for specific table.
         # Set to empty list if table isn't in dictionary, which will return ValueError
         index = responses.get(table, []).index(key)
     except ValueError:
         index = -1
+
 
     # If the NASA_EXOPLANET_ARCHIVE_GENERATE_RESPONSES environment variable is set, we make a
     # remote request if necessary. Otherwise we throw a ValueError.
@@ -116,9 +118,8 @@ def mock_get(self, method, url, *args, **kwargs):  # pragma: nocover
         index = len(responses[table]) - 1 # set index to write new entry
         if table in ["ps", "pscomppars", "k2names", "keplernames"]:
             tap = pyvo.dal.tap.TAPService(baseurl=url)
-            tap.create_query()
-            # with requests.Session() as session: # TODO: this has be changed to a TAP request
-            #     resp = session.old_request(method, url, params=params)
+            resp = tap.run_async(query=key, language="ADQL")
+            ap_write(resp.to_table(), output=os.path.join(TEST_DATA, "{0}_expect_{1}.txt".format(table, index)))
         else: # use basic http request of URL
             with requests.Session() as session:
                 resp = session.old_request(method, url, params=params)
@@ -141,17 +142,19 @@ def patch_get(request):  # pragma: nocover
         mp = request.getfuncargvalue("monkeypatch")
 
     # Keep track of the original function so that we can use it to generate the expected responses
+    # TODO: Need to add similar for TAP here ...
     requests.Session.old_request = requests.Session.request
     mp.setattr(requests.Session, "request", mock_get)
     return mp
 
 
 def test_regularize_object_name(patch_get):
+    # aliastable temporarily inacessible, so no regularization possible
     assert NasaExoplanetArchive._regularize_object_name("kepler 2") == "HAT-P-7"
     assert NasaExoplanetArchive._regularize_object_name("kepler 1 b") == "TrES-2 b"
 
     with pytest.warns(NoResultsWarning) as warning:
-        NasaExoplanetArchive._regularize_object_name("not a planet")
+        NasaExoplanetArchive._regularize_object_name("not a planet") # 
     assert "No aliases found for name: 'not a planet'" == str(warning[0].message)
 
 
@@ -206,7 +209,7 @@ def test_all_tables(patch_get, table, query):
         assert isinstance(data[col], SkyCoord) or not isinstance(data[col].unit, u.UnrecognizedUnit)
 
 
-def test_select(patch_get):
+def test_select(): # removed patch_get for now
     payload = NasaExoplanetArchive.query_criteria(
         "ps",
         select=["hostname", "pl_name"],
@@ -224,36 +227,30 @@ def test_select(patch_get):
     _compare_tables(table1, table2)
 
 
-def test_warnings(patch_get):
+def test_warnings(): # removed patch_get for now
     with pytest.warns(NoResultsWarning):
         NasaExoplanetArchive.query_criteria("ps", where="hostname='not a host'")
 
     with pytest.warns(InputWarning):
         NasaExoplanetArchive.query_object("HAT-P-11 b", where="nothing")
 
-    with pytest.warns(AstropyDeprecationWarning) as warning:
-        NasaExoplanetArchive.query_planet(
-            "HAT-P-11 b", all_columns=False, show_progress=True, table_path="nothing"
-        )
-    assert len(warning) == 3
-
     with pytest.raises(InvalidQueryError) as error:
         NasaExoplanetArchive.query_object("HAT-P-11 b", table="cumulative")
     assert "Invalid table 'cumulative'" in str(error)
 
 
-def test_query_region(patch_get):
+def test_query_region(): # removed patch_get for now
     coords = SkyCoord(ra=330.79488 * u.deg, dec=18.8843 * u.deg)
     radius = 0.001
-    table1 = NasaExoplanetArchive.query_region("ps", coords, radius * u.deg)
+    table1 = NasaExoplanetArchive.query_region("pscomppars", coords, radius * u.deg)
     assert len(table1) == 1
     assert table1["hostname"] == "HD 209458"
 
-    table2 = NasaExoplanetArchive.query_region("ps", coords, radius)
+    table2 = NasaExoplanetArchive.query_region("pscomppars", coords, radius)
     _compare_tables(table1, table2)
 
 
-def test_format(patch_get):
+def test_format(): # removed patch_get for now
     table1 = NasaExoplanetArchive.query_object("HAT-P-11 b")
     table2 = NasaExoplanetArchive.query_object("HAT-P-11 b", format="votable")
     _compare_tables(table1, table2)
