1f2f366a93a82115a439583b2d6f4fa381f130e8
Brigitta Sipőcz <bsipocz@gmail.com>
2021-07-19 10:29:32 -0700

    Use sphinx-astropy rather than astropy-helpers

--- docs/conf.py
+++ docs/conf.py
@@ -29,18 +29,13 @@ import datetime
 import os
 import sys
 
-try:
-    import astropy_helpers
-except ImportError:
-    # Building from inside the docs/ directory?
-    if os.path.basename(os.getcwd()) == 'docs':
-        a_h_path = os.path.abspath(os.path.join('..', 'astropy_helpers'))
-        if os.path.isdir(a_h_path):
-            sys.path.insert(1, a_h_path)
-
 # Load all of the global Astropy configuration
-from astropy_helpers.sphinx.conf import *
-import urllib
+try:
+    from sphinx_astropy.conf.v1 import *  # noqa
+except ImportError:
+    print('ERROR: the documentation requires the sphinx-astropy package to '
+          'be installed')
+    sys.exit(1)
 
 from configparser import ConfigParser
 conf = ConfigParser()
