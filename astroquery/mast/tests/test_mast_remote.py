84a685653b336f848ddeb55febb651672668b392
Jennifer V Medina <jmedina@stsci.edu>
2021-10-07 15:48:46 -0400

    Update astroquery/mast/tests/test_mast_remote.py
    Co-authored-by: Brigitta Sipőcz <b.sipocz@gmail.com>

--- astroquery/mast/tests/test_mast_remote.py
+++ astroquery/mast/tests/test_mast_remote.py
@@ -233,7 +233,7 @@ class TestMast:
         filenames2 = list(result2['productFilename'])
         assert isinstance(result1, Table)
         assert len(result1) == len(result2)
-        assert set(filenames1).difference(set(filenames2)) == set()
+        assert set(filenames1) == set(filenames2)
 
         result1 = mast.Observations.get_product_list(mult_obs_ids)
         result2 = mast.Observations.get_product_list(observations[0:2])
@@ -241,7 +241,7 @@ class TestMast:
         filenames2 = list(result2['productFilename'])
         assert isinstance(result1, Table)
         assert len(result1) == len(result2)
-        assert set(filenames1).difference(set(filenames2)) == set()
+        assert set(filenames1) == set(filenames2)
 
         obsLoc = np.where(observations["obs_id"] == 'ktwo200071160-c92_lc')
         result = mast.Observations.get_product_list(observations[obsLoc])
