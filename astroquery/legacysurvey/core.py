87e3c0b3606618488e50193b71ede79d89e2f4da
burnout87 <gabriele.barni@gmail.com>
2021-10-18 16:21:07 +0200

    option to still not use tap, default uses it

--- astroquery/legacysurvey/core.py
+++ astroquery/legacysurvey/core.py
@@ -174,7 +174,7 @@ class LegacySurveyClass(BaseQuery):
     # actual HTTP request and returns the HTTP response
 
     def query_region_async(self, coordinates, radius,
-                           get_query_payload=False, cache=True, data_release=9):
+                           get_query_payload=False, cache=True, data_release=9, use_tap=True):
         """
         Queries a region around the specified coordinates.
 
@@ -196,23 +196,101 @@ class LegacySurveyClass(BaseQuery):
             All async methods should return the raw HTTP response.
         """
 
-        # TAP query
-        # Download tractor catalogue
-        url = 'https://datalab.noirlab.edu/tap'
-        tap_service = vo.dal.TAPService(url)
-        qstr = "SELECT all * FROM ls_dr" + str(data_release) + ".tractor WHERE dec>" + str(coordinates.dec.deg - radius.deg) + " and dec<" + str(
-            coordinates.dec.deg + radius.deg) + " and ra>" + str(coordinates.ra.deg - radius.deg / cos(coordinates.dec.deg * pi / 180.)) + " and ra<" + str(
-            coordinates.ra.deg + radius.deg / cos(coordinates.dec.deg * pi / 180))
-        print(qstr)
+        if use_tap:
+            # TAP query`
+            # Download tractor catalogue
+            url = 'https://datalab.noirlab.edu/tap'
+            tap_service = vo.dal.TAPService(url)
+            qstr = "SELECT all * FROM ls_dr" + str(data_release) + ".tractor WHERE dec>" + str(coordinates.dec.deg - radius.deg) + " and dec<" + str(
+                coordinates.dec.deg + radius.deg) + " and ra>" + str(coordinates.ra.deg - radius.deg / cos(coordinates.dec.deg * pi / 180.)) + " and ra<" + str(
+                coordinates.ra.deg + radius.deg / cos(coordinates.dec.deg * pi / 180))
+            print(qstr)
 
-        tap_result = tap_service.run_sync(qstr)
-        tap_result = tap_result.to_table()
-        # ra = tap_result['ra']
-        # dec = tap_result['dec']
-        mask = tap_result['type'] != 'D'
-        masked_table = tap_result[mask]
+            tap_result = tap_service.run_sync(qstr)
+            tap_result = tap_result.to_table()
+            # filter out duplicated lines from the table
+            mask = tap_result['type'] != 'D'
+            filtered_table = tap_result[mask]
 
-        return masked_table
+            return filtered_table
+        else:
+            # call the brick list
+            table_north = self.query_brick_list(data_release=data_release, emisphere="north")
+            table_south = self.query_brick_list(data_release=data_release, emisphere="south")
+            # needed columns: ra1, ra2, dec1, dec2 (corners of the bricks), and also brickname
+            # radius not used for the moment, but it will be in the future
+            # must find the brick within ra1 and ra2
+            dec = coordinates.dec.deg
+            ra = coordinates.ra.deg
+
+            responses = []
+
+            # north table extraction
+            brick_name = table_north['brickname']
+            ra1 = table_north['ra1']
+            dec1 = table_north['dec1']
+            ra2 = table_north['ra2']
+            dec2 = table_north['dec2']
+
+            corners1 = SkyCoord(ra1, dec1, unit="deg")
+            corners2 = SkyCoord(ra1, dec2, unit="deg")
+            corners3 = SkyCoord(ra2, dec1, unit="deg")
+            corners4 = SkyCoord(ra2, dec2, unit="deg")
+
+            sep1 = coordinates.separation(corners1)
+            sep2 = coordinates.separation(corners2)
+            sep3 = coordinates.separation(corners3)
+            sep4 = coordinates.separation(corners4)
+
+            t0 = time.time()
+            print("Beginning processing bricks northern emishpere")
+            for i in range(len(table_north)):
+                if ((ra1[i] < ra < ra2[i]) and (dec1[i] < dec < dec2[i])) \
+                        or (sep1[i] < radius) or (sep2[i] < radius) or (sep3[i] < radius) or (sep4[i] < radius):
+                    # row_north_list.append(table_north[i])
+                    brickname = brick_name[i]
+                    raIntPart = "{0:03}".format(int(ra1[i]))
+                    URL = f"{self.URL}/dr{data_release}/north/tractor/{raIntPart}/tractor-{brickname}.fits"
+
+                    response = requests.get(URL)
+                    if response is not None and response.status_code == 200:
+                        responses.append(response)
+            print("Completion processing bricks northern emishpere, total time: ", time.time() - t0)
+
+            # south table extraction
+            brick_name = table_south['brickname']
+            ra1 = table_south['ra1']
+            dec1 = table_south['dec1']
+            ra2 = table_south['ra2']
+            dec2 = table_south['dec2']
+
+            corners1 = SkyCoord(ra1, dec1, unit="deg")
+            corners2 = SkyCoord(ra1, dec2, unit="deg")
+            corners3 = SkyCoord(ra2, dec1, unit="deg")
+            corners4 = SkyCoord(ra2, dec2, unit="deg")
+
+            sep1 = coordinates.separation(corners1)
+            sep2 = coordinates.separation(corners2)
+            sep3 = coordinates.separation(corners3)
+            sep4 = coordinates.separation(corners4)
+
+            t0 = time.time()
+            print("Beginning processing bricks southern emisphere")
+            for i in range(len(table_south)):
+                if ((ra1[i] < ra < ra2[i]) and (dec1[i] < dec < dec2[i])) \
+                        or (sep1[i] < radius) or (sep2[i] < radius) or (sep3[i] < radius) or (sep4[i] < radius):
+                    # row_south_list.append(table_south[i])
+                    brickname = brick_name[i]
+                    raIntPart = "{0:03}".format(int(ra1[i]))
+                    URL = f"{self.URL}/dr{data_release}/south/tractor/{raIntPart}/tractor-{brickname}.fits"
+
+                    response = requests.get(URL)
+                    if response is not None and response.status_code == 200:
+                        responses.append(response)
+            print("Completion processing bricks southern emisphere, total time: ", time.time() - t0)
+            print("-----------------------------------------------------")
+
+            return responses
 
 
     # as we mentioned earlier use various python regular expressions, etc
