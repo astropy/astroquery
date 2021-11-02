23261368976f636d45b6e1c41755b3512c6d8e38
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-11-02 09:00:14 -0400

    implement tests

--- astroquery/simbad/tests/test_simbad.py
+++ astroquery/simbad/tests/test_simbad.py
@@ -10,6 +10,7 @@ import numpy as np
 from ... import simbad
 from ...utils.testing_tools import MockResponse
 from ...utils import commons
+from ...query import AstroQuery
 from ...exceptions import TableParseError
 from .test_simbad_remote import multicoords
 
@@ -118,12 +119,15 @@ def test_get_frame_coordinates(coordinates, expected_frame):
 
 
 def test_parse_result():
-    result1 = simbad.core.Simbad._parse_result(
+    sb = simbad.core.Simbad()
+    # need _last_query to be defined
+    sb._last_query = AstroQuery('GET', 'http://dummy')
+    result1 = sb._parse_result(
         MockResponseSimbad('query id '), simbad.core.SimbadVOTableResult)
     assert isinstance(result1, Table)
     with pytest.raises(TableParseError) as ex:
-        simbad.core.Simbad._parse_result(MockResponseSimbad('query error '),
-                                         simbad.core.SimbadVOTableResult)
+        sb._parse_result(MockResponseSimbad('query error '),
+                simbad.core.SimbadVOTableResult)
     assert str(ex.value) == ('Failed to parse SIMBAD result! The raw response '
                              'can be found in self.last_response, and the '
                              'error in self.last_table_parse_error. '
