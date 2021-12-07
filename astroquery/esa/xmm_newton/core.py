a45e0df008f3baef5655226a2c0fe5ab03db2762
javier-ballester <javier.ballester@ext.esa.int>
2021-12-07 10:36:20 +0100

    NXSA-1091: Removed an unnecessary print statement

--- astroquery/esa/xmm_newton/core.py
+++ astroquery/esa/xmm_newton/core.py
@@ -106,7 +106,7 @@ class XMMNewtonClass(BaseQuery):
         reveals the url being sent which in turn reveals the users username and password
         """
         previouslevel = log.getEffectiveLevel()
-        log.setLevel(50)
+        log.setLevel(21)
 
         # create url to access the aio
         link = self._create_link(observation_id, **kwargs)
@@ -123,7 +123,6 @@ class XMMNewtonClass(BaseQuery):
         params = self._request_link(link, cache)
         r_filename = params["filename"]
         suffixes = Path(r_filename).suffixes
-        print(suffixes)
 
         # get desired filename
         filename = self._create_filename(filename, observation_id, suffixes)
