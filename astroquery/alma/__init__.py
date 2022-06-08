f3f2240d13b706302438c357418c4070f4ab3285
Dustin Jenkins <djenkins.cadc@gmail.com>
2022-06-08 06:35:28 -0700

    Force actions build.

--- astroquery/alma/__init__.py
+++ astroquery/alma/__init__.py
@@ -5,11 +5,13 @@ ALMA Archive service.
 from astropy import config as _config
 
 
+# separate list of ARC URLs as they each deploy their own IVOA services.
+_arc_url_list = ['https://almascience.eso.org', 
+                 'https://almascience.nrao.edu',
+                 'https://almascience.nao.ac.jp']
+
 # list the URLs here separately so they can be used in tests.
-_url_list = ['http://almascience.org',
-             'https://almascience.eso.org',
-             'https://almascience.nrao.edu',
-             'https://almascience.nao.ac.jp']
+_url_list = ['http://almascience.org'] + _arc_url_list
 
 auth_urls = ['asa.alma.cl', 'rh-cas.alma.cl']
 
@@ -18,6 +20,39 @@ class Conf(_config.ConfigNamespace):
     """
     Configuration parameters for `astroquery.alma`.
     """
+
+    registry_path = _config.ConfigItem(
+        '/reg/resource-caps', 
+        'ALMA Registry path')
+
+    registry_url = _config.ConfigItem(
+        'https://almascience.org/reg/resource-caps', 
+        'ALMA registry information')
+
+    tap_service_uri_path = _config.ConfigItem(
+        '/tap', 
+        'ALMA TAP ObsCore service URI path')
+
+    tap_standard_id = _config.ConfigItem(
+        'ivo://ivoa.net/std/TAP', 
+        'ALMA TAP service standard ID')
+
+    datalink_service_uri_path = _config.ConfigItem(
+        '/datalink', 
+        'ALMA DataLink service URI path')
+
+    datalink_standard_id = _config.ConfigItem(
+        'ivo://ivoa.net/std/DataLink#links-1.0',
+        'ALMA DataLink service standard ID'
+    )
+
+    sia_service_uri_path = _config.ConfigItem(
+        '/sia', 
+        'ALMA SIAv2 service URI path')
+
+    sia_standard_id = _config.ConfigItem(
+        'ivo://ivoa.net/std/SIA#query-2.0', 
+        'ALMA SIAv2 service standard ID')
 
     timeout = _config.ConfigItem(60, "Timeout in seconds.")
 
