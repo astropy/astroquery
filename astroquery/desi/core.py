e41373d5d23905fd6d061240c9aafc75c71a69de
burnout87 <gabriele.barni@unige.ch>
2022-06-14 08:44:53 +0200

    using Quantity class instead of Angle, fixed docstr

--- astroquery/desi/core.py
+++ astroquery/desi/core.py
@@ -7,9 +7,11 @@ import pyvo as vo
 import numpy as np
 import astropy.coordinates as coord
 
+from astropy import units as u
+from astropy.units import Quantity
 from astroquery.exceptions import NoResultsWarning
 from astroquery.query import BaseQuery
-from astroquery.utils import commons, async_to_sync
+from astroquery.utils import commons
 from astroquery.desi import conf
 
 __all__ = ['DESILegacySurvey', 'DESILegacySurveyClass']
@@ -25,7 +27,7 @@ class DESILegacySurveyClass(BaseQuery):
         ----------
         coordinates : `~astropy.coordinates.SkyCoord`
             coordinates around which to query.
-        radius : `~astropy.coordinates.Angle`,  optional
+        radius : `~astropy.units.Quantity`, optional
             the radius of the region. If missing, set to default
             value of 0.5 arcmin.
         data_release: int
@@ -33,20 +35,20 @@ class DESILegacySurveyClass(BaseQuery):
 
         Returns
         -------
-        response : `astropy.table.Table`
+        response : `~astropy.table.Table`
         """
 
         if radius is None:
-            radius = coord.Angle(0.5, unit='arcmin')
+            radius = Quantity(0.5, unit='arcmin')
 
         tap_service = vo.dal.TAPService(conf.tap_service_url)
         coordinates_transformed = coordinates.transform_to(coord.ICRS)
 
         qstr = (f"SELECT all * FROM ls_dr{data_release}.tractor WHERE "
-               f"dec>{coordinates_transformed.dec.deg - radius.deg} and "
-               f"dec<{coordinates_transformed.dec.deg + radius.deg} and "
-               f"ra>{coordinates_transformed.ra.deg - radius.deg / np.cos(coordinates_transformed.dec.deg * np.pi / 180.)} and "
-               f"ra<{coordinates_transformed.ra.deg + radius.deg / np.cos(coordinates_transformed.dec.deg * np.pi / 180)}")
+               f"dec>{(coordinates_transformed.dec - radius).to(u.deg).value} and "
+               f"dec<{(coordinates_transformed.dec + radius).to(u.deg).value} and "
+               f"ra>{coordinates_transformed.ra.to(u.deg).value - radius.to(u.deg).value / np.cos(coordinates_transformed.dec.to(u.deg).value * np.pi / 180.)} and "
+               f"ra<{coordinates_transformed.ra.to(u.deg).value + radius.to(u.deg).value / np.cos(coordinates_transformed.dec.to(u.deg).value * np.pi / 180)}")
 
         tap_result = tap_service.run_sync(qstr)
         tap_result = tap_result.to_table()
@@ -56,23 +58,30 @@ class DESILegacySurveyClass(BaseQuery):
 
         return filtered_table
 
-    def get_images(self, position, pixels, radius, *, data_release=9, show_progress=True, image_band='g'):
+    def get_images(self, position, pixels, radius=None, *, data_release=9, show_progress=True, image_band='g'):
         """
         Downloads the images for a certain region of interest.
 
         Parameters
         -------
-        position: `astropy.coordinates`.
+        position: `~astropy.coordinates`.
             coordinates around which we define our region of interest.
-        radius: `astropy.units.Quantity`.
-            the radius of the cone search.
-        data_release: int
+        radius: `~astropy.units.Quantity`,  optional
+            the radius of our region of interest.
+        data_release: int, optional
             the data release of the LegacySurvey to use.
+        show_progress: bool, optional
+            Whether to display a progress bar if the file is downloaded
+            from a remote server.  Default is True.
+        image_band: str, optional
 
         Returns
         -------
-        list: A list of `astropy.io.fits.HDUList` objects.
+        list: A list of `~astropy.io.fits.HDUList` objects.
         """
+
+        if radius is None:
+            radius = Quantity(0.5, u.arcmin)
 
         position_transformed = position.transform_to(coord.ICRS)
 
@@ -91,9 +100,9 @@ class DESILegacySurveyClass(BaseQuery):
 
         try:
             fits_file = file_container.get_fits()
-        except (requests.exceptions.HTTPError, urllib.error.HTTPError) as e:
+        except (requests.exceptions.HTTPError, urllib.error.HTTPError) as exp:
             fits_file = None
-            warnings.warn(f"{str(e)} - Problem retrieving the file at the url: {image_url}", NoResultsWarning)
+            warnings.warn(f"{str(exp)} - Problem retrieving the file at the url: {image_url}", NoResultsWarning)
 
         return [fits_file]
 
