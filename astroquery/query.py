7ab4992a0a645b22de722860999cec4bd09124d9
C. E. Brasseur <cebrasseur@gmail.com>
2022-09-19 15:01:30 +0100

    making new caching backwards compatible

--- astroquery/query.py
+++ astroquery/query.py
@@ -338,7 +338,7 @@ class BaseQuery(metaclass=LoginABCMeta):
                 # ":" so replace them with an underscore
                 local_filename = local_filename.replace(':', '_')
 
-            local_filepath = os.path.join(savedir or self._cache_location or '.', local_filename)
+            local_filepath = os.path.join(savedir or self.cache_location or '.', local_filename)
 
             response = self._download_file(url, local_filepath, cache=cache, timeout=timeout,
                                            continuation=continuation, method=method,
@@ -364,13 +364,13 @@ class BaseQuery(metaclass=LoginABCMeta):
 
                 if not response:
                     response = query.request(self._session,
-                                             self._cache_location,
+                                             self.cache_location,
                                              stream=stream,
                                              auth=auth,
                                              allow_redirects=allow_redirects,
                                              verify=verify,
                                              json=json)
-                    to_cache(response, query.request_file(self._cache_location))
+                    to_cache(response, query.request_file(self.cache_location))
 
             self._last_query = query
             return response
