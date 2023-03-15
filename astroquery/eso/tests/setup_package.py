0e534405868a19695a93aec86dad543c8e853240
Stefano Zampieri <szampier@eso.org>
2023-03-15 16:38:31 +0100

    add new test data files to setup_package.py

--- astroquery/eso/tests/setup_package.py
+++ astroquery/eso/tests/setup_package.py
@@ -8,5 +8,8 @@ def get_package_data():
     paths = [os.path.join('data', '*.pickle'),
              os.path.join('data', '*.html'),
              os.path.join('data', '*.tbl'),
+             os.path.join('data', '*.xml'),
+             os.path.join('data', '*.json'),
+             os.path.join('data', '*.fits*')
              ]
     return {'astroquery.eso.tests': paths}
