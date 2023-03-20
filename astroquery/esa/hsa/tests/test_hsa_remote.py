91bd7c277bd59654e6f418d74b38f9716e2b46f7
Eero Vaher <eero.vaher@astro.lu.se>
2023-03-20 18:30:43 +0100

    Parametrize `esa.hsa` remote tests
    The parametrized tests are less verbose because there is less code
    duplication.

--- astroquery/esa/hsa/tests/test_hsa_remote.py
+++ astroquery/esa/hsa/tests/test_hsa_remote.py
@@ -7,7 +7,6 @@ import pytest
 from ..core import HSAClass
 
 
-
 PACS_ENDINGS = ["571.xml", "571.jpg", "214.fits.gz", "008.fits.gz",
                 "674.fits.gz", "350.fits.gz", "README.pdf"]
 SPIRE_ENDINGS = ["898.xml", "898.jpg", "141.fits.gz", "045.fits.gz", "952.fits.gz",
@@ -20,139 +19,62 @@ SPIRE_ENDINGS = ["898.xml", "898.jpg", "141.fits.gz", "045.fits.gz", "952.fits.g
 @pytest.mark.remote_data
 class TestHSARemote:
 
-    def test_download_data_observation_pacs(self, tmp_path):
-        obs_id = "1342191813"
-        parameters = {'retrieval_type': "OBSERVATION",
-                      'observation_id': obs_id,
+    @pytest.mark.parametrize(
+        "method,kwargs,expected_filename,expected_endings",
+        [
+            ("download_data", {}, "1342191813.tar", PACS_ENDINGS),
+            ("download_data", {"filename": "output_file"}, "output_file.tar", PACS_ENDINGS),
+            ("download_data", {"compress": "true"}, "1342191813.tgz", PACS_ENDINGS),
+            (
+                "download_data",
+                {
+                    "observation_id": "1342191188",
+                    "instrument_name": "SPIRE",
+                    "product_level": "LEVEL2",
+                },
+                "1342191188.tar",
+                SPIRE_ENDINGS,
+            ),
+            ("get_observation", {}, "1342191813.tar", PACS_ENDINGS),
+        ],
+    )
+    def test_download_data_observation(
+        self, method, kwargs, expected_filename, expected_endings, tmp_path
+    ):
+        parameters = {"observation_id": "1342191813",
                       'instrument_name': "PACS",
                       'product_level': 'LEVEL3',
                       'cache': False,
                       'download_dir': tmp_path}
-        expected_res = Path(tmp_path, obs_id + ".tar")
-        hsa = HSAClass()
-        res = hsa.download_data(**parameters)
-        assert Path(res) == expected_res
+        parameters.update(kwargs)
+        if method == "download_data":
+            res = HSAClass().download_data(**parameters, retrieval_type="OBSERVATION")
+        elif method == "get_observation":
+            res = HSAClass().get_observation(**parameters)
+        assert Path(res) == tmp_path / expected_filename
         assert Path(res).is_file()
         with tarfile.open(res) as tar:
             names = tar.getnames()
-        assert len(names) == len(PACS_ENDINGS)
-        for name, ending in zip(names, PACS_ENDINGS):
+        assert len(names) == len(expected_endings)
+        for name, ending in zip(names, expected_endings):
             assert name.endswith(ending)
 
-    def test_download_data_observation_pacs_filename(self, tmp_path):
-        obs_id = "1342191813"
-        fname = "output_file"
-        parameters = {'retrieval_type': "OBSERVATION",
-                      'observation_id': obs_id,
-                      'instrument_name': "PACS",
-                      'product_level': 'LEVEL3',
-                      'filename': fname,
-                      'cache': False,
-                      'download_dir': tmp_path}
-        expected_res = Path(tmp_path, fname + ".tar")
-        hsa = HSAClass()
-        res = hsa.download_data(**parameters)
-        assert Path(res) == expected_res
-        assert Path(res).is_file()
-        with tarfile.open(res) as tar:
-            names = tar.getnames()
-        assert len(names) == len(PACS_ENDINGS)
-        for name, ending in zip(names, PACS_ENDINGS):
-            assert name.endswith(ending)
-
-    def test_download_data_observation_pacs_compressed(self, tmp_path):
-        obs_id = "1342191813"
-        parameters = {'retrieval_type': "OBSERVATION",
-                      'observation_id': obs_id,
-                      'instrument_name': "PACS",
-                      'product_level': 'LEVEL3',
-                      'compress': 'true',
-                      'cache': False,
-                      'download_dir': tmp_path}
-        expected_res = Path(tmp_path, obs_id + ".tgz")
-        hsa = HSAClass()
-        res = hsa.download_data(**parameters)
-        assert Path(res) == expected_res
-        assert Path(res).is_file()
-        with tarfile.open(res) as tar:
-            names = tar.getnames()
-        assert len(names) == len(PACS_ENDINGS)
-        for name, ending in zip(names, PACS_ENDINGS):
-            assert name.endswith(ending)
-
-    def test_download_data_observation_spire(self, tmp_path):
-        obs_id = "1342191188"
-        parameters = {'retrieval_type': "OBSERVATION",
-                      'observation_id': obs_id,
-                      'instrument_name': "SPIRE",
-                      'product_level': 'LEVEL2',
-                      'cache': False,
-                      'download_dir': tmp_path}
-        expected_res = Path(tmp_path, obs_id + ".tar")
-        hsa = HSAClass()
-        res = hsa.download_data(**parameters)
-        assert Path(res) == expected_res
-        assert Path(res).is_file()
-        with tarfile.open(res) as tar:
-            names = tar.getnames()
-        assert len(names) == len(SPIRE_ENDINGS)
-        for name, ending in zip(names, SPIRE_ENDINGS):
-            assert name.endswith(ending)
-
-    def test_download_data_postcard_pacs(self, tmp_path):
-        obs_id = "1342191813"
-        parameters = {'retrieval_type': "POSTCARD",
-                      'observation_id': obs_id,
+    @pytest.mark.parametrize(
+        "method,kwargs,expected_filename",
+        [
+            ("download_data", {}, "1342191813.jpg"),
+            ("download_data", {"filename": "output_file"}, "output_file.jpg"),
+            ("get_postcard", {}, "1342191813.jpg"),
+        ],
+    )
+    def test_download_data_postcard(self, method, kwargs, expected_filename, tmp_path):
+        parameters = {"observation_id": "1342191813",
                       'instrument_name': "PACS",
                       'cache': False,
                       'download_dir': tmp_path}
-        expected_res = Path(tmp_path, obs_id + ".jpg")
-        hsa = HSAClass()
-        res = hsa.download_data(**parameters)
-        assert Path(res) == expected_res
-        assert Path(res).is_file()
-
-    def test_download_data_postcard_pacs_filename(self, tmp_path):
-        obs_id = "1342191813"
-        fname = "output_file"
-        parameters = {'retrieval_type': "POSTCARD",
-                      'observation_id': obs_id,
-                      'instrument_name': "PACS",
-                      'filename': fname,
-                      'cache': False,
-                      'download_dir': tmp_path}
-        expected_res = Path(tmp_path, fname + ".jpg")
-        hsa = HSAClass()
-        res = hsa.download_data(**parameters)
-        assert Path(res) == expected_res
-        assert Path(res).is_file()
-
-    def test_get_observation(self, tmp_path):
-        obs_id = "1342191813"
-        parameters = {'observation_id': obs_id,
-                      'instrument_name': "PACS",
-                      'product_level': 'LEVEL3',
-                      'cache': False,
-                      'download_dir': tmp_path}
-        expected_res = Path(tmp_path, obs_id + ".tar")
-        hsa = HSAClass()
-        res = hsa.get_observation(**parameters)
-        assert Path(res) == expected_res
-        assert Path(res).is_file()
-        with tarfile.open(res) as tar:
-            names = tar.getnames()
-        assert len(names) == len(PACS_ENDINGS)
-        for name, ending in zip(names, PACS_ENDINGS):
-            assert name.endswith(ending)
-
-    def test_get_postcard(self, tmp_path):
-        obs_id = "1342191813"
-        parameters = {'observation_id': obs_id,
-                      'instrument_name': "PACS",
-                      'cache': False,
-                      'download_dir': tmp_path}
-        expected_res = Path(tmp_path, obs_id + ".jpg")
-        hsa = HSAClass()
-        res = hsa.get_postcard(**parameters)
-        assert Path(res) == expected_res
+        if method == "download_data":
+            res = HSAClass().download_data(**parameters, **kwargs, retrieval_type="POSTCARD")
+        elif method == "get_postcard":
+            res = HSAClass().get_postcard(**parameters, **kwargs)
+        assert Path(res) == tmp_path / expected_filename
         assert Path(res).is_file()
