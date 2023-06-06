f6868ca7dffd00d99ec0277cdb6dd056dbe9924c
Michael S. P. Kelley <msk@astro.umd.edu>
2023-06-06 10:59:09 -0400

    Add a test for topocentric coordinate queries.

--- astroquery/jplhorizons/tests/test_jplhorizons_remote.py
+++ astroquery/jplhorizons/tests/test_jplhorizons_remote.py
@@ -361,6 +361,22 @@ class TestHorizonsClass:
 
         assert len(res) >= 6412
 
+    def test_vectors_query_topocentric_coordinates(self):
+        "Test vectors query specifying observer's longitude, latitude, and elevation"
+        q = jplhorizons.Horizons(id='Ceres',
+                                location={"lon": 244, "lat": 33, "elevation": 1700},
+                                id_type='smallbody',
+                                epochs=2451544.5)
+        res = q.vectors_async()
+        i = res.text.find("Center geodetic :")
+        j = res.text.find("\n", i)
+        parts = res.text[i:j].split()
+        assert parts[3:6] == ['244.0,', '33.0,', '1700.0']
+
+        start = res.text.find("$$SOE")
+        end = res.text.find("$$EOE")
+        assert res.text[start:end].find("2000-Jan-01") > 0
+
     def test_unknownobject(self):
         with pytest.raises(ValueError):
             jplhorizons.Horizons(id='spamspamspameggsspam', location='500',
