cf5c9c34e436681ebea14733a741d8475231b79d
Brigitta Sipőcz <bsipocz@gmail.com>
2023-07-31 21:02:36 -0700

    DEP: website of explanet_orbit_database has been retired, deprecate module, too

--- astroquery/exoplanet_orbit_database/__init__.py
+++ astroquery/exoplanet_orbit_database/__init__.py
@@ -5,9 +5,16 @@ Exoplanet Orbit Database Query Tool
 
 :Author: Brett M. Morris (brettmorris21@gmail.com)
 """
+import warnings
 from .exoplanet_orbit_database import (ExoplanetOrbitDatabase,
                                        ExoplanetOrbitDatabaseClass)
 from astropy import config as _config
+from astropy.utils.exceptions import AstropyDeprecationWarning
+
+
+warnings.warn("due to the retirement of its upstream website, the ``exoplanet_orbit_database`` module "
+              "has been deprecated as of v0.4.7 and will be removed in a future release.",
+              AstropyDeprecationWarning, stacklevel=2)
 
 
 class Conf(_config.ConfigNamespace):
