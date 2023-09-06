bf217bb00f92d7098aaa359938615fbc412ea305
C. E. Brasseur <cebrasseur@gmail.com>
2023-09-06 17:33:08 +0100

    Updating doc strings

--- astroquery/esa/neocc/utils.py
+++ astroquery/esa/neocc/utils.py
@@ -6,9 +6,23 @@ from astropy.time import Time, TimeDelta
 
 def convert_time(date_col, time_col=None, conversion_string=None):
     """
-    Take a column with times as strings and turn it into an astropy Time object.
+    Converte date/time column(s) and turn them into an astropy Time object.
 
-    TODO: finish documentation
+    Parameters
+    ----------
+    date_col : `astropy.table.Column`
+        The column with dates (or datetime) strings in it.
+    time_col : `astropy.table.Column`
+        Optional. Column with only time strings.
+    conversion_string : str
+        Optional. If the date(time) string are in a non-standard format
+        the conversion string will be used with `astropy.time.Time.strptime`
+        to create the Time object.
+
+    Returns
+    -------
+    response : `astropy.time.Time`
+        The converted time column as an astropy Time object.
     """
 
     if time_col is None:
