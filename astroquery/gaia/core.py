8552c59e57838fd26032c5cfad60fdac84271624
jferna01 <Jorge.Fernandez.Hernandez@ext.esa.int>
2023-10-21 07:38:10 +0200

    GAIAPCR-1290 Set  SOURCE_ID as the default value

--- astroquery/gaia/core.py
+++ astroquery/gaia/core.py
@@ -164,7 +164,8 @@ class GaiaClass(TapPlus):
             log.error("Error logging out data server")
 
     def load_data(self, ids, *, data_release=None, data_structure='INDIVIDUAL', retrieval_type="ALL",
-                  linking_parameter=None, valid_data=False, band=None, avoid_datatype_check=False, format="votable",
+                  linking_parameter='SOURCE_ID', valid_data=False, band=None, avoid_datatype_check=False,
+                  format="votable",
                   output_file=None, overwrite_output_file=False, verbose=False):
         """Loads the specified table
         TAP+ only
@@ -193,7 +194,7 @@ class GaiaClass(TapPlus):
             For GAIA DR4, possible values will be ['EPOCH_PHOTOMETRY', 'RVS', 'XP_CONTINUOUS', 'XP_SAMPLED',
             'MCMC_GSPPHOT', 'MCMC_MSC', 'EPOCH_ASTROMETRY', 'RV_EPOCH_SINGLE', 'RV_EPOCH_DOUBLE', 'RVS_EPOCH' or
             'RVS_TRANSIT']
-        linking_parameter : str, optional, default None, valid values: SOURCE_ID, TRANSIT_ID, IMAGE_ID
+        linking_parameter : str, optional, default SOURCE_ID, valid values: SOURCE_ID, TRANSIT_ID, IMAGE_ID
             By default, all the identifiers are considered as source_id
             SOURCE_ID: the identifiers are considered as source_id
             TRANSIT_ID: the identifiers are considered as transit_id
@@ -276,14 +277,13 @@ class GaiaClass(TapPlus):
         params_dict['RETRIEVAL_TYPE'] = str(retrieval_type)
         params_dict['USE_ZIP_ALWAYS'] = 'true'
 
-        if linking_parameter is not None:
-            valid_param = {'SOURCE_ID', 'TRANSIT_ID', 'IMAGE_ID'}
-            if linking_parameter not in valid_param:
-                raise ValueError(f"Invalid linking_parameter value '{linking_parameter}' "
-                                 f"(Valid values: {', '.join(valid_param)})")
-            else:
-                if linking_parameter != 'SOURCE_ID':
-                    params_dict['LINKING_PARAMETER'] = linking_parameter
+        valid_param = {'SOURCE_ID', 'TRANSIT_ID', 'IMAGE_ID'}
+        if linking_parameter not in valid_param:
+            raise ValueError(
+                f"Invalid linking_parameter value '{linking_parameter}' (Valid values: {', '.join(valid_param)})")
+        else:
+            if linking_parameter != 'SOURCE_ID':
+                params_dict['LINKING_PARAMETER'] = linking_parameter
 
         if path != '':
             try:
