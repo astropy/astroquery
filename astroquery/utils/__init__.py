d8dfe3c407caad842f829c9b4550b7da967ddcee
Brigitta Sipőcz <bsipocz@gmail.com>
2024-03-12 17:15:24 -0700

    MAINT: dropping astropy <5 and numpy <1.20 support

--- astroquery/utils/__init__.py
+++ astroquery/utils/__init__.py
@@ -6,8 +6,7 @@ functions that will ultimately be merged into `astropy.utils`.
 from .progressbar import chunk_report, chunk_read
 from .class_or_instance import class_or_instance
 from .commons import (parse_coordinates, TableList, suppress_vo_warnings,
-                      validate_email, ASTROPY_LT_4_3, ASTROPY_LT_5_0,
-                      ASTROPY_LT_5_1)
+                      validate_email, ASTROPY_LT_5_1)
 from .process_asyncs import async_to_sync
 from .docstr_chompers import prepend_docstr_nosections
 from .cleanup_downloads import cleanup_saved_downloads
@@ -19,8 +18,6 @@ __all__ = ['chunk_report', 'chunk_read',
            'TableList',
            'suppress_vo_warnings',
            'validate_email',
-           'ASTROPY_LT_4_3',
-           'ASTROPY_LT_5_0',
            'ASTROPY_LT_5_1',
            "async_to_sync",
            "prepend_docstr_nosections",
