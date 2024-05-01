852e6218abbd3e2f69918ae216e9aa8530587c53
Brigitta Sipőcz <bsipocz@gmail.com>
2024-05-01 01:27:40 -0700

    Fixing ecsv case

--- astroquery/utils/tap/xmlparser/utils.py
+++ astroquery/utils/tap/xmlparser/utils.py
@@ -61,6 +61,9 @@ def read_http_response(response, output_format, *, correct_units=True, use_names
 
             else:
                 result = APTable.read(data, format=astropy_format)
+
+        elif output_format == 'ecsv':
+            result = APTable.read(data, format=astropy_format)
         else:
             result = APTable.read(data, format=astropy_format, use_names_over_ids=use_names_over_ids)
 
