6d895fe9233a622426c2ba9d328a8636b0c61315
Javier Espinosa <javier.espinosa@ext.esa.int>
2024-11-29 18:25:29 +0100

    INTEGRALSWRQ-154: initial version of astroquery module for ISLA

---
+++ astroquery/esa/integral/__init__.py
@@ -0,0 +1,50 @@
+"""
+
+@author: Javier Duran
+@contact: javier.duran@sciops.esa.int
+
+European Space Astronomy Centre (ESAC)
+European Space Agency (ESA)
+
+Created on 07 July. 2020
+
+"""
+
+from astropy import config as _config
+
+ISLA_TAP_URL = 'https://isladev.esac.esa.int/tap-dev/tap'
+ISLA_DATA_URL = 'https://isladev.esac.esa.int/tap-dev/data?'
+
+
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for `astroquery.esa.integral`.
+    """
+    ISLA_TAP_SERVER = _config.ConfigItem(ISLA_TAP_URL, "ISLA TAP Server")
+    ISLA_DATA_SERVER = _config.ConfigItem(ISLA_DATA_URL, "ISLA Data Server")
+    ISLA_SERVLET = _config.ConfigItem(ISLA_TAP_URL + "/sync/?PHASE=RUN",
+                                      "ISLA Servlet Request")
+
+    ISLA_INSTRUMENT_BAND_QUERY = _config.ConfigItem('select i.name as instrument, b."name" as band, '
+                                                    'i.instrument_oid, b.band_oid from ila.instrument i join '
+                                                    'ila.band b using(instrument_oid);',
+                                                    "ISLA Instrument Band Query")
+    ISLA_EPOCH_QUERY = _config.ConfigItem("select distinct epoch from ila.epoch where source_id = '{}' and "
+                                          "(instrument_oid = {} or band_oid = {})",
+                                          "ISLA Epoch Query")
+    ISLA_OBSERVATION_BASE_QUERY = _config.ConfigItem("select * from ila.cons_pub_obs",
+                                                     "ISLA Observation Base Query")
+    ISLA_TARGET_CONDITION = _config.ConfigItem("select distinct src.name, src.ra, src.dec, src.source_id from "
+                                               "ila.v_cat_source src where "
+                                               "src.name ilike '%{}%' order by src.name asc",
+                                               "ISLA Target Condition")
+    ISLA_COORDINATE_CONDITION = _config.ConfigItem("1=CONTAINS(POINT('ICRS',ra,dec),CIRCLE('ICRS',{},{},{}))",
+                                                   "ISLA Coordinate Condition")
+    TIMEOUT = 60
+
+
+conf = Conf()
+
+from .core import Integral, IntegralClass
+
+__all__ = ['Integral', 'IntegralClass', 'Conf', 'conf']
