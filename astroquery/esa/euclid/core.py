d0837528fd40d725d3782fa7085243ce19706a2a
Jorge Fernandez Hernandez <Jorge.Fernandez.Hernandez@ext.esa.int>
2025-02-21 09:12:04 +0100

    EUCLIDPCR-1863 Remove redundant information in the header of the files

--- astroquery/esa/euclid/core.py
+++ astroquery/esa/euclid/core.py
@@ -3,12 +3,8 @@
 ===============
 Euclid TAP plus
 ===============
-@author: Juan Carlos Segovia (original code from Gaia) / John Hoar (Euclid adaptation)
-@contact: juan.carlos.segovia@sciops.esa.int / jhoar@sciops.esa.int
 European Space Astronomy Centre (ESAC)
 European Space Agency (ESA)
-Created on 30 jun. 2016
-Euclid adaptation: 31 January 2022
 """
 import binascii
 import os
