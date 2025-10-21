929cd38bacd82179941e318c315942753aee7514
hhsieh00 <hhsieh@gmail.com>
2025-10-21 07:36:30 -1000

    First draft of completed astorbdb class
    First complete draft of the astorbdb class including tests and documentation

---
+++ astroquery/solarsystem/astorbdb/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+astroquery.solarsystem.astorbdb
+-------------------------------
+
+a collection of data services provided by Lowell Observatory's astorbDB
+"""
+
+from ...astorbdb import AstInfo, AstInfoClass
+
+
+__all__ = ["AstInfo", "AstInfoClass"]
