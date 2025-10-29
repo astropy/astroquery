9b497ef09db76862877a80956f0801bce1075035
hhsieh00 <hhsieh@gmail.com>
2025-10-28 22:15:40 -1000

    Updates to __init__.py files in solarsystem module

---
+++ astroquery/solarsystem/lowellmps/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+astroquery.solarsystem.lowellmps
+--------------------------------
+
+a collection of data services provided by Lowell Minor Planet Services
+"""
+
+from ...lowellmps import AstInfo, AstInfoClass
+
+
+__all__ = ["AstInfo", "AstInfoClass"]
