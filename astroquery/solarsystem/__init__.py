7e43cb4397c929833bdbd38b60239d64b0015eed
hhsieh00 <hhsieh@gmail.com>
2025-10-28 22:15:40 -1000

    Updates to __init__.py files in solarsystem module

--- astroquery/solarsystem/__init__.py
+++ astroquery/solarsystem/__init__.py
@@ -10,8 +10,9 @@ a collection of Solar-System related data services
 from .jpl import SBDB, SBDBClass, Horizons, HorizonsClass
 from .imcce import Miriade, MiriadeClass, Skybot, SkybotClass
 from .mpc import MPC, MPCClass
+from .lowellmps import AstInfo, AstInfoClass
 
 
 __all__ = ["SBDB", "SBDBClass", "Horizons", "HorizonsClass",
            "Miriade", "MiriadeClass", "Skybot", "SkybotClass",
-           "MPC", "MPCClass"]
+           "MPC", "MPCClass","AstInfo", "AstInfoClass"]
