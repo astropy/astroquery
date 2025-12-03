45d57d6b4f199d36685fec4a6bec2fdd4c71cb65
Michael S. P. Kelley <msk@astro.umd.edu>
2025-12-03 09:47:31 -0500

    Numbers are not allowed in fragment designations.

--- astroquery/mpc/core.py
+++ astroquery/mpc/core.py
@@ -910,9 +910,9 @@ class MPCClass(BaseQuery):
 
             pat = ('(^[0-9]*$)|'  # [0] asteroid number
                    '(^[0-9]{1,3}[PI]$)'  # [1] periodic comet or interstellar object: 1P, 2P, 1I, 2I, etc.
-                   '(-[1-9A-Z]{0,2})?$|'  # [2] fragment clause for [1]
+                   '(-[A-Z]{0,2})?$|'  # [2] fragment clause for [1]
                    '(^[PDCXA]/[- 0-9A-Za-z]*)'  # [3] comet designation
-                   '(-[1-9A-Z]{0,2})?$|'  # [4] fragment clause for [3]
+                   '(-[A-Z]{0,2})?$|'  # [4] fragment clause for [3]
                    '(^([1A][8-9][0-9]{2}[ _][A-Z]{2}[0-9]{0,3}$|'
                    '^20[0-9]{2}[ _][A-Z]{2}[0-9]{0,3}$)|'
                    '(^[1-9][0-9]{3}[ _](P-L|T-[1-3]))$)'
