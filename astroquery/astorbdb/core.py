a7d6cd27a3f3b1390d55183ff847a22b8709b4da
hhsieh00 <hhsieh@gmail.com>
2025-12-15 18:27:30 -1000

    Revert "added URL checks"
    This reverts commit 84ad8b1b19d745d9c41e00dafb2659c949730d21.

--- astroquery/astorbdb/core.py
+++ astroquery/astorbdb/core.py
@@ -363,6 +363,115 @@ class AstInfoClass(BaseQuery):
 
         return response
 
+    def lightcurves_async(self, object_name, *,
+                          get_uri=False,
+                          cache=True):
+        """
+        This method uses a REST interface to query the `Lowell Observatory
+        astorbDB database <https://asteroid.lowell.edu/>`_ for lightcurve
+        data for a single object and returns a dictionary from JSON results
+
+        Parameters
+        ----------
+        object_name : str
+            name of the identifier to query.
+
+        Returns
+        -------
+        res : A dictionary holding available lightcurve data
+
+        Examples
+        --------
+        >>> from astroquery.astorbdb import AstInfo
+        >>> lightcurves = AstInfo.lightcurves('Beagle')  # doctest: +SKIP
+        >>> print(lightcurves)  # doctest: +SKIP
+        [{..., 'amp_max': <Quantity 1.2 mag>, 'amp_min': <Quantity 0.57 mag>, ..., 'period': <Quantity 7.035 h>, ...}]
+        """
+
+        self.query_type = 'lightcurves'
+
+        response = self._request('GET',
+                                 url=self.URL + object_name + '/data/lightcurves',
+                                 timeout=self.TIMEOUT, cache=cache)
+
+        if get_uri:
+            self._uri = response.url
+
+        return response
+
+    def orbit_async(self, object_name, *,
+                    get_uri=False,
+                    cache=True):
+        """
+        This method uses a REST interface to query the `Lowell Observatory
+        astorbDB database <https://asteroid.lowell.edu/>`_ for orbit
+        data for a single object and returns a dictionary from JSON results
+
+        Parameters
+        ----------
+        object_name : str
+            name of the identifier to query.
+
+        Returns
+        -------
+        res : A dictionary holding available orbit fitting data
+
+        Examples
+        --------
+        >>> from astroquery.astorbdb import AstInfo
+        >>> orbit = AstInfo.orbit('Beagle')  # doctest: +SKIP
+        >>> print(orbit)  # doctest: +SKIP
+        {'a1con': <Quantity 0. AU / d2>, 'a2con': <Quantity 0. AU / d2>, ...}
+        """
+
+        self.query_type = 'orbit'
+
+        response = self._request('GET',
+                                 url=self.URL + object_name + '/orbit',
+                                 timeout=self.TIMEOUT, cache=cache)
+
+        if get_uri:
+            self._uri = response.url
+
+        return response
+
+    def taxonomies_async(self, object_name, *,
+                         get_uri=False,
+                         cache=True):
+        """
+        This method uses a REST interface to query the `Lowell Observatory
+        astorbDB database <https://asteroid.lowell.edu/>`_ for taxonomy
+        data for a single object and returns a dictionary from JSON results
+
+        Parameters
+        ----------
+        object_name : str
+            name of the identifier to query.
+
+        Returns
+        -------
+        res : A dictionary holding available taxonomy data
+
+        Examples
+        --------
+        >>> from astroquery.astorbdb import AstInfo
+        >>> taxonomies = AstInfo.taxonomies('Beagle')  # doctest: +SKIP
+        >>> print(taxonomies)  # doctest: +SKIP
+        [{'citation_bibcode': '2011PDSS..145.....H', ..., 'survey_name': 'Carvano et al. (2010)', 'taxonomy': 'C', ...},
+         {'citation_bibcode': '2013Icar..226..723D', ..., 'survey_name': 'DeMeo et al. (2013)', 'taxonomy': 'C', ...}]
+        """
+
+        self.query_type = 'taxonomies'
+
+        response = self._request('GET',
+                                 url=self.URL + object_name + '/data/taxonomies',
+                                 timeout=self.TIMEOUT, cache=cache)
+
+        if get_uri:
+            self._uri = response.url
+
+        return response
+
     def all_astinfo_async(self, object_name, *,
                           get_uri=False,
                           cache=True):
