698ef3dbe3a495bca7cd6b3959be306361b16503
hhsieh00 <hhsieh@gmail.com>
2025-12-15 18:27:15 -1000

    Revert "reduced amount of test data"
    This reverts commit 7756c521b08b979562c00653bb3a80ac49ee2548.

--- astroquery/astorbdb/tests/test_astorbdb.py
+++ astroquery/astorbdb/tests/test_astorbdb.py
@@ -10,7 +10,18 @@ from astropy.time import Time
 from .. import AstInfo, AstInfoClass
 
 # files in data/ for different query types
-DATA_FILES = {'Apophis':
+DATA_FILES = {'1':
+              {'designations': 'ceres_desig.dat',
+               'elements': 'ceres_elements.dat',
+               'orbit': 'ceres_orbit.dat',
+               'albedos': 'ceres_albedos.dat',
+               'colors': 'ceres_colors.dat',
+               'taxonomies': 'ceres_taxonomies.dat',
+               'lightcurves': 'ceres_lightcurves.dat',
+               'dynamical-family': 'ceres_dynfamily.dat',
+               'escape-routes': 'ceres_escaperoutes.dat'
+               },
+              'Apophis':
               {'designations': 'apophis_desig.dat',
                'elements': 'apophis_elements.dat',
                'orbit': 'apophis_orbit.dat',
@@ -20,6 +31,17 @@ DATA_FILES = {'Apophis':
                'lightcurves': 'apophis_lightcurves.dat',
                'dynamical-family': 'apophis_dynfamily.dat',
                'escape-routes': 'apophis_escaperoutes.dat'
+               },
+              'Toutatis':
+              {'designations': 'toutatis_desig.dat',
+               'elements': 'toutatis_elements.dat',
+               'orbit': 'toutatis_orbit.dat',
+               'albedos': 'toutatis_albedos.dat',
+               'colors': 'toutatis_colors.dat',
+               'taxonomies': 'toutatis_taxonomies.dat',
+               'lightcurves': 'toutatis_lightcurves.dat',
+               'dynamical-family': 'toutatis_dynfamily.dat',
+               'escape-routes': 'toutatis_escaperoutes.dat'
                },
               'Beagle':
               {'designations': 'beagle_desig.dat',
@@ -32,6 +54,28 @@ DATA_FILES = {'Apophis':
                'dynamical-family': 'beagle_dynfamily.dat',
                'escape-routes': 'beagle_escaperoutes.dat'
                },
+              '2060':
+              {'designations': 'chiron_desig.dat',
+               'elements': 'chiron_elements.dat',
+               'orbit': 'chiron_orbit.dat',
+               'albedos': 'chiron_albedos.dat',
+               'colors': 'chiron_colors.dat',
+               'taxonomies': 'chiron_taxonomies.dat',
+               'lightcurves': 'chiron_lightcurves.dat',
+               'dynamical-family': 'chiron_dynfamily.dat',
+               'escape-routes': 'chiron_escaperoutes.dat'
+               },
+              '3200':
+              {'designations': 'phaethon_desig.dat',
+               'elements': 'phaethon_elements.dat',
+               'orbit': 'phaethon_orbit.dat',
+               'albedos': 'phaethon_albedos.dat',
+               'colors': 'phaethon_colors.dat',
+               'taxonomies': 'phaethon_taxonomies.dat',
+               'lightcurves': 'phaethon_lightcurves.dat',
+               'dynamical-family': 'phaethon_dynfamily.dat',
+               'escape-routes': 'phaethon_escaperoutes.dat'
+               },
               '3556':
               {'designations': 'lixiaohua_desig.dat',
                'elements': 'lixiaohua_elements.dat',
@@ -42,6 +86,28 @@ DATA_FILES = {'Apophis':
                'lightcurves': 'lixiaohua_lightcurves.dat',
                'dynamical-family': 'lixiaohua_dynfamily.dat',
                'escape-routes': 'lixiaohua_escaperoutes.dat'
+               },
+              '300163':
+              {'designations': '300163_desig.dat',
+               'elements': '300163_elements.dat',
+               'orbit': '300163_orbit.dat',
+               'albedos': '300163_albedos.dat',
+               'colors': '300163_colors.dat',
+               'taxonomies': '300163_taxonomies.dat',
+               'lightcurves': '300163_lightcurves.dat',
+               'dynamical-family': '300163_dynfamily.dat',
+               'escape-routes': '300163_escaperoutes.dat'
+               },
+              '2024 ON':
+              {'designations': '2024on_desig.dat',
+               'elements': '2024on_elements.dat',
+               'orbit': '2024on_orbit.dat',
+               'albedos': '2024on_albedos.dat',
+               'colors': '2024on_colors.dat',
+               'taxonomies': '2024on_taxonomies.dat',
+               'lightcurves': '2024on_lightcurves.dat',
+               'dynamical-family': '2024on_dynfamily.dat',
+               'escape-routes': '2024on_escaperoutes.dat'
                },
               'Ceres':
               {'designations': 'ceres_missing_value_desig.dat',
@@ -56,49 +122,103 @@ DATA_FILES = {'Apophis':
                }
               }
 
-ALBEDOS = {'Apophis': None,
+ALBEDOS = {'1': 0.087,
+           'Apophis': None,
+           'Toutatis': 0.405,
            'Beagle': 0.065,
+           '2060': 0.11,
+           '3200': 0.16,
            '3556': 0.042,
+           '300163': None,
+           '2024 ON': None
            }
 
-COLORS = {'Apophis': None,
+COLORS = {'1': 0.377,
+          'Apophis': None,
+          'Toutatis': -0.566,
           'Beagle': 0.431,
+          '2060': 0.334,
+          '3200': 0.54,
           '3556': None,
+          '300163': 1.26,
+          '2024 ON': None
           }
 
-DESIGS = {'Apophis': "Apophis",
+DESIGS = {'1': "Ceres",
+          'Apophis': "Apophis",
+          'Toutatis': "Toutatis",
           'Beagle': "Beagle",
+          '2060': "Chiron",
+          '3200': "Phaethon",
           '3556': "Lixiaohua",
+          '300163': "2006 VW139",
+          '2024 ON': "2024 ON"
           }
 
-DYNFAMILY = {'Apophis': None,
+DYNFAMILY = {'1': None,
+             'Apophis': None,
+             'Toutatis': None,
              'Beagle': "Themis",
+             '2060': None,
+             '3200': None,
              '3556': "Lixiaohua",
+             '300163': None,
+             '2024 ON': None
              }
 
-ELEMENTS = {'Apophis': 0.7460876463977817 * u.au,
+ELEMENTS = {'1': 2.546883130334219 * u.au,
+            'Apophis': 0.7460876463977817 * u.au,
+            'Toutatis': 0.9543335929799223 * u.au,
             'Beagle': 2.741852545719269 * u.au,
+            '2060': 8.525493361792556 * u.au,
+            '3200': 0.14015636489876157 * u.au,
             '3556': 2.493684653191043 * u.au,
+            '300163': 2.440748125244917 * u.au,
+            '2024 ON': 1.006933657057601 * u.au
             }
 
-ESCAPEROUTES = {'Apophis': 0.87852,
+ESCAPEROUTES = {'1': None,
+                'Apophis': 0.87852,
+                'Toutatis': 0.22495,
                 'Beagle': None,
+                '2060': None,
+                '3200': 0.64189,
                 '3556': None,
+                '300163': None,
+                '2024 ON': 0.53245
                 }
 
-LIGHTCURVES = {'Apophis': 30.56 * u.h,
+LIGHTCURVES = {'1': 9.07417 * u.h,
+               'Apophis': 30.56 * u.h,
+               'Toutatis': 176 * u.h,
                'Beagle': 7.035 * u.h,
+               '2060': 5.918 * u.h,
+               '3200': 3.604 * u.h,
                '3556': None,
+               '300163': 3240 * u.h,
+               '2024 ON': None
                }
 
-ORBITS = {'Apophis': 18.068 * u.yr,
+ORBITS = {'1': 223.748 * u.yr,
+          'Apophis': 18.068 * u.yr,
+          'Toutatis': 48.588 * u.yr,
           'Beagle': 116.943 * u.yr,
+          '2060': 129.695 * u.yr,
+          '3200': 41.237 * u.yr,
           '3556': 59.856 * u.yr,
+          '300163': 22.461 * u.yr,
+          '2024 ON': 11.644 * u.yr
           }
 
-TAXONOMIES = {'Apophis': "Sq",
+TAXONOMIES = {'1': "G",
+              'Apophis': "Sq",
+              'Toutatis': "S",
               'Beagle': "C",
+              '2060': "B",
+              '3200': "F",
               '3556': None,
+              '300163': None,
+              '2024 ON': None
               }
 
 
