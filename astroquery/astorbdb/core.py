523709764c7e9dfdf54242114b04bafc3a19c2e2
hhsieh00 <hhsieh@gmail.com>
2025-12-15 18:28:49 -1000

    Revert "Fixing some codestyle issues in core.py"
    This reverts commit 70c829769c3d82d517409a95027fc49a0de84147.

--- astroquery/astorbdb/core.py
+++ astroquery/astorbdb/core.py
@@ -12,7 +12,6 @@ from . import conf
 
 __all__ = ['AstInfo', 'AstInfoClass']
 
-
 @async_to_sync
 class AstInfoClass(BaseQuery):
 
@@ -707,6 +706,7 @@ class AstInfoClass(BaseQuery):
             src = src['taxonomies']
 
         return src
+
 
 AstInfo = AstInfoClass()
 
