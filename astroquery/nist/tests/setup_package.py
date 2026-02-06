4ef5bf154d4b444b50128f2805aed2d044c29f16
Brigitta Sipőcz <bsipocz@gmail.com>
2026-02-06 10:59:30 -0800

    MAINT: cleaning up package data metadata

--- astroquery/nist/tests/setup_package.py
+++
@@ -1,9 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-
-import os
-
-
-def get_package_data():
-    paths_test = [os.path.join('data', '*.html')]
-    return {'astroquery.nist.tests': paths_test}
--- astroquery/solarsystem/pds/tests/setup_package.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def get_package_data():
-    paths = [os.path.join("data", "*.html")]  # etc, add other extensions
-
-    return {"astroquery.solarsystem.pds.tests": paths}
