43cb5dc99b595a56239e78de416433b2ab3138b0
Brigitta Sipőcz <bsipocz@gmail.com>
2026-02-10 10:45:03 -0800

    CI: windows workaround for pytest-dependency issue

--- conftest.py
+++ conftest.py
@@ -40,6 +40,14 @@ def pytest_configure(config):
 
     TESTED_VERSIONS['astroquery'] = version.version
 
+    # Workaround for https://github.com/RKrahl/pytest-dependency/issues/91
+    # We use pytest-dependency for some of the remote tests and it is not a critical dependency.
+    # We don't install it in CI for windows as use the following workaround to ensure the marker is always defined.
+    if sys.platform == 'win32':
+        config.addinivalue_line(
+            "markers", "dependency: this should be set by pytest-dependency, we do it here as a temp workaround"
+        )
+
 
 def pytest_addoption(parser):
     parser.addoption(
