7e0dee88c01871df14fddb9429b01af2b08af53b
Henrik Norman <henrik@winterway.eu>
2026-04-06 13:11:19 +0200

    Fixed codestyle issues

--- astroquery/esasky/__init__.py
+++ astroquery/esasky/__init__.py
@@ -17,7 +17,7 @@ class Conf(_config.ConfigNamespace):
     Configuration parameters for `astroquery.esasky`.
     """
 
-    ESASKY_DOMAIN_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER, "ESASky TAP Common Server", alias='urlBase')
+    ESASKY_DOMAIN_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER, "ESASky TAP Common Server", aliases='urlBase')
     ESASKY_TAP_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER + ESASKY_TAP_COMMON, "ESASky TAP Server")
     ESASKY_DATA_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER + 'data?', "ESASky Data Server")
     ESASKY_TABLES_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER + ESASKY_TAP_COMMON + "/tables",
@@ -26,8 +26,10 @@ class Conf(_config.ConfigNamespace):
     ESASKY_MESSAGES = _config.ConfigItem("notification?action=GetNotifications", "ESASky Messages")
     ESASKY_LOGIN_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER + 'login', "ESASky Login Server")
     ESASKY_LOGOUT_SERVER = _config.ConfigItem(ESASKY_COMMON_SERVER + 'logout', "ESASky Logout Server")
-    ESASKY_CONNECTION_TIMEOUT = _config.ConfigItem(1000, 'Time limit for connecting to a data product server.', alias='timeout')
-    ESASKY_ROW_LIMIT = _config.ConfigItem(10000, 'Maximum number of rows returned (set to -1 for unlimited).', alias='row_limit')
+    ESASKY_CONNECTION_TIMEOUT = _config.ConfigItem(1000, 'Time limit for connecting to a data product server.', 
+                                                   aliases='timeout')
+    ESASKY_ROW_LIMIT = _config.ConfigItem(10000, 'Maximum number of rows returned (set to -1 for unlimited).', 
+                                          aliases='row_limit')
 
     @property
     def urlBase(self):
