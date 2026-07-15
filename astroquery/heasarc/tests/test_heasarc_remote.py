2261afb773a8f75c0a174074733b065334f69cf1
azoghbi <a.zoghbi@nasa.gov>
2026-07-15 17:13:03 -0400

    remove maxrec test; irrelevant after pyvo behaviour change

--- astroquery/heasarc/tests/test_heasarc_remote.py
+++ astroquery/heasarc/tests/test_heasarc_remote.py
@@ -10,8 +10,6 @@ from astropy.coordinates import SkyCoord
 from astropy.utils.exceptions import AstropyDeprecationWarning
 from astroquery.exceptions import NoResultsWarning
 
-from pyvo.dal.exceptions import DALOverflowWarning
-
 from astroquery.heasarc import Heasarc
 
 
@@ -55,9 +53,6 @@ DEFAULT_COLS = [
         ],
     ],
 ]
-
-# The MAXREC related overflow message is different in pyvo 1.7+, remove workaround when we have it as a minimum
-overflow_message = r"Partial result set. Potential causes MAXREC|Result set limited by user- or server-supplied MAXREC"
 
 
 @pytest.mark.remote_data
@@ -156,12 +151,6 @@ class TestHeasarc:
         assert "rosmaster" in catalogs
         assert "rassmaster" in catalogs
 
-    def test_tap__maxrec(self):
-        query = "SELECT TOP 10 ra,dec FROM xray"
-        with pytest.warns(expected_warning=DALOverflowWarning, match=overflow_message):
-            result = Heasarc.query_tap(query=query, maxrec=5)
-        assert len(result) == 5
-        assert result.to_table().colnames == ["ra", "dec"]
 
     @pytest.mark.parametrize("tdefault", DEFAULT_COLS)
     def test__get_default_columns(self, tdefault):
